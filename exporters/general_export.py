a61be5da75db6885d545b1d3f96492e793f7846b
Robert Jensen <jensen.robert@gmail.com>
2018-11-23 12:27:22 +0000

    General export now takes argument from command line

--- exporters/general_export.py
+++ exporters/general_export.py
@@ -8,34 +8,42 @@
 
 """
 Make a number of pre-defined queries into MO.
+In the abcense of known information about the dataset, the deepest root
+tree is assumed to be the one to be exported.
 These are for the general case, specific version for each municipality
 is typically a better solution.
 """
-
 import time
+import click
 from mora_helpers import MoraHelper
 import common_queries as cq
 
+@click.command()
+@click.option('--root', default=None, help='uuid of the root to be exported')
+@click.option('--threaded-speedup', default=False, help='Run in multithreaded mode')
 
-if __name__ == '__main__':
-    threaded_speedup = True
+def export_from_mo(root, threaded_speedup):
+    threaded_speedup = threaded_speedup
     t = time.time()
 
-    mh = MoraHelper()
+    mh = MoraHelper(export_ansi=False)
 
     org = mh.read_organisation()
     roots = mh.read_top_units(org)
 
-    trees = {}
-    max_height = 0
-    for root in roots:
-        name = root['name']
-        uuid = root['uuid']
-        trees[name] = mh.read_ou_tree(uuid)
-        if trees[name]['root'].height > max_height:
-            main_root = name
+    if root is None:
+        trees = {}
+        max_height = 0
+        for root in roots:
+            name = root['name']
+            uuid = root['uuid']
+            trees[name] = mh.read_ou_tree(uuid)
+            if trees[name]['root'].height > max_height:
+                main_root = name
+            nodes = trees[main_root]
+    else:
+        nodes = mh.read_ou_tree(root)
     print('Find main tree: {}'.format(time.time() - t))
-    nodes = trees[main_root]
 
     if threaded_speedup:
         cq.pre_cache_users(mh)
@@ -57,6 +65,10 @@ if __name__ == '__main__':
     cq.export_adm_org(mh, nodes, filename)
     print('adm-org-incl-start-stop: {}s'.format(time.time() - t))
 
-    # filename = 'Teams-tilknyttede.csv'
-    # cq.export_all_teams(mh, nodes, filename)
-    # print('Teams: {}s'.format(time.time() - t))
+    filename = 'tilknytninger.csv'
+    cq.export_all_teams(mh, nodes, filename)
+    print('tilknytninger: {}s'.format(time.time() - t))
+
+
+if __name__ == '__main__':
+    export_from_mo()
