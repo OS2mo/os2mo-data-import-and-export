31022c6bd07fe4c841fdf2011cc0d148fa06afd0
Steffen Park <dev@istherepie.com>
2019-01-30 08:11:18 +0100

    26329: Example updated to reflect the recent changes

--- tooling/os2mo_data_import/example.py
+++ tooling/os2mo_data_import/example.py
@@ -1,7 +1,7 @@
 # -- coding: utf-8 --
+from uuid import uuid4
 
-from os2mo_data_import import Organisation, ImportUtility
-
+from os2mo_data_import import ImportUtility
 
 def example_import():
     """
@@ -9,319 +9,325 @@ def example_import():
 
     """
 
+    # Init
+    os2mo = ImportUtility()
+
+
     # The Organisation class is the main entry point,
     # It exposes the related sub classes such as:
     # Facet, Klasse, Itsystem, OrganisationUnit, Employee
-    Magenta = Organisation(
-        name="Magenta Aps",
+
+    os2mo.add_organisation(
+        identifier="Magenta Aps",
         user_key="Magenta",
-        municipality_code=101,
-        create_defaults=True
+        municipality_code=101
     )
 
+
     # Add klasse with reference to facet "Enhedstype"
-    Magenta.Klasse.add(
-        identifier="Hovedenhed",  # User defined identifier
-        facet_type_ref="Enhedstype",  # Belongs to facet: Enhedstype
-        user_key="D1ED90C5-643A-4C12-8889-6B4174EF4467",  # User key for internal reference
+    os2mo.add_klasse(
+        identifier="Hovedenhed",
+        facet_type_ref="Enhedstype",
+        user_key="D1ED90C5-643A-4C12-8889-6B4174EF4467",
         title="Hovedenhed"  # This is the displayed value
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Afdeling",
         facet_type_ref="Enhedstype",
         user_key="91154D1E-E7CA-439B-B910-D4622FD3FD21",
         title="Afdeling"
     )
 
+
     # Root unit: Magenta
     # Belongs to unit type: "Hovedenhed"
-    Magenta.OrganisationUnit.add(
+    os2mo.add_organisation_unit(
         identifier="Magenta",
         name="Magenta Aps",
-        org_unit_type_ref="Hovedenhed",  # Reference to the unit type
+        type_ref="Hovedenhed",  # Reference to the unit type
         date_from="1986-01-01"
     )
 
     # Use parent_ref to make it a sub group of "Magenta"
-    Magenta.OrganisationUnit.add(
+    os2mo.add_organisation_unit(
         identifier="Pilestræde",
-        org_unit_type_ref="Afdeling",  # This unit is of type: Afdeling
+        type_ref="Afdeling",  # This unit is of type: Afdeling
         parent_ref="Magenta",  # Sub unit of/Belongs to Magenta
         date_from="1986-01-01"
     )
 
-    Magenta.OrganisationUnit.add(
+    os2mo.add_organisation_unit(
         identifier="SJA2",
-        org_unit_type_ref="Afdeling",
+        type_ref="Afdeling",
         parent_ref="Magenta",  # Sub unit of/Belongs to Magenta
-        date_from="1986-01-01"
+        date_from="1986-01-01",
     )
+
+
 
     # HOTFIX: nested units are failing
     # This will 'really' be fixed in the refactoring state
 
     # Adding sub units to the example
-    Magenta.OrganisationUnit.add(
+    os2mo.add_organisation_unit(
         identifier="Sysadmins",
-        org_unit_type_ref="Afdeling",
+        type_ref="Afdeling",
         parent_ref="SJA2",  # Sub unit of/Belongs to SJA2
         date_from="1986-01-01"
     )
 
-    Magenta.OrganisationUnit.add(
+    os2mo.add_organisation_unit(
         identifier="Dummy",
-        org_unit_type_ref="Afdeling",
+        type_ref="Afdeling",
         parent_ref="Sysadmins",  # Sub unit of/Belongs to SJA2
         date_from="1986-01-01"
     )
 
-    # Address types for Organisation Unit
-    Magenta.OrganisationUnit.add_type_address(
-        owner_ref="Magenta",
+    os2mo.add_address_type(
+        organisation_unit="Magenta",
         uuid="0a3f50c4-379f-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
-        date_from="1986-01-01",
+        type_ref="AdressePost",
+        date_from="1986-01-01"
     )
 
-    Magenta.OrganisationUnit.add_type_address(
-        owner_ref="Magenta",
+    os2mo.add_address_type(
+        organisation_unit="Magenta",
         value="00112233",
-        address_type_ref="EAN",
+        type_ref="EAN",
         date_from="1986-01-01",
     )
 
-    Magenta.OrganisationUnit.add_type_address(
-        owner_ref="Magenta",
+    os2mo.add_address_type(
+        organisation_unit="Magenta",
         value="11223344",
-        address_type_ref="Telefon",
+        type_ref="Telefon",
         date_from="1986-01-01",
     )
-
-
-    # Create employees
-    Magenta.Employee.add(
-        identifier="Susanne Chæf",
-        cpr_no="0101862233"
-    )
-
-    Magenta.Employee.add(
-        identifier="Odin Perskov",
-        cpr_no="0102862234"
-    )
-
-    Magenta.Employee.add(
-        identifier="Ronja Rwander",
-        cpr_no="0103862234"
-    )
-
-    Magenta.Employee.add(
-        identifier="Jens Mortensen",
-        cpr_no="0104862235"
-    )
-
-    Magenta.Employee.add(
-        identifier="Bolette Buhl",
-        cpr_no="0105862235"
-    )
-
-    Magenta.Employee.add(
-        identifier="Carl Sand Holth",
-        cpr_no="0106862235"
-    )
-
 
     # Create job functions and assign to employees
 
     # Add job functions
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Direktør",
         facet_type_ref="Stillingsbetegnelse",
         user_key="Direktør",
         title="Direktør"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Projektleder",
         facet_type_ref="Stillingsbetegnelse",
         user_key="Projektleder",
         title="Projektleder"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Udvikler",
         facet_type_ref="Stillingsbetegnelse",
         user_key="Udvikler",
         title="Udvikler"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Projektmedarbejder",
         facet_type_ref="Stillingsbetegnelse",
         user_key="Projektmedarbejder",
         title="Projektmedarbejder"
     )
 
+    os2mo.add_employee(
+        identifier="Susanne Chæf",
+        cpr_no="0101862233"
+    )
+
+    os2mo.add_employee(
+        identifier="Odin Perskov",
+        cpr_no="0102862234"
+    )
+
+    os2mo.add_employee(
+        identifier="Ronja Rwander",
+        cpr_no="0103862234"
+    )
+
+    os2mo.add_employee(
+        identifier="Jens Mortensen",
+        cpr_no="0104862235"
+    )
+
+    os2mo.add_employee(
+        identifier="Bolette Buhl",
+        cpr_no="0105862235"
+    )
+
+    os2mo.add_employee(
+        identifier="Carl Sand Holth",
+        cpr_no="0106862235"
+    )
+
     # Assign job functions
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Susanne Chæf",
-        org_unit_ref="Magenta",
+    os2mo.add_engagement(
+        employee="Susanne Chæf",
+        organisation_unit="Magenta",
         job_function_ref="Direktør",
         engagement_type_ref="Ansat",
         date_from="1986-01-01"
     )
 
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Odin Perskov",
-        org_unit_ref="Pilestræde",
+    os2mo.add_engagement(
+        employee="Odin Perskov",
+        organisation_unit="Pilestræde",
         job_function_ref="Projektleder",
         engagement_type_ref="Ansat",
         date_from="1986-02-01"
     )
 
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Ronja Rwander",
-        org_unit_ref="SJA2",
+    os2mo.add_engagement(
+        employee="Ronja Rwander",
+        organisation_unit="SJA2",
         job_function_ref="Projektleder",
         engagement_type_ref="Ansat",
         date_from="1986-03-01"
     )
 
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Jens Mortensen",
-        org_unit_ref="Pilestræde",
+    os2mo.add_engagement(
+        employee="Jens Mortensen",
+        organisation_unit="Pilestræde",
         job_function_ref="Udvikler",
         engagement_type_ref="Ansat",
         date_from="1986-04-01"
     )
 
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Bolette Buhl",
-        org_unit_ref="SJA2",
+    os2mo.add_engagement(
+        employee="Bolette Buhl",
+        organisation_unit="SJA2",
         job_function_ref="Udvikler",
         engagement_type_ref="Ansat",
         date_from="1986-05-01"
     )
 
-    Magenta.Employee.add_type_engagement(
-        owner_ref="Carl Sand Holth",
-        org_unit_ref="Pilestræde",
+    os2mo.add_engagement(
+        employee="Carl Sand Holth",
+        organisation_unit="Pilestræde",
         job_function_ref="Projektmedarbejder",
         engagement_type_ref="Ansat",
         date_from="1986-06-01"
     )
 
-    # Add association
-    Magenta.Klasse.add(
+
+    # Association
+    os2mo.add_klasse(
         identifier="Ekstern Konsulent",
         facet_type_ref="Tilknytningstype",
         user_key="Ekstern Konsulent",
         title="Ekstern Konsulent"
     )
 
-    Magenta.Employee.add_type_association(
-        owner_ref="Carl Sand Holth",
-        org_unit_ref="Pilestræde",
+    os2mo.add_association(
+        employee="Carl Sand Holth",
+        organisation_unit="Pilestræde",
         job_function_ref="Projektmedarbejder",
         association_type_ref="Ekstern Konsulent",
         address_uuid="0a3f50c4-379f-32b8-e044-0003ba298018",
         date_from="1986-10-01"
     )
 
-
-    # Add addresses
-    Magenta.Employee.add_type_address(
-        owner_ref="Susanne Chæf",
+    os2mo.add_address_type(
+        employee="Susanne Chæf",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
-    Magenta.Employee.add_type_address(
-        owner_ref="Odin Perskov",
+    os2mo.add_address_type(
+        employee="Odin Perskov",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
-    Magenta.Employee.add_type_address(
-        owner_ref="Ronja Rwander",
+
+    os2mo.add_address_type(
+        employee="Ronja Rwander",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
-    Magenta.Employee.add_type_address(
-        owner_ref="Jens Mortensen",
+    os2mo.add_address_type(
+        employee="Jens Mortensen",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
-    Magenta.Employee.add_type_address(
-        owner_ref="Bolette Buhl",
+    os2mo.add_address_type(
+        employee="Bolette Buhl",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
-    Magenta.Employee.add_type_address(
-        owner_ref="Carl Sand Holth",
+    os2mo.add_address_type(
+        employee="Carl Sand Holth",
         uuid="0a3f50a0-ef5a-32b8-e044-0003ba298018",
-        address_type_ref="AdressePost",
+        type_ref="AdressePost",
         date_from="1986-11-01",
     )
 
 
     # Add roles and assign to employees
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Medarbejder repræsentant",
         facet_type_ref="Rolletype",
         user_key="Medarbejder repræsentant",
         title="Medarbejder repræsentant"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Nøgleansvarlig",
         facet_type_ref="Rolletype",
         user_key="Nøgleansvarlig",
         title="Nøgleansvarlig"
     )
 
-    Magenta.Employee.add_type_role(
-        owner_ref="Susanne Chæf",
-        org_unit_ref="Magenta",
+
+    os2mo.add_role(
+        employee="Susanne Chæf",
+        organisation_unit="Magenta",
         role_type_ref="Nøgleansvarlig",
         date_from="1986-12-01"
     )
 
-    Magenta.Employee.add_type_role(
-        owner_ref="Bolette Buhl",
-        org_unit_ref="SJA2",
+    os2mo.add_role(
+        employee="Bolette Buhl",
+        organisation_unit="SJA2",
         role_type_ref="Medarbejder repræsentant",
         date_from="1986-12-01"
     )
 
-    Magenta.Employee.add_type_role(
-        owner_ref="Jens Mortensen",
-        org_unit_ref="Pilestræde",
+    os2mo.add_role(
+        employee="Jens Mortensen",
+        organisation_unit="Pilestræde",
         role_type_ref="Medarbejder repræsentant",
         date_from="1986-12-01"
     )
+
 
     # Create manager type, level and responsibilites
     # and assign to employee
 
     # Manager type
-    Magenta.Klasse.add(
-        identifier="Direktør",
+    os2mo.add_klasse(
+        identifier="Leder",
         facet_type_ref="Ledertyper",
         user_key="Direktør",
         title="Virksomhedens direktør"
     )
 
     # Manager level
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Højeste niveau",
         facet_type_ref="Lederniveau",
         user_key="Højeste niveau",
@@ -329,86 +335,85 @@ def example_import():
     )
 
     # Add responsabilities
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Tage beslutninger",
         facet_type_ref="Lederansvar",
         user_key="Tage beslutninger",
         title="Tage beslutninger"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Motivere medarbejdere",
         facet_type_ref="Lederansvar",
         user_key="Motivere medarbejdere",
         title="Motivere medarbejdere"
     )
 
-    Magenta.Klasse.add(
+    os2mo.add_klasse(
         identifier="Betale løn",
         facet_type_ref="Lederansvar",
         user_key="Betale løn",
         title="Betale løn"
     )
 
-    Magenta.Employee.add_type_manager(
-        owner_ref="Susanne Chæf",
-        org_unit_ref="Magenta",
-        manager_type_ref="Direktør",
+    os2mo.add_manager(
+        employee="Susanne Chæf",
+        organisation_unit="Magenta",
+        manager_type_ref="Leder",
         manager_level_ref="Højeste niveau",
         responsibility_list=["Tage beslutninger", "Motivere medarbejdere", "Betale løn"],
         date_from="1986-12-01",
     )
 
 
+
+
     # Leave of absence (Does not work after release 0.10)
     # Leave type requires an exisiting engagement type
 
-    # Magenta.Klasse.add(
-    #     identifier="Sygeorlov",
-    #     facet_type_ref="Orlovstype",
-    #     user_key="Sygeorlov",
-    #     title="Sygeorlov"
-    # )
-    #
-    # Magenta.Employee.add_type_leave(
-    #     owner_ref="Jens Mortensen",
-    #     leave_type_ref="Sygeorlov",
-    #     date_from="2018-01-22",
-    #     date_to="2018-11-02"
-    # )
-    #
-    # Magenta.Employee.add_type_leave(
-    #     owner_ref="Bolette Buhl",
-    #     leave_type_ref="Sygeorlov",
-    #     date_from="2018-01-22",
-    #     date_to="2018-11-02"
-    # )
+    os2mo.add_klasse(
+        identifier="Sygeorlov",
+        facet_type_ref="Orlovstype",
+        user_key="Sygeorlov",
+        title="Sygeorlov"
+    )
+
+    os2mo.add_leave(
+        employee="Jens Mortensen",
+        leave_type_ref="Sygeorlov",
+        date_from="2018-01-22",
+        date_to="2018-11-02"
+    )
+
+    os2mo.add_leave(
+        employee="Bolette Buhl",
+        leave_type_ref="Sygeorlov",
+        date_from="2018-01-22",
+        date_to="2018-11-02"
+    )
 
     # Create IT system and assign to employee
 
-    Magenta.Itsystem.add(
+    os2mo.new_itsystem(
         identifier="Servermiljø",
         system_name="Servermiljø"
     )
 
-    Magenta.Employee.add_type_itsystem(
-        owner_ref="Jens Mortensen",
+    os2mo.join_itsystem(
+        employee="Jens Mortensen",
         user_key="jmort",
         itsystem_ref="Servermiljø",
         date_from="1987-10-01"
     )
 
-    Magenta.Employee.add_type_itsystem(
-        owner_ref="Bolette Buhl",
+    os2mo.join_itsystem(
+        employee="Bolette Buhl",
         user_key="bolbu",
         itsystem_ref="Servermiljø",
         date_from="1987-10-01"
     )
 
-    # Import everything into running instance
-    # Requires an actual running instance
-    os2mo = ImportUtility(dry_run=False)
-    os2mo.import_all(Magenta)
+    os2mo.export()
 
 
 if __name__ == "__main__":
