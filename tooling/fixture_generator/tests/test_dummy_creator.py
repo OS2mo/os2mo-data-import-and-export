0b7716ec56f4feac4db7ed70179217931423fae0
Robert Jensen <jensen.robert@gmail.com>
2019-02-12 11:37:09 +0000

    Updated tests to fit new random results

--- tooling/fixture_generator/tests/test_dummy_creator.py
+++ tooling/fixture_generator/tests/test_dummy_creator.py
@@ -17,10 +17,11 @@ class DummyTest(unittest.TestCase):
         for i in range(0, 10):
             names.append(self.ddc.create_name(return_user_key=False))
         expected_names = [
-            'Bjarne Tornbjerg', 'Elsebeth Silkjær Schelde', 'Inge Graverholt Brandt',
-            'Sonja Kjær Jamil', 'Tage Vendelbo Stanimirovic', 'Inga Nielsen',
-            'Maria Brejnholt Skinbjerg', 'Christian Ankjær Gustavsen',
-            'Ralf Greve Christensen', 'Marie Mauritzen Werenfeldt Christensen',
+            'Kristine Serup Jørgensen', 'Arne Larsen',
+            'Christian Heldgaard Frederiksen', 'Flemming Lindgaard Skorstengaard',
+            'Maria Ahmednor Brøsted', 'Anders Maagaard Pedersen',
+            'Anne Balsgaard Barløse', 'Margit Hesselbjerg', 'Tina Petersen',
+            'Christen Bødker Weinell'
         ]
         self.assertEqual(names, expected_names)
 
@@ -36,7 +37,7 @@ class DummyTest(unittest.TestCase):
         for node in PreOrderIter(self.ddc.nodes['root']):
             if node.type == 'ou':
                 number_of_ous += 1
-        self.assertEqual(number_of_ous, 23)
+        self.assertEqual(number_of_ous, 24)
 
     def test_cpr(self):
         """ Test that the cpr generator is deterministic when the random
@@ -45,11 +46,12 @@ class DummyTest(unittest.TestCase):
         for i in range(0, 20):
             cpr = dummy_data_creator._cpr()
             cprs.append(cpr)
-        expected_cprs = ['2812580526', '2312723746', '0711592857', '2808723851',
-                         '1601553631', '0611632215', '2208720301', '2709652721',
-                         '0112700498', '2812793929', '0603721357', '3009951619',
-                         '1209942689', '2205750595', '1712911766', '0709670719',
-                         '1906751743', '1607922639', '0106742685', '0110911912']
+        expected_cprs = [
+            '1603902889', '2306542888', '0812940899', '1107671443', '0304801840',
+            '1908981878', '1505731278', '2903853568', '2505651246', '0712852186',
+            '0802652739', '0602420868', '2901881971', '1601853538', '2711570880',
+            '0612461619', '1604683315', '0804922695', '1909641671', '0912613852'
+        ]
         self.assertEqual(expected_cprs, cprs)
 
     def test_number_of_users(self):
@@ -59,7 +61,7 @@ class DummyTest(unittest.TestCase):
         for node in PreOrderIter(self.ddc.nodes['root']):
             if node.type == 'user':
                 number_of_users += 1
-        self.assertEqual(number_of_users, 98)
+        self.assertEqual(number_of_users, 111)
 
     def test_it_systems(self):
         """ Test that all it-systems are given to users """
@@ -79,7 +81,7 @@ class DummyTest(unittest.TestCase):
         """ Test that all roles are given to users and that the
         roles are given in a valid unit """
         expected_roles = {}
-        for role in dummy_data_creator.CLASSES['Rolletype']:
+        for role in dummy_data_creator.CLASSES['role_type']:
             expected_roles[role] = 0
         self.ddc.add_users_to_tree(ou_size_scale=2)
         for node in PreOrderIter(self.ddc.nodes['root']):
@@ -98,9 +100,9 @@ class DummyTest(unittest.TestCase):
         unit = None
         for node in PreOrderIter(self.ddc.nodes['root']):
             if node.type == 'user':
-                if node.user[0]['cpr'] == '1404433829':
+                if node.user[0]['cpr'] == '2310750800':
                     unit = node.user[0]['association']['unit']
-        assert(unit == '4efa9a5f-6185-54e8-9dbc-d4d8518e9754')
+        assert(unit == 'c2a70b50-6a30-5771-bf8e-24c515fa1e34')
 
     def test_managers_in_ous(self):
         """ Test that all units have a manager """
