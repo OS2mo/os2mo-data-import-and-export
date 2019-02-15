336a6942b7aa8606199e32932798246b1c34acf9
Steffen Park <dev@istherepie.com>
2019-02-15 09:25:31 +0100

    26329: Docstrings updated for mora type classes

--- tooling/os2mo_data_import/os2mo_data_import/mora_data_types.py
+++ tooling/os2mo_data_import/os2mo_data_import/mora_data_types.py
@@ -8,11 +8,29 @@
 
 
 class MoType():
+    """
+    Base class for all Mo (MORA) type objects
+
+    :param uuid:
+    The object uuid (str/uuid)
+    This can be set to forcefully import the object with its current uuid.
+    If the object already exists in the database, it is updated/overridden.
+
+    :param type_id:
+    Type identifier for "detail" objects (str)
+    A detail object can be one of the following types:
+        address, association, role, itsystem, engagement, manager
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     insert_data = True
 
     def __init__(self):
-
         self.type_id = None
 
         self.payload = {}
@@ -30,6 +48,17 @@ class MoType():
         self.org_unit_uuid = None
 
     def _build_payload(self):
+        """
+        Create a POST data payload for os2mo (mora)
+
+        As a minimum the date_from and date_to attributes must be set.
+        Additionally must each set the type_id attribute.
+        Optionally a person_uuid and org_unit_uuid can be set
+        on "detail" class objects which belong to either
+        an organisation unit or a employee.
+
+        :return: Returns a POST data payload (Type: dict)
+        """
 
         if not isinstance(self.payload, dict):
             raise TypeError("Cannot build non-dict types")
@@ -64,6 +93,39 @@ class MoType():
 
 
 class AddressType(MoType):
+    """
+    Address type detail which belongs to eit
+
+    :param value:
+    The address value (str)
+    Example:
+        The value can be a phone number:
+        value: 11223344
+
+        or a reference to a DAR object:
+        value: 62B99146-BA66-4563-B15E-A4F4B9000B58
+
+    :param type_ref:
+    Reference to the Klasse object that contains the scope (str)
+
+    The os2mo representation of the values depends on its scope.
+    The type_ref refers to a scope by referencing the Klasse object that contains
+    the scope.
+
+    For more information, please see mox_data_types:Klasse.
+
+    :param visibility:
+    Visibility toggle of whether the value is displayed (bool)
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, value, type_ref, date_from,
                  date_to=None, uuid=None, visibility=None):
@@ -80,7 +142,7 @@ class AddressType(MoType):
         self.visibility_ref = visibility
         self.visibility_ref_uuid = None
 
-        # https://os2mo.readthedocs.io/en/development/api/address.html#writing
+
         self.organisation_uuid = None
 
         # SPECIAL
@@ -90,6 +152,12 @@ class AddressType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         if not self.organisation_uuid:
             raise ValueError("Organisation UUID is missing")
@@ -109,6 +177,30 @@ class AddressType(MoType):
 
 
 class EngagementType(MoType):
+    """
+    Engagement type detail.
+    This detail describes the current employment.
+
+    :param org_unit_ref:
+    Reference to the parent organisation unit of the employment (str)
+
+    :param engagement_type_ref:
+    Reference to the type of employment (str)
+    Examples: "Full time", "Part time", "External", "Internal" etc.
+
+    :param job_function_ref:
+    Reference job function definition aka "job title" (str)
+    Examples: "Bridge officer", "Science assistant", "Space dock cleaner" etc.
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, org_unit_ref, job_function_ref, engagement_type_ref,
                  date_from, date_to=None, uuid=None):
@@ -130,6 +222,12 @@ class EngagementType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         # Reference the parent org unit uuid
         if not self.org_unit_uuid:
@@ -159,6 +257,49 @@ class EngagementType(MoType):
 
 
 class AssociationType(MoType):
+    """
+    Engagement type detail.
+    This detail describes the current employment.
+
+    :param org_unit_ref:
+    Reference to the parent organisation unit of the employment (str)
+
+    :param engagement_type_ref:
+    Reference to the type of employment (str)
+    Examples: "Full time", "Part time", "External", "Internal" etc.
+
+    :param job_function_ref:
+    Reference job function definition aka "job title" (str)
+    Examples: "Bridge officer", "Science assistant", "Space dock cleaner" etc.
+
+    :param association_type_ref:
+    Reference to the association that the employee has with the organisation unit (str)
+    There are several ways to define an association,
+    for example it could be that an employee is associated with
+    any given projects/tasks within the organisation unit.
+
+    Example:
+        Employee: Luke Skywalker
+        Organisation unit: Republic
+        Engagement: Jedi/Rebel scum
+        Association: Affairs on the moon of Endor
+
+    :param address_uuid:
+    The (uuid) identifier of a DAR address object (str/uuid)
+
+    :param address_type_ref:
+    Reference to the address type (str)
+    NOTE: this can only be a reference to a Klasse object with the scope: DAR.
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, association_type_ref, org_unit_ref, job_function_ref,
                  date_from, date_to=None, address_uuid=None, address_type_ref=None,
@@ -188,6 +329,12 @@ class AssociationType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         if not self.org_unit_uuid:
             raise ReferenceError("Reference to org_unit_uuid is missing")
@@ -222,6 +369,22 @@ class AssociationType(MoType):
 
 
 class ItsystemType(MoType):
+    """
+    Creates a detail object that describes the connection
+    with an existing it system.
+
+    :param: itsystem_ref:
+    Reference to the it system type (str)
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, user_key, itsystem_ref, date_from, date_to=None, uuid=None):
         super().__init__()
@@ -238,6 +401,12 @@ class ItsystemType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         if not self.itsystem_uuid:
             raise ReferenceError("UUID of itsystem type is missing")
@@ -253,6 +422,23 @@ class ItsystemType(MoType):
 
 
 class LeaveType(MoType):
+    """
+    Leave type detail describes a period of leave of absence
+    of an associated employee.
+
+    :param leave_type_ref:
+    Reference to the type of leave (str)
+    Examples: "Illness", "R & R", "Captivity", "Off-record secret mission"
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, leave_type_ref, date_from, date_to=None, uuid=None):
         super().__init__()
@@ -268,6 +454,12 @@ class LeaveType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         self.payload = {
             "leave_type": {
@@ -279,6 +471,26 @@ class LeaveType(MoType):
 
 
 class RoleType(MoType):
+    """
+    Role type detail describes a which role an employee holds
+    with the associated organisation unit.
+
+    :param org_unit:
+    Reference to the associated organisation unit (str)
+
+    :param role_type_ref:
+    Reference to the role type (str)
+    Examples: "Illness", "R & R", "Captivity", "Off-record secret mission"
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, org_unit, role_type_ref, date_from, date_to=None, uuid=None):
         super().__init__()
@@ -297,6 +509,12 @@ class RoleType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         self.payload = {
             "org_unit": {
@@ -311,6 +529,41 @@ class RoleType(MoType):
 
 
 class ManagerType(MoType):
+    """
+    Role type detail describes a which role an employee holds
+    with the associated organisation unit.
+
+    :param org_unit:
+    Reference to the associated organisation unit (str)
+
+    :param manager_type_ref:
+    Reference to type of manager position (str)
+    Examples: "Jedi Master", "Emperor" etc.
+
+    :param manager_level_ref:
+    Reference to the role type (str)
+    Examples: "Security level 5", "Bridge access" etc
+
+    :param responsibility_list:
+    List of references to responsibility types (list)
+    Examples: "Bridge officer", "Squad leader", "Victory party organiser"
+
+    :param address_uuid:
+    The (uuid) identifier of a DAR address object (str/uuid)
+
+    :param address_type_ref:
+    Reference to the address type (str)
+    NOTE: this can only be a reference to a Klasse object with the scope: DAR.
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, org_unit, manager_type_ref, manager_level_ref,
                  responsibility_list, date_from, date_to=None,
@@ -344,6 +597,13 @@ class ManagerType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        :return:
+        """
 
         self.payload = {
             "org_unit": {
@@ -375,6 +635,30 @@ class ManagerType(MoType):
 
 
 class OrganisationUnitType(MoType):
+    """
+    :param name:
+    Name of the organisation unit (str)
+
+    :param type_ref:
+    Reference to the organisation unit type/Klasse (str)
+
+    :param user_key:
+    Optionally set a specific user_key (str)
+    This is mainly usefull as an internal reference.
+
+    :param parent_ref:
+    Reference to the parent organisation unit/Klasse (str)
+    If this value is not set, the organisation becomes the parent.
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, name, type_ref, date_from, date_to=None,
                  user_key=None, parent_ref=None, uuid=None):
@@ -396,6 +680,12 @@ class OrganisationUnitType(MoType):
         self.date_to = date_to
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         if not self.parent_uuid:
             raise ReferenceError("UUID of the parent organisation is missing")
@@ -418,6 +708,29 @@ class OrganisationUnitType(MoType):
 
 
 class EmployeeType(MoType):
+    """
+    :param name:
+    Full name of the employee (str)
+
+    :param cpr_no:
+    10 digit CPR identifier code (str)
+
+    :param user_key:
+    Optionally set a specific user_key (str)
+    This is mainly usefull as an internal reference.
+
+    :param org:
+    Reference to the organisation to which the employee belongs (str)
+
+    :param uuid:
+    The object uuid (str/uuid)
+
+    :param date_from:
+    Start date e.g. "1900-01-01" (str)
+
+    :param date_to:
+    End date e.g. "1900-01-01" (str)
+    """
 
     def __init__(self, name, cpr_no, org=None, uuid=None, user_key=None):
         super().__init__()
@@ -432,6 +745,12 @@ class EmployeeType(MoType):
         self.org_uuid = None
 
     def build(self):
+        """
+        Apply class specific attributes before calling
+        the underlying _build method to create the POST data payload.
+
+        :return: POST data payload (dict)
+        """
 
         if not self.org_uuid:
             raise ReferenceError("UUID of the organisation is missing")
