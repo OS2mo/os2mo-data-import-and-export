1e4649e598d7e25ed13152909c90e63e9424ef7b
Robert Jensen <jensen.robert@gmail.com>
2019-02-22 07:41:26 +0000

    Fix name and remove global start date

--- integrations/ballerup/ballerup.sh
+++ integrations/ballerup/ballerup.sh
@@ -2,10 +2,9 @@
 export MOX_BASE=http://localhost:5000
 export MORA_BASE=http://localhost:80
 
-export MUNICIPALITY_NAME=Ballerup
+export MUNICIPALITY_NAME="Ballerup Kommune"
 export MUNICIPALITY_CODE=151
 export ANSAT_UUID=56e1214a-330f-4592-89f3-ae3ee8d5b2e6
-export GLOBAL_DATE=2010-01-01
 export BASE_APOS_URL=http://apos.balk.dk:8080/apos2-
 export CREATE_UDVALGS_CLASSES=yes
 export PHONE_NAMES="Alt. Tlf.":"Kontakt Tlf."
