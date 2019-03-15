ecf0b981a91909a2fd59cf7dacb892f32f15faaa
Robert Jensen <jensen.robert@gmail.com>
2019-03-15 09:18:57 +0000

    Support for the expanded syntax of the sd-importer

--- integrations/viborg/viborg.py
+++ integrations/viborg/viborg.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import datetime
+
 from os2mo_data_import import ImportHelper
 import viborg_uuids
 sys.path.append('..')
@@ -9,6 +11,16 @@ MUNICIPALTY_NAME = os.environ.get('MUNICIPALITY_NAME', 'SD-Løn Import')
 MUNICIPALTY_CODE = os.environ.get('MUNICIPALITY_CODE', 0)
 MOX_BASE = os.environ.get('MOX_BASE', 'http://localhost:8080')
 MORA_BASE = os.environ.get('MORA_BASE', 'http://localhost:80')
+
+# ORIGIN FOR TESTS WIH ACTUAL API
+# GLOBAL_GET_DATE = datetime.datetime(2006, 1, 1, 0, 0) # CACHED - will not work
+# GLOBAL_GET_DATE = datetime.datetime(2009, 1, 1, 0, 0) # CACHED
+# GLOBAL_GET_DATE = datetime.datetime(2011, 1, 1, 0, 0) # CACHED
+# GLOBAL_GET_DATE = datetime.datetime(2014, 2, 15, 0, 0) # CACHED
+GLOBAL_GET_DATE = datetime.datetime(2019, 2, 15, 0, 0) # CACHED
+
+# GLOBAL_TO = GLOBAL_GET_DATE + datetime.timedelta(days=10)
+GLOBAL_TO = None
 
 importer = ImportHelper(
     create_defaults=True,
@@ -30,7 +42,9 @@ sd = sd_importer.SdImport(
     importer,
     MUNICIPALTY_NAME,
     MUNICIPALTY_CODE,
-    forced_uuids=ad_uuids,
+    import_date_from=GLOBAL_GET_DATE,
+    import_date_to = GLOBAL_TO,
+    forced_uuids=ad_uuids
 )
 
 for cpr, username in ad_usernames.items():
