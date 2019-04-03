313211c18ff7f09a9185ee7bb5ae589cfc23593e
Robert Jensen <jensen.robert@gmail.com>
2019-04-03 12:47:44 +0200

    Code is now able to update and create users.

--- integrations/sd_changed_at.py
+++ integrations/sd_changed_at.py
@@ -12,7 +12,7 @@ class ChangeAtSD(object):
         self.helper = MoraHelper()
         self.from_date = from_date
         self.to_date = to_date
-        # print(self.helper.read_organisation())
+        self.org_uuid = self.helper.read_organisation()
 
     def read_employment_changed(self):
         url = 'GetEmploymentChangedAtDate20111201'
@@ -40,17 +40,37 @@ class ChangeAtSD(object):
             'StatusPassiveIndicator': 'false',
             'ContactInformationIndicator': 'false',
             'PostalAddressIndicator': 'false'
+            # TODO: Er der kunder, som vil udlæse adresse-information?
         }
         url = 'GetPersonChangedAtDate20111201'
         response = sd_lookup(url, params=params)
         return response['Person']
 
     def update_changed_persons(self):
+        # Så vidt vi ved, består person_changed af navn, cpr nummer og ansættelser.
+        # Ansættelser håndteres af update_employment, så vi tjekker for ændringer i
+        # navn og opdaterer disse poster. Nye personer oprettes.
         person_changed = self.read_person_changed()
         for person in person_changed:
-            print('--------')
+            # TODO: Shold this go in sd_common?
+            given_name = person.get('PersonGivenName', '')
+            sur_name = person.get('PersonSurnameName', '')
+            sd_name = '{} {}'.format(given_name, sur_name)
             cpr = person['PersonCivilRegistrationIdentifier']
-            print(person)
+
+            mo_person = self.helper.read_user(user_cpr=cpr, org_uuid=self.org_uuid)
+            if mo_person:
+                if mo_person['name'] == sd_name:
+                    return
+
+            payload = {
+                "name": sd_name,
+                "cpr_no": cpr,
+                "org": {
+                    "uuid": self.org_uuid
+                }
+            }
+            print(payload)
 
     def update_employments(self):
         employments_changed = self.read_employment_changed()
@@ -89,4 +109,4 @@ if __name__ == '__main__':
 
     sd_updater = ChangeAtSD(from_date, to_date)
     sd_updater.update_changed_persons()
-    # sd_updater.update_employments()
+    sd_updater.update_employments()
