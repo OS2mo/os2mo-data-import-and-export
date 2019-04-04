f37976b2b2a1fa537a9aafa7ba010378acf8153d
Robert Jensen <jensen.robert@gmail.com>
2019-04-04 10:55:52 +0000

    Further overview of components in the replys

--- integrations/sd_changed_at.py
+++ integrations/sd_changed_at.py
@@ -1,38 +1,108 @@
-# import os
+import os
+import requests
 import datetime
 from sd_common import sd_lookup
 from os2mo_helpers.mora_helpers import MoraHelper
+MOX_BASE = os.environ.get('MOX_BASE', None)
 
-import os
-import requests
-SAML_TOKEN = os.environ.get('SAML_TOKEN', None)
+
+#SAML_TOKEN = os.environ.get('SAML_TOKEN', None)
 
 
 class ChangeAtSD(object):
 
     def __init__(self, from_date, to_date):
+        self.mox_base = MOX_BASE
         self.helper = MoraHelper()
         self.from_date = from_date
         self.to_date = to_date
         self.org_uuid = self.helper.read_organisation()
 
-    def read_employment_changed(self):
-        url = 'GetEmploymentChangedAtDate20111201'
-        params = {
-            'ActivationDate': self.from_date.strftime('%d.%m.%Y'),
-            'DeactivationDate': self.to_date.strftime('%d.%m.%Y'),
-            'StatusActiveIndicator': 'true',
-            'DepartmentIndicator': 'true',
-            'EmploymentStatusIndicator': 'true',
-            'ProfessionIndicator': 'true',
-            'WorkingTimeIndicator': 'true',
-            'UUIDIndicator': 'true',
-            'StatusPassiveIndicator': 'false',
-            'SalaryAgreementIndicator': 'false',
-            'SalaryCodeGroupIndicator': 'false'
+        self.employment_response = None
+
+        facet_info = self.helper.read_classes_in_facet('engagement_job_function')
+        job_functions = facet_info[0]
+        self.job_function_facet = facet_info[1]
+        self.job_functions = {}
+        for job in job_functions:
+            self.job_functions[job['name']] = job['uuid']
+
+        # If this assertment fails, we will need to re-run the organisation
+        # stucture through the normal importer.
+        # assert self.check_non_existent_departments()
+
+    def _add_profession_to_lora(self, profession):
+        validity = {
+            'from': '1900-01-01',
+            'to': 'infinity'
         }
-        response = sd_lookup(url, params=params)
-        return response['Person']
+
+        properties = {
+            'brugervendtnoegle': profession,
+             # "integrationsdata":  # TODO: Check this
+            'titel':  profession,
+            'omfang': 'TEXT',
+            "virkning": validity
+        }
+        attributter = {
+            'klasseegenskaber': [properties]
+        }
+        relationer = {
+            'ansvarlig': [
+                {
+                    'objekttype': 'organisation',
+                    'uuid': self.org_uuid,
+                    'virkning': validity
+                }
+            ],
+            'facet': [
+                {
+                    'objekttype': 'facet',
+                    'uuid': self.job_function_facet,
+                    'virkning': validity
+                }
+            ]
+        }
+        tilstande = {
+            'klassepubliceret': [
+                {
+                    'publiceret': 'Publiceret',
+                    'virkning': validity
+                }
+            ]
+        }
+
+        payload = {
+            "attributter": attributter,
+            "relationer": relationer,
+            "tilstande": tilstande
+        }
+        response = requests.post(
+            url=self.mox_base + '/klassifikation/klasse',
+            json=payload
+        )
+        assert response.status_code == 201
+        return response.json()
+
+    def read_employment_changed(self):
+        if not self.employment_response:
+            url = 'GetEmploymentChangedAtDate20111201'
+            params = {
+                'ActivationDate': self.from_date.strftime('%d.%m.%Y'),
+                'DeactivationDate': self.to_date.strftime('%d.%m.%Y'),
+                'StatusActiveIndicator': 'true',
+                'DepartmentIndicator': 'true',
+                'EmploymentStatusIndicator': 'true',
+                'ProfessionIndicator': 'true',
+                'WorkingTimeIndicator': 'true',
+                'UUIDIndicator': 'true',
+                'StatusPassiveIndicator': 'false',
+                'SalaryAgreementIndicator': 'false',
+                'SalaryCodeGroupIndicator': 'false'
+            }
+            response = sd_lookup(url, params=params)
+            self.employment_resonse = response['Person']
+        return self.employment_resonse
 
     def read_person_changed(self):
         params = {
@@ -74,39 +144,66 @@ class ChangeAtSD(object):
             }
             print(payload)
 
+    def check_non_existent_departments(self):
+        """
+        Runs through all changes and checks if all org units exists in MO.
+        :return: True if org is self consistent, False if not.
+        """
+        all_ok = True
+        employments_changed = self.read_employment_changed()
+        for employment in employments_changed:
+            sd_engagement = employment['Employment']
+            if not isinstance(sd_engagement, list):
+                sd_engagement = [sd_engagement]
+            for engagement in sd_engagement:
+                departments = engagement.get('EmploymentDepartment')
+                if departments:
+                    if not isinstance(departments, list):
+                        departments = [departments]
+                    for department in departments:
+                        department_uuid = department['DepartmentUUIDIdentifier']
+                        ou = self.helper.read_ou(department_uuid)
+                        if 'status' in ou:
+                            all_ok = False
+                            print('Error: {}'.format(department_uuid))
+                        else:
+                            print('Success: {}'.format(department_uuid))
+        return all_ok
+
     def update_employments(self):
         employments_changed = self.read_employment_changed()
         for employment in employments_changed:
             cpr = employment['PersonCivilRegistrationIdentifier']
-            
+
             sd_engagement = employment['Employment']
             if not isinstance(sd_engagement, list):
                 sd_engagement = [sd_engagement]
 
-            #mo_person = self.helper.read_user(user_cpr=cpr, org_uuid=self.org_uuid)
-            #mo_engagement = self.helper.read_user_engagement(
-            #    mo_person['uuid'],
-            #    at=self.from_date.strftime('%Y-%m-%d'),
-            #    use_cache=False
-            #)
+            mo_person = self.helper.read_user(user_cpr=cpr, org_uuid=self.org_uuid)
+            if mo_person:
+                print(mo_person)
+                1/0
+            mo_engagement = self.helper.read_user_engagement(
+                mo_person['uuid'],
+                at=self.from_date.strftime('%Y-%m-%d'),
+                use_cache=False
+            )
             print()
             print('----')
             for engagement in sd_engagement:
-                #print()
-                #print(engagement.keys())
                 job_id = engagement['EmploymentIdentifier']
-                print(job_id)
-                status_list = engagement.get('EmploymentStatus', None)
-                department = engagement.get('EmploymentDepartment', None)
-                profession = engagement.get('Profession', None)
-                working_time = engagement.get('WorkingTime', None)
-                employment_date = engagement.get('EmploymentDate', None)
+                print('Job id: {}'.format(job_id))
+                status_list = engagement.get('EmploymentStatus')
+                department = engagement.get('EmploymentDepartment')
+                professions = engagement.get('Profession')
+                working_time = engagement.get('WorkingTime')
+                employment_date = engagement.get('EmploymentDate')
                 if status_list:
                     if not isinstance(status_list, list):
                         status_list = [status_list]
                     for status in status_list:
-                        code = status['EmploymentStatusCode'] 
-                        if not code in ('0', '1', '3', '8', '9', 'S'):
+                        code = status['EmploymentStatusCode']
+                        if code not in ('0', '1', '3', '8', '9', 'S'):
                             print(status)
                             1/0
                         if status['EmploymentStatusCode'] == '0':
@@ -121,19 +218,38 @@ class ChangeAtSD(object):
                 if department:
                     # This field is typically used along with a status change
                     # Jobid 23531 has a department entry with no status change
-                    # print(department)
+                    department_uuid = department['DepartmentUUIDIdentifier']
+                    print(self.helper.read_ou(department_uuid))
                     print('Change in department')
+                    1/0
                     pass
 
-                if profession:
-                    # print(profession)
+                if professions:
+                    # If the profession has changed, this will be a list
+                    if not isinstance(professions, list):
+                        professions = [professions]
+                    for profession in professions:
+                        emp_name = profession['EmploymentName']
+                        job_uuid = self.job_functions.get(emp_name)
+                        #if not job_uuid:
+                        #    print('New job function: {}'.format(emp_name))
+                        #    # uuid = self._add_profession_to_lora(emp_name)
+                        #    uuid = self._add_profession_to_lora('KLAF')
+                        #    self.job_functions[emp_name] = uuid
+
+                        # Now we are ready to update the employment
                     print('Change in profession')
                     pass
 
                 if working_time:
                     # Here we need to re-calculate primary engagement
+                    # print(working_time)
                     print('Change in working time')
-                    print(working_time)
+
+                if employment_date:
+                    # This seems to be redundant information
+                    pass
+
 
 if __name__ == '__main__':
     from_date = datetime.datetime(2019, 2, 15, 0, 0)
