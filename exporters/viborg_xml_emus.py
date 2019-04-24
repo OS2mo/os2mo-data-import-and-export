ac822a810ea8b222ac67bbbe022c5e439a237952
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-04-24 12:50:12 +0200

    emus integration

--- exporters/viborg_xml_emus.py
+++ exporters/viborg_xml_emus.py
@@ -17,7 +17,12 @@ import common_queries as cq
 import datetime
 import requests
 import uuid
+import os
 
+MORA_BASE = os.environ.get('MORA_BASE', 'http://localhost:80')
+MORA_ROOT_ORG_UNIT_NAME = os.environ.get('MORA_ROOT_ORG_UNIT_NAME', 'Viborg Kommune')
+USERID_ITSYSTEM = os.environ.get('USERID_ITSYSTEM', 'Active Directory')
+EMUS_FILENAME = os.environ.get('EMUS_FILENAME', 'emus_filename.xml')
 
 
 def get_emus_address(ou_uuid):
@@ -36,20 +41,23 @@ def get_emus_address(ou_uuid):
     if not len(response.json()) == 1:
         return {}
     address = response.json()[0]
+
     pstnrby = " ".join([
         address["postnr"],
         address["postnrnavn"],
      ])
+
     return {
         'zipCode': address["postnr"],
         'city': address["postnrnavn"],
-        'street' : mh_address["Adresse"].replace(", " + pstnrby, "")
+        'street': mh_address["Adresse"].replace(", " + pstnrby, "")
     }
 
 
 def export_ou_emus(mh, nodes, emus_file):
     fieldnames = ['startDate', 'endDate', 'parentOrgUnit', 'manager',
-        'longName', 'street', 'zipCode', 'city', 'phoneNumber']
+                  'longName', 'street', 'zipCode', 'city', 'phoneNumber']
+
     rows = []
     for node in cq.PreOrderIter(nodes['root']):
         ou = mh.read_ou(node.name)
@@ -59,7 +67,9 @@ def export_ou_emus(mh, nodes, emus_file):
         fra = ou['validity']['from'] if ou['validity']['from'] else ''
         til = ou['validity']['to'] if ou['validity']['to'] else ''
         over_uuid = ou['parent']['uuid'] if ou['parent'] else ''
-        phone= mh.read_ou_address(ou["uuid"], scope="PHONE").get("Adresse","")
+        phone = mh.read_ou_address(
+            ou["uuid"], scope="PHONE"
+        ).get("Adresse", "")
 
         row = {
             'uuid': ou['uuid'],
@@ -68,29 +78,114 @@ def export_ou_emus(mh, nodes, emus_file):
             'parentOrgUnit': over_uuid,
             'manager': manager_uuid,
             'longName': ou['name'],
-            'street': address.get("street",''),
-            'zipCode': address.get("zipCode",''),
-            'city': address.get("city",''),
-            'phoneNumber':phone,
+            'street': address.get("street", ''),
+            'zipCode': address.get("zipCode", ''),
+            'city': address.get("city", ''),
+            'phoneNumber': phone,
         }
         rows.append(row)
 
     last_changed = datetime.datetime.now().strftime("%Y-%m-%d")
     for r in rows:
-        emus_file.write("<orgUnit id=\"%s\" client=\"1\" lastChanged=\"%s\">\n" %(
-            r["uuid"],
+        emus_file.write(
+            "<orgUnit id=\"%s\" client=\"1\" lastChanged=\"%s\">\n" % (
+                r["uuid"],
+                last_changed,
+            )
+        )
+
+        for fn in fieldnames:
+            emus_file.write("<%s>%s</%s>\n" % (fn, r.get(fn, ''), fn))
+
+        emus_file.write("</orgUnit>\n")
+
+
+def get_e_username(e_uuid, id_it_system, mh):
+    for its in mh._mo_lookup(e_uuid, 'e/{}/details/it'):
+        if its['itsystem']["user_key"] == id_it_system:
+            return its['user_key']
+    return ''
+
+
+def get_e_address(e_uuid, scope, mh):
+    for address in mh._mo_lookup(e_uuid, 'e/{}/details/address'):
+        if address['address_type']['scope'] == scope:
+            return address
+    return {}
+
+
+def export_e_emus(mh, nodes, emus_file):
+    fieldnames = ['entryDate', 'leaveDate', 'cpr', 'firstName',
+                  'lastName', 'workPhone', 'workContract', 'workContractText',
+                  'positionId', 'position', "orgUnit", 'email', "username"]
+    rows = []
+
+    for node in cq.PreOrderIter(nodes['root']):
+        ou = mh.read_ou(node.name)
+        for engagement in mh._mo_lookup(
+                ou["uuid"],
+                'ou/{}/details/engagement'
+        ):
+            entrydate = engagement.get("validity", {}).get("from")
+            leavedate = engagement.get("validity", {}).get("to")
+            employee = mh._mo_lookup(
+                engagement["person"]["uuid"],
+                'e/{}'
+            )
+            firstname, lastname = engagement["person"]["name"].rsplit(
+                " ", maxsplit=1
+            )
+            username = get_e_username(
+                engagement["person"]["uuid"],
+                'Active Directory',
+                mh
+            )
+            _phone = get_e_address(engagement["person"]["uuid"], "PHONE", mh)
+            _email = get_e_address(engagement["person"]["uuid"], "EMAIL", mh)
+
+            row = {
+                'tjenestenr': engagement.get("user_key", ''),
+                'entryDate': entrydate if entrydate else '',
+                'leaveDate': leavedate if leavedate else '',
+                'cpr': employee['cpr_no'],
+                'firstName': firstname,
+                'lastName': lastname,
+                'workPhone': _phone.get("name", '') if _phone.get(
+                    "visibility", {}
+                ).get("scope", "") != "SECRET" else 'hemmelig',
+                'workContract': engagement.get(
+                    "engagement_type", {}
+                ).get("uuid", ""),
+                'workContractText': engagement.get(
+                    "engagement_type", {}
+                ).get("name", ""),
+                'positionId': engagement.get("job_function", {}).get("uuid", ""),
+                'position': engagement.get("job_function", {}).get("name", ""),
+                'orgUnit': ou["uuid"],
+                'email': _email.get("name", "") if _email.get(
+                    "visibility", {}
+                ).get("scope", "") != "SECRET" else 'hemmelig',
+                'username': username,
+            }
+
+            rows.append(row)
+
+    last_changed = datetime.datetime.now().strftime("%Y-%m-%d")
+    for r in rows:
+        emus_file.write("<employee id=\"%s\" client=\"1\" lastChanged=\"%s\">\n" % (
+            r["tjenestenr"],
             last_changed,
         ))
         for fn in fieldnames:
-            emus_file.write("<%s>%s</%s>\n"%(fn, r.get(fn,''), fn))
-        emus_file.write("</orgUnit>\n")
+            emus_file.write("<%s>%s</%s>\n" % (fn, r.get(fn, ''), fn))
+        emus_file.write("</employee>\n")
+
 
 def main(
-    root_org_unit_name="Læsø Kommune",
-    threaded_speedup=False,
+    root_org_unit_name=MORA_ROOT_ORG_UNIT_NAME,
     mh=MoraHelper(),
     t=time.time(),
-    emus_filename="emus_filename.xml",
+    emus_filename=EMUS_FILENAME
 ):
     org = mh.read_organisation()
     roots = mh.read_top_units(org)
@@ -98,26 +193,19 @@ def main(
         if root['name'] == root_org_unit_name:
             root_org_unit_uuid = root['uuid']
 
-    if threaded_speedup:
-        cq.pre_cache_users(mh)
-        print('Build cache: {}'.format(time.time() - t))
-
     nodes = mh.read_ou_tree(root_org_unit_uuid)
-    print('Read nodes: {}s'.format(time.time() - t))
 
     with open(emus_filename, "w", encoding="utf-8") as emus_xml:
 
         emus_xml.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-        emus_xml.write("<OS2MO>")
+        emus_xml.write("<OS2MO>\n")
 
         export_ou_emus(mh, nodes, emus_xml)
-        print('Writing Org Units: {}s'.format(time.time() - t))
+        export_e_emus(mh, nodes, emus_xml)
 
         emus_xml.write("</OS2MO>")
 
 
 if __name__ == '__main__':
-    mh = MoraHelper("os2mo-28377:5000")
-    #mh.read_organisationsenhed = mh.read_ou
+    mh = MoraHelper(MORA_BASE)
     main(mh=mh)
-
