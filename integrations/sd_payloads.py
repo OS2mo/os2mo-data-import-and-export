bc6a0f9373feee9ec77e0fbf8463cf53bb7eaeff
Robert Jensen <jensen.robert@gmail.com>
2019-04-24 11:34:16 +0000

    Moved more payloads to seperate file.

--- integrations/sd_payloads.py
+++ integrations/sd_payloads.py
@@ -11,3 +11,64 @@ def new_department(department, org, unit_type):
         }
     }
     return payload
+
+
+def create_leave(mo_eng, mo_person, leave_uuid, job_id, validity):
+    payload = {
+        'type': 'leave',
+        'org_unit': {'uuid': mo_eng['org_unit']['uuid']},
+        'person': {'uuid': mo_person['uuid']},
+        'leave_type': {'uuid': leave_uuid},
+        'user_key': job_id,
+        'validity': validity
+    }
+    return payload
+
+
+def profession(profession, org, job_function_facet):
+    validity = {
+        'from': '1900-01-01',
+        'to': 'infinity'
+    }
+
+    # "integrationsdata":  # TODO: Check this
+    properties = {
+        'brugervendtnoegle': profession,
+        'titel':  profession,
+        'omfang': 'TEXT',
+        "virkning": validity
+    }
+    attributter = {
+        'klasseegenskaber': [properties]
+    }
+    relationer = {
+        'ansvarlig': [
+            {
+                'objekttype': 'organisation',
+                'uuid': org,
+                'virkning': validity
+            }
+        ],
+        'facet': [
+            {
+                'objekttype': 'facet',
+                'uuid': job_function_facet,
+                'virkning': validity
+            }
+        ]
+    }
+    tilstande = {
+        'klassepubliceret': [
+            {
+                'publiceret': 'Publiceret',
+                'virkning': validity
+            }
+        ]
+    }
+
+    payload = {
+        "attributter": attributter,
+        "relationer": relationer,
+        "tilstande": tilstande
+    }
+    return payload
