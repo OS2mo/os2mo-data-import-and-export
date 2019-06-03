c9218dc76685dafd99b730f006537ad6b95f38a8
Robert Jensen <jensen.robert@gmail.com>
2019-06-03 12:03:11 +0000

    Rebild specifics.

---
+++ integrations/rebild/rebild.sh
@@ -0,0 +1,7 @@
+export MUNICIPALITY_NAME='Rebild Kommune'
+
+export MOX_BASE=http://localhost:5000
+export MORA_BASE=http://localhost:80
+export XML_FILE_PATH=ZLPE20190522223720_delta.xml
+
+python3 rebild.py
