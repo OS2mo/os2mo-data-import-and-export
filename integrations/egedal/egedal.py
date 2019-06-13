68fa22c4b709f9cf42cf279c123b62aa82ce977a
Robert Jensen <jensen.robert@gmail.com>
2019-06-13 12:42:59 +0000

    Specifics for Egedal.

---
+++ integrations/egedal/egedal.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import datetime
+
+from os2mo_data_import import ImportHelper
+sys.path.append('../SD_Lon')
+import sd_importer
+
+MUNICIPALTY_NAME = os.environ.get('MUNICIPALITY_NAME', 'SD-Løn Import')
+MUNICIPALTY_CODE = os.environ.get('MUNICIPALITY_CODE', 0)
+MOX_BASE = os.environ.get('MOX_BASE', 'http://localhost:8080')
+MORA_BASE = os.environ.get('MORA_BASE', 'http://localhost:80')
+
+GLOBAL_GET_DATE = datetime.datetime(2019, 6, 13, 0, 0)
+
+importer = ImportHelper(
+    create_defaults=True,
+    mox_base=MOX_BASE,
+    mora_base=MORA_BASE,
+    system_name='SD-Import',
+    end_marker='SDSTOP',
+    store_integration_data=True
+)
+
+sd = sd_importer.SdImport(
+    importer,
+    MUNICIPALTY_NAME,
+    MUNICIPALTY_CODE,
+    import_date_from=GLOBAL_GET_DATE
+)
+
+sd.create_ou_tree()
+# sd.create_employees()
+
+importer.import_all()
+
---
+++ integrations/egedal/egedal.sh
@@ -0,0 +1,7 @@
+export MUNICIPALITY_NAME='Egedal Kommune'
+export MUNICIPALITY_CODE=240
+
+export MOX_BASE=http://localhost:5000
+export MORA_BASE=http://localhost:80
+
+python3 egedal.py
