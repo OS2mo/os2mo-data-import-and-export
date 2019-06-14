3e53f3f55f93e849a8c728cf3c622975b7714e98
Robert Jensen <jensen.robert@gmail.com>
2019-06-14 13:53:22 +0200

    Getting closer to decent POC for import.

--- integrations/SD_Lon/sd-mox.py
+++ integrations/SD_Lon/sd-mox.py
@@ -30,7 +30,7 @@ class sdMox(object):
 
         self.mh = MoraHelper(hostname='localhost:5000')
 
-        self.from_date = datetime.datetime(2010, 5, 1, 0, 0)
+        self.from_date = datetime.datetime(2019, 5, 1, 0, 0)
         to_date = datetime.datetime(2040, 6, 1, 0, 0)
         self.virkning = sd_mox_payloads.sd_virkning(self.from_date, to_date)
         self.xml = None
@@ -64,55 +64,67 @@ class sdMox(object):
         }
         return registrering
 
-    def create_objekt_id(self, unit):
+    def create_objekt_id(self, unit_uuid):
         objekt_id = {
-            'sd:UUIDIdentifikator': unit,
+            'sd:UUIDIdentifikator': unit_uuid,
             'sd:IdentifikatorType': 'OrganisationEnhed'
         }
         return objekt_id
 
-    def create_attribut_liste(self, unit, unit_name=None):
-        # print(self.mh.read_ou(unit)) #This will not work for test-data
-        if not unit_name:
-            pass
+    def create_attribut_liste(self, unit_uuid=None, unit_name='Klaf', unit_code=None):
+        # print(self.mh.read_ou(unit_uuid)) #This will not work for test-data
         attribut_liste = {
-            "Egenskab": {
-                "sd:EnhedNavn": 'UUUUUUUUUUUUU',
-                "sd:Virkning": self.virkning
-            },
             "sd:LokalUdvidelse": {
                 "silkdata:Integration": [
                     {
                         "sd:Virkning": self.virkning,
                         "silkdata:AttributNavn": 'EnhedKode',
-                        "silkdata:AttributVaerdi": "KLAF"
+                        "silkdata:AttributVaerdi": unit_code
                     },
                     {
                         "sd:Virkning": self.virkning,
                         "silkdata:AttributNavn": 'Niveau',
                         "silkdata:AttributVaerdi": "Afdelings-niveau"
-                    }#,
-                    #{
-                    #    "sd:Virkning": self.virkning,
-                    #    "silkdata:AttributNavn": "FunktionKode",
-                    #    "silkdata:AttributVaerdi": "32201"
-                    #},
-                    #{
-                    #    "sd:Virkning": self.virkning,
-                    #    "silkdata:AttributNavn": "SkoleKode",
-                    #    "silkdata:AttributVaerdi": "12346"
-                    #},
-                    #{
-                    #    "sd:Virkning": self.virkning,
-                    #    "silkdata:AttributNavn": "Tidsregistrering",
-                    #    "silkdata:AttributVaerdi": "Arbejdstidsplaner"
-                    #}
+                    },
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "FunktionKode",
+                        "silkdata:AttributVaerdi": "32201"
+                    },
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "SkoleKode",
+                        "silkdata:AttributVaerdi": "12346"
+                    },
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "Tidsregistrering",
+                        "silkdata:AttributVaerdi": "Arbejdstidsplaner"
+                    }
                 ]
             }
         }
+        if unit_name:
+            attribut_liste['Egenskab'] = {
+                "sd:EnhedNavn": unit_name,
+                "sd:Virkning": self.virkning
+            }
+            
         return attribut_liste
 
-    def create_relations_liste(self, pnummer=None, tlf=None, parent=None):
+    def create_relations_liste_import(self, parent=None):
+        relations_liste = {
+            'sd:Overordnet': {
+                'sd:Virkning': self.virkning,
+                'sd:ReferenceID': {
+                    'sd:UUIDIdentifikator': parent
+                }
+            },
+            'sd:LokalUdvidelse': {}
+        }
+        return relations_liste
+
+    def create_relations_liste_ret(self, pnummer=None, tlf=None):
         """
         relations_liste = {
             'sd:LokalUdvidelse': {
@@ -156,6 +168,20 @@ class sdMox(object):
             }
         return relations_liste
 
+    def create_xml_import(self, uuid, unit_code, parent):
+        value_dict = {
+            'RelationListe': self.create_relations_liste_import(parent),
+            'AttributListe': self.create_attribut_liste(unit_code=unit_code),
+            'Registrering': self.create_registrering(registry_type='Opstaaet'),
+            'ObjektID': self.create_objekt_id(unit_uuid=uuid)
+        }
+        edit_dict = {'RegistreringBesked': value_dict}
+        edit_dict['RegistreringBesked'].update(sd_mox_payloads.boilerplate)
+        self.xml = xmltodict.unparse(edit_dict)
+
+    def create_xml_ret(self):
+        pass
+    
     def create_xml(self, unit, create=False, parent=None):
         edit_dict = {'RegistreringBesked': {}}
         edit_dict['RegistreringBesked'].update(sd_mox_payloads.boilerplate)
@@ -169,44 +195,31 @@ class sdMox(object):
         self.xml = xmltodict.unparse(edit_dict)
 
     def on_response(self, ch, method, props, body):
-        print('Response:')
-        print('Body: {}'.format(body))
-        print('Corr id: {}'.format(self.corr_id))
-        print('props: {}'.format(props))
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
+        print('Response?!??!?!')
+        print(body)
+    
     def call(self):
         self.response = None
-        self.corr_id = str(uuid.uuid4())
-
         self.channel.basic_publish(
             exchange=self.exchange_name,
             routing_key='#',
             properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
+                reply_to=self.callback_queue
             ),
             body=self.xml
         )
 
-        while self.response is None:
-            print(self.response)
-            self.connection.process_data_events()
-            time.sleep(0.1)
-
-            return self.response # NOTICE. Return is here to temporary prevent endless loop
-
-        return self.response
-
+        # Todo: We should to a lookup at verify actual unit
+        # matches the expected result
+        return True
 
 mox = sdMox()
 print('Send request')
 
-mox.create_xml(
+mox.create_xml_import(
     # unit='89ad7a4c-61c0-4900-9200-000001550002'
-    unit='00000000-00c0-4900-9200-000001550002',
-    create=True,
+    uuid='07782000-0000-4900-9200-000001550002',
+    unit_code='LLAK',
     parent='fd47d033-61c0-4900-b000-000001520002'
 )
 
