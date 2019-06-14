bd4b7e7a147233c6fd4d2e39cd7f0be26e3ca078
Robert Jensen <jensen.robert@gmail.com>
2019-06-14 11:11:33 +0200

    Now starting tests of creation of units.

--- integrations/SD_Lon/sd-mox.py
+++ integrations/SD_Lon/sd-mox.py
@@ -4,102 +4,13 @@ import datetime
 import xmltodict
 import sd_mox_payloads
 
+from os2mo_helpers.mora_helpers import MoraHelper
+
 AMQP_USER = os.environ.get('AMQP_USER')
 AMQP_PASSWORD = os.environ.get('AMQP_PASSWORD', None)
 VIRTUAL_HOST = os.environ.get('VIRTUAL_HOST', None)
 if not (AMQP_USER and AMQP_PASSWORD and VIRTUAL_HOST):
     raise Exception('Credentials missing')
-
-from_date = datetime.datetime(2020, 2, 1, 0, 0)
-to_date = datetime.datetime(2020, 3, 1, 0, 0)
-now = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%dT%H:%M:%S.00')
-virkning = sd_mox_payloads.sd_virkning(from_date, to_date)
-
-print(virkning)
-
-edit_dict = {
-    "RegistreringBesked": {
-    }
-}
-
-objekt_id = {
-    'sd:UUIDIdentifikator': '89ad7a4c-61c0-4900-9200-000001550002',
-    'sd:IdentifikatorType': 'OrganisationEnhed'
-}
-
-relations_liste = {
-    "sd:LokalUdvidelse": {
-        "silkdata:Lokation": {
-            "silkdata:ProduktionEnhed": {
-                "sd:Virkning": virkning,
-                "silkdata:ProduktionEnhedIdentifikator": "1011600936"
-            },
-            "silkdata:Kontakt": {
-                "sd:Virkning": virkning,
-                "silkdata:LokalTelefonnummerIdentifikator": "89892540"
-            },
-            "silkdata:DanskAdresse": {
-                "sd:Virkning": virkning,
-                "silkdata:AdresseNavn": "Arnegaardsvej 5",
-                "silkdata:PostKodeIdentifikator": "8600",
-                "silkdata:ByNavn": "Silkeborg"
-            }
-        }
-    }
-}
-
-registrering = {
-    "sd:FraTidspunkt": {
-        "sd:TidsstempelDatoTid": now
-    },
-    "sd:LivscyklusKode": "Rettet",
-    "sd:BrugerRef": {
-        "sd:UUIDIdentifikator": "3bb66b0d-132d-4b98-a903-ea29f6552d53",
-        "sd:IdentifikatorType": "AD"
-    },
-    "TilstandListe": {
-        "orgfaelles:Gyldighed": {
-            "sd:Virkning": virkning,
-            "orgfaelles:GyldighedStatusKode": "Aktiv"
-        },
-        "sd:LokalUdvidelse": None
-    },
-}
-
-attribut_liste = {
-    "Egenskab": {
-        "sd:EnhedNavn": 'PPPPP',
-        "sd:Virkning": virkning
-    },
-    "sd:LokalUdvidelse": {
-        "silkdata:Integration": [
-            {
-                "sd:Virkning": virkning,
-                "silkdata:AttributNavn": "FunktionKode",
-                "silkdata:AttributVaerdi": "32201"
-            },
-            {
-                "sd:Virkning": virkning,
-                "silkdata:AttributNavn": "SkoleKode",
-                "silkdata:AttributVaerdi": "12346"
-            },
-            {
-                "sd:Virkning": virkning,
-                "silkdata:AttributNavn": "Tidsregistrering",
-                "silkdata:AttributVaerdi": "Arbejdstidsplaner"
-            }
-        ]
-    }
-}
-
-
-edit_dict['RegistreringBesked'].update(sd_mox_payloads.boilerplate)
-edit_dict['RegistreringBesked']['RelationListe'] = relations_liste
-edit_dict['RegistreringBesked']['AttributListe'] = attribut_liste
-edit_dict['RegistreringBesked']['Registrering'] = registrering
-edit_dict['RegistreringBesked']['ObjektID'] = objekt_id
-
-XML = xmltodict.unparse(edit_dict)
 
 import pika
 import uuid
@@ -117,6 +28,13 @@ class sdMox(object):
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
 
+        self.mh = MoraHelper(hostname='localhost:5000')
+
+        self.from_date = datetime.datetime(2020, 5, 1, 0, 0)
+        to_date = datetime.datetime(2020, 6, 1, 0, 0)
+        self.virkning = sd_mox_payloads.sd_virkning(self.from_date, to_date)        
+        self.xml = None
+        
         result = self.channel.queue_declare('', exclusive=True)
         self.callback_queue = result.method.queue
         self.channel.basic_consume(
@@ -126,6 +44,102 @@ class sdMox(object):
         )
         
 
+    def create_registrering(self):
+        now =  datetime.datetime.strftime(
+            datetime.datetime.now(),
+            '%Y-%m-%dT%H:%M:%S.00'
+        )
+        registrering = {
+            "sd:FraTidspunkt": {
+                "sd:TidsstempelDatoTid": now
+            },
+            "sd:LivscyklusKode": "Rettet",
+            "TilstandListe": {
+                "orgfaelles:Gyldighed": {
+                    "sd:Virkning": self.virkning,
+                    "orgfaelles:GyldighedStatusKode": "Aktiv"
+                },
+                "sd:LokalUdvidelse": None
+            },
+        }
+        return registrering
+
+    def create_objekt_id(self, unit):
+        objekt_id = {
+            'sd:UUIDIdentifikator': unit,
+            'sd:IdentifikatorType': 'OrganisationEnhed'
+        }
+        return objekt_id
+
+    def create_attribut_liste(self, unit, unit_name=None):
+        # print(self.mh.read_ou(unit)) #This will not work for test-data
+        if not unit_name:
+            pass
+        attribut_liste = {
+            "Egenskab": {
+                "sd:EnhedNavn": 'UUUUUUUUUUUUU',
+                "sd:Virkning": self.virkning
+            },
+            "sd:LokalUdvidelse": {
+                "silkdata:Integration": [
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "FunktionKode",
+                        "silkdata:AttributVaerdi": "32201"
+                    },
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "SkoleKode",
+                        "silkdata:AttributVaerdi": "12346"
+                    },
+                    {
+                        "sd:Virkning": self.virkning,
+                        "silkdata:AttributNavn": "Tidsregistrering",
+                        "silkdata:AttributVaerdi": "Arbejdstidsplaner"
+                    }
+                ]
+            }
+        }
+        return attribut_liste
+
+    def create_relations_liste(self, pnummer=None, tlf=None, parent=None):
+        relations_liste = {
+            'sd:LokalUdvidelse': {
+                'silkdata:Lokation': {
+                    #"silkdata:DanskAdresse": {
+                    #    "sd:Virkning": self.virkning,
+                    #    "silkdata:AdresseNavn": "Arnegaardsvej 5",
+                    #    "silkdata:PostKodeIdentifikator": "8600",
+                    #    "silkdata:ByNavn": "Silkeborg"
+                    #}
+                }
+            }
+        }
+        if pnummer is not None:
+            relations_liste['sd:LokalUdvidelse']['silkdata:Lokation'] = {
+                'silkdata:ProduktionEnhed': {
+                    'sd:Virkning': self.virkning,
+                    'silkdata:ProduktionEnhedIdentifikator': pnummer
+                }
+            }
+        if tlf is not None:
+            relations_liste['sd:LokalUdvidelse']['silkdata:Lokation'] = {
+                'silkdata:Kontakt': {
+                    'sd:Virkning': self.virkning,
+                    'silkdata:LokalTelefonnummerIdentifikator': tlf
+                }
+            }
+        return relations_liste
+
+    def create_xml(self, unit, create=False, parent=None):
+        edit_dict = {'RegistreringBesked': {}}
+        edit_dict['RegistreringBesked'].update(sd_mox_payloads.boilerplate)
+        edit_dict['RegistreringBesked']['RelationListe'] = self.create_relations_liste(parent=parent)
+        edit_dict['RegistreringBesked']['AttributListe'] = self.create_attribut_liste(unit)
+        edit_dict['RegistreringBesked']['Registrering'] = self.create_registrering()
+        edit_dict['RegistreringBesked']['ObjektID'] = self.create_objekt_id(unit)
+        self.xml = xmltodict.unparse(edit_dict)
+    
     def on_response(self, ch, method, props, body):
         print('Response:')
         print('Body: {}'.format(body))
@@ -145,7 +159,7 @@ class sdMox(object):
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
             ),
-            body=XML
+            body=self.xml
         )
 
         while self.response is None:
@@ -160,6 +174,14 @@ class sdMox(object):
 
 mox = sdMox()
 print('Send request')
+
+mox.create_xml(
+    # unit='89ad7a4c-61c0-4900-9200-000001550002'
+    unit='00000000-61c0-4900-9200-000001550002',
+    create=True,
+    parent='fd47d033-61c0-4900-b000-000001520002'
+)
+
 response = mox.call()
 print('-----------------------')
 print(response)
