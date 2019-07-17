5b902f7a3e8ab428a7b49be8bfcbe2e9706e3d94
Robert Jensen <jensen.robert@gmail.com>
2019-07-17 14:02:30 +0200

    Enable AD integration in Holstebro.

--- integrations/holstebro/holstebro.py
+++ integrations/holstebro/holstebro.py
@@ -6,6 +6,9 @@ from os2mo_data_import import ImportHelper
 sys.path.append('../SD_Lon')
 import sd_importer
 
+sys.path.append('../ad_integration')
+import ad_reader
+
 MUNICIPALTY_NAME = os.environ.get('MUNICIPALITY_NAME', 'SD-Løn Import')
 MUNICIPALTY_CODE = os.environ.get('MUNICIPALITY_CODE', 0)
 MOX_BASE = os.environ.get('MOX_BASE', 'http://localhost:8080')
@@ -24,15 +27,18 @@ importer = ImportHelper(
     seperate_names=True
 )
 
+ad_info_reader = ad_reader.ADParameterReader()
+
 sd = sd_importer.SdImport(
     importer,
     MUNICIPALTY_NAME,
     MUNICIPALTY_CODE,
-    import_date_from=GLOBAL_GET_DATE
+    import_date_from=GLOBAL_GET_DATE,
+    ad_info=ad_info_reader
 )
 
-sd.create_ou_tree()
+sd.create_ou_tree(create_orphan_container=True)
 sd.create_employees()
 
-importer.import_all()
+# importer.import_all()
 
