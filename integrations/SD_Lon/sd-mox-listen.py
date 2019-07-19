1095cd295c2662a28d6b5b1b8746c9176103da00
Robert Jensen <jensen.robert@gmail.com>
2019-07-19 14:28:28 +0200

    Improved queue listener.

--- integrations/SD_Lon/sd-mox-listen.py
+++ integrations/SD_Lon/sd-mox-listen.py
@@ -1,6 +1,9 @@
 import os
+import time
 import pika
+import html
 import xmltodict
+
 
 AMQP_USER = os.environ.get('AMQP_USER')
 AMQP_PASSWORD = os.environ.get('AMQP_PASSWORD', None)
@@ -8,28 +11,43 @@ VIRTUAL_HOST = os.environ.get('VIRTUAL_HOST', None)
 if not (AMQP_USER and AMQP_PASSWORD and VIRTUAL_HOST):
     raise Exception('Credentials missing')
 
+
+def callback(ch, method, properties, body):
+    body = str(body)[2:-1]
+    body = html.unescape(body)
+    body = body.replace('\\t', '\t')
+    body = body.replace('\\r', '\n')
+    body = body.replace('\\n', '')
+    xml_response = xmltodict.parse(body)
+    print(xml_response)
+
+
 credentials = pika.PlainCredentials(AMQP_USER, AMQP_PASSWORD)
 parameters = pika.ConnectionParameters(host='msg-amqp.silkeborgdata.dk',
                                        port=5672,
                                        virtual_host=VIRTUAL_HOST,
                                        credentials=credentials)
 
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+print('Listning to queue')
+while True:
+    try:
+        t = time.time()
 
-channel.exchange_declare(exchange='callback-topic', exchange_type='topic')
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
 
-result = channel.queue_declare('callback', exclusive=False, durable=True)
-queue_name = result.method.queue
+        channel.exchange_declare(exchange='callback-topic', exchange_type='topic')
 
-channel.queue_bind(exchange='callback-topic', queue=queue_name, routing_key='#')
-print(' [*] Waiting for logs. To exit press CTRL+C')
+        result = channel.queue_declare('callback', exclusive=False, durable=True)
+        queue_name = result.method.queue
 
-def callback(ch, method, properties, body):
-        xml_response = xmltodict.parse(body)
-        print(xml_response)
+        channel.queue_bind(exchange='callback-topic', queue=queue_name,
+                           routing_key='#')
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-channel.start_consuming()
+        channel.basic_consume(
+            queue=queue_name, on_message_callback=callback, auto_ack=True
+        )
+        channel.start_consuming()
+    except pika.exceptions.StreamLostError:
+        print('Restart: {}s'.format(time.time() - t))
+        t = time.time()
