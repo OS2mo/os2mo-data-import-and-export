0acadd1f08ca8ad563bd4903803a90cbe95d7c13
Robert Jensen <jensen.robert@gmail.com>
2019-08-16 11:00:33 +0000

    Combination of import commands that actually yeilds desired tree.

--- integrations/egedal/egedal.py
+++ integrations/egedal/egedal.py
@@ -13,6 +13,9 @@ MORA_BASE = os.environ.get('MORA_BASE', 'http://localhost:80')
 
 GLOBAL_GET_DATE = datetime.datetime(2019, 6, 13, 0, 0)
 
+# Import of Administration
+adm_name = 'AdmOrg'
+
 importer = ImportHelper(
     create_defaults=True,
     mox_base=MOX_BASE,
@@ -23,18 +26,18 @@ importer = ImportHelper(
 )
 
 importer.add_klasse(
-    identifier='AdmOrg',
+    identifier=adm_name,
     facet_type_ref='org_unit_type',
-    user_key='AdmOrg',
+    user_key=adm_name,
     scope='TEXT',
-    title='AdmOrg'
+    title=adm_name
 )
 
 importer.add_organisation_unit(
-    identifier='AdmOrg',
-    name='AdmOrg',
-    user_key='AdmOrg',
-    type_ref='AdmOrg',
+    identifier=adm_name,
+    name=adm_name,
+    user_key=adm_name,
+    type_ref=adm_name,
     date_from='1900-01-01',
     date_to=None,
     parent_ref=None
@@ -46,19 +49,51 @@ sd = sd_importer.SdImport(
     MUNICIPALTY_CODE,
     import_date_from=GLOBAL_GET_DATE,
     org_only=True,
-    org_id_prefix='test'
+    org_id_prefix='adm_'
 )
 
 sd.create_ou_tree(
     create_orphan_container=False,
-    # sub_tree='6d8f88af-b93a-7455-9b4a-970e2dafbf7c',
-    super_unit = 'AdmOrg'
+    # 'Direktionen encoded with org prefix 'adm_'
+    sub_tree='fff9e2a6-d670-b656-c719-994eeac03a74',
+    super_unit = adm_name
 )
 
-# sd.create_ou_tree(
-#   create_orphan_container=True,
-#    sub_tree=None
-#)
+
+# Import of Lønorganisation
+løn_name = 'LønOrg'
+
+
+importer.add_klasse(
+    identifier=løn_name,
+    facet_type_ref='org_unit_type',
+    user_key=løn_name,
+    scope='TEXT',
+    title=løn_name
+)
+
+importer.add_organisation_unit(
+    identifier=løn_name,
+    name=løn_name,
+    user_key=løn_name,
+    type_ref=løn_name,
+    date_from='1900-01-01',
+    date_to=None,
+    parent_ref=None
+)
+
+sd = sd_importer.SdImport(
+    importer,
+    MUNICIPALTY_NAME,
+    MUNICIPALTY_CODE,
+    import_date_from=GLOBAL_GET_DATE,
+    org_only=True,
+    org_id_prefix=None
+)
+
+sd.create_ou_tree(
+    create_orphan_container=False,
+    super_unit = løn_name
+)
 
 importer.import_all()
-
