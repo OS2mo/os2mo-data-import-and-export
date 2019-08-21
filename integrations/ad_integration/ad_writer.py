56e20ad911a5075d98be4e6671e1943dd20a1ac3
Robert Jensen <jensen.robert@gmail.com>
2019-08-21 15:23:27 +0200

    Started a more generalized write function.

--- integrations/ad_integration/ad_writer.py
+++ integrations/ad_integration/ad_writer.py
@@ -1,36 +1,58 @@
-import time
-import json
-import pickle
 import logging
-import hashlib
 from ad_common import AD
 # import read_ad_conf_settings
-from pathlib import Path
 
-logger = logging.getLogger("AdReader")
+logger = logging.getLogger("AdWriter")
+
+
+def remove_redundant(text):
+    text = text.replace('\n', '')
+    text = text.replace('\r', '')
+    while text.find('  ') > -1:
+        text = text.replace('  ', ' ')
+    return text
 
 
 class ADWriter(AD):
     def __init__(self):
         super().__init__()
-    
 
+    def create_user(self, name, mo_uuid, cpr):
+        """
+        Create an AD user
+        :param name: Tuple with (givenname, surname)
+        """
+        school = False  # TODO
 
-    def create_user(self):
-        school = False # TODO
-
-        create_user_template = 'New-ADUser -Name "TestMO005" -SamAccountName "TestMO005" -OtherAttributes @{"extensionattribute1"="1111110101";"hkstsuuid"="5826074e-66c3-4100-8a00-000001510001"} '
         settings = self._get_setting(school)
         server = ''
         if settings['server']:
             server = ' -Server {} '.format(settings['server'])
 
         path = ' -Path "{}" '.format(settings['search_base'])
-        
+
         credentials = ' -Credential $usercredential'
+
+        create_user_template = """
+        New-ADUser
+        -Name "{} {}"
+        -GivenName "{}"
+        -Surname "{}"
+        -SamAccountName "AseAsesen2"
+        -OtherAttributes @{{"extensionattribute1"="{}";"hkstsuuid"="{}"}}"""
+        create_user_string = create_user_template.format(
+            name[0],
+            name[1],
+            name[0],
+            name[1],
+            cpr,
+            mo_uuid
+        )
+        create_user_string = remove_redundant(create_user_string)
+
         ps_script = (
             self._build_user_credential(school) +
-            create_user_template + 
+            create_user_string +
             server +
             path +
             credentials
@@ -40,8 +62,41 @@ class ADWriter(AD):
         print()
         print(response)
 
+    def create_user_test(self):
+        school = False  # TODO
+
+        create_user_template = (
+            'New-ADUser -Name "TestMO005" -SamAccountName "TestMO005" ' +
+            '-OtherAttributes @{"extensionattribute1"="1111110101";' +
+            '"hkstsuuid"="5826074e-66c3-4100-8a00-000001510001"} '
+        )
+        settings = self._get_setting(school)
+        server = ''
+        if settings['server']:
+            server = ' -Server {} '.format(settings['server'])
+
+        path = ' -Path "{}" '.format(settings['search_base'])
+
+        credentials = ' -Credential $usercredential'
+        ps_script = (
+            self._build_user_credential(school) +
+            create_user_template +
+            server +
+            path +
+            credentials
+        )
+        print(ps_script)
+        # response = self._run_ps_script(ps_script)
+        # print()
+        # print(response)
+
+
 if __name__ == '__main__':
     ad_writer = ADWriter()
 
-    ad_writer.create_user()
-
+    # ad_writer.create_user_test()
+    ad_writer.create_user(
+        name=('Daw', 'Dawsen'),
+        mo_uuid='5826074e-66c3-4100-8a00-000001510001',
+        cpr='1111110101'
+    )
