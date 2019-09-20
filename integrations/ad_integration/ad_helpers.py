48b175277e9f609902635ee43a6803035787e0ab
Robert Jensen <jensen.robert@gmail.com>
2019-09-20 14:05:24 +0200

    Started a class of service functions to help manage the integration.

---
+++ integrations/ad_integration/ad_helpers.py
@@ -0,0 +1,45 @@
+import os
+
+import ad_logger
+
+from os2mo_helpers.mora_helpers import MoraHelper
+
+
+MORA_BASE = os.environ.get('MORA_BASE')
+
+
+def find_mo_engagements_without_ad(helper):
+    """"
+    Find a list of all current MO engagements that do not have a valid AD account
+    registred as an IT-system.
+    """
+    org = helper.read_organisation()
+    query = 'o/{}/e?limit=1000000000'
+    employees = helper._mo_lookup(org, query)
+
+    active_users = set()
+    missing_in_ad = set()
+    i = 0
+    for employee in employees['items']:
+        i += 1
+        print('{}/{}'.format(i, len(employees['items'])))
+        uuid = employee['uuid']
+        engagements = helper.read_user_engagement(uuid)
+        if engagements:
+            active_users.add(uuid)
+
+    for employee in active_users:
+        ad_user = helper.get_e_username(employee, 'Active Directory')
+        # Notice: mora_helpers returns an empty string (not None) if no
+        # account is found.
+        if ad_user == '':
+            missing_in_ad.add(employee)
+    return missing_in_ad
+
+
+if __name__ == '__main__':
+    ad_logger.start_logging('ad_helpers.log')
+    helper = MoraHelper(hostname=MORA_BASE, use_cache=False)
+
+    missing_in_ad = find_mo_engagements_without_ad(helper)
+    print(len(missing_in_ad))
