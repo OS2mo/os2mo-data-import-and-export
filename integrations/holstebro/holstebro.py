9475cadbc27e60638baca0a7a44ec139ddffdd17
Robert Jensen <jensen.robert@gmail.com>
2019-10-28 14:41:23 +0100

    Allow Holstebro to use the new cpr<->uuid functionality

--- integrations/holstebro/holstebro.py
+++ integrations/holstebro/holstebro.py
@@ -2,14 +2,20 @@ import json
 import pathlib
 import datetime
 
+from integrations import cpr_mapper
 from os2mo_data_import import ImportHelper
 from integrations.SD_Lon import sd_importer
-from integrations.ad_integrations import ad_reader
+from integrations.ad_integration import ad_reader
 
 cfg_file = pathlib.Path.cwd() / 'settings' / 'kommune-holstebro.json'
 if not cfg_file.is_file():
     raise Exception('No setting file')
 settings = json.loads(cfg_file.read_text())
+
+cpr_map = pathlib.Path.cwd() / 'integrations' / 'holstebro' / 'cpr_mo_ad_map.csv'
+if not cpr_map.is_file():
+    raise Exception('No mapping file')
+employee_mapping = cpr_mapper.employee_mapper(str(cpr_map))
 
 GLOBAL_GET_DATE = datetime.datetime(2019, 10, 1, 0, 0)
 
@@ -17,8 +23,6 @@ importer = ImportHelper(
     create_defaults=True,
     mox_base=settings['mox.base'],
     mora_base=settings['mora.base'],
-    # system_name='SD-Import',
-    # end_marker='SDSTOP',
     store_integration_data=False,
     seperate_names=True
 )
@@ -29,7 +33,8 @@ sd = sd_importer.SdImport(
     importer,
     settings=settings,
     import_date_from=GLOBAL_GET_DATE,
-    ad_info=ad_info_reader
+    ad_info=ad_info_reader,
+    employee_mapping=employee_mapping
 )
 
 importer.add_klasse(identifier='IT-Org. Alias',
