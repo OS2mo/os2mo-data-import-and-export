385f91ec8ef5b13b0b4248f1876b892e683bd16f
Robert Jensen <jensen.robert@gmail.com>
2019-11-05 12:34:42 +0100

    Added a bunch of code usefull for trouble-shooting and fixing specifics

--- integrations/SD_Lon/sd_fix_departments.py
+++ integrations/SD_Lon/sd_fix_departments.py
@@ -44,7 +44,7 @@ class FixDepartmentsSD(object):
         logger.info('Read org_unit types')
         self.unit_types = self.helper.read_classes_in_facet('org_unit_type')[0]
 
-    def _create_department(self, department, activation_date):
+    def create_department(self, department, activation_date):
         params = {
             'ActivationDate': activation_date,
             'DeactivationDate': activation_date,
@@ -78,13 +78,103 @@ class FixDepartmentsSD(object):
         )
         logger.debug('Response: {}'.format(response.text))
 
-    def fix_departments(self):
+    def create_single_department(self, shortname, date):
+        """ Create a single department at a single snapshot in time """
+        activation_date = date.strftime('%d.%m.%Y')
+        params = {
+            'ActivationDate': activation_date,
+            'DeactivationDate': activation_date,
+            'DepartmentIdentifier': shortname,
+            'ContactInformationIndicator': 'true',
+            'DepartmentNameIndicator': 'true',
+            'PostalAddressIndicator': 'false',
+            'ProductionUnitIndicator': 'false',
+            'UUIDIndicator': 'true',
+            'EmploymentDepartmentIndicator': 'false'
+        }
+        department_info = sd_lookup('GetDepartment20111201', params)
+        department = department_info['Department']
+        parent = self.get_parent(department['DepartmentUUIDIdentifier'], date)
+
+        for unit_type in self.unit_types:
+            if unit_type['user_key'] == department['DepartmentLevelIdentifier']:
+                unit_type_uuid = unit_type['uuid']
+
+        payload = sd_payloads.create_single_org_unit(
+            department=department,
+            unit_type=unit_type_uuid,
+            parent=parent
+        )
+        logger.debug('Create department payload: {}'.format(payload))
+        print(payload)
+        response = self.helper._mo_post('ou/create', payload)
+        response.raise_for_status()
+        logger.info('Created unit {}'.format(
+            department['DepartmentIdentifier'])
+        )
+        logger.debug('Response: {}'.format(response.text))
+
+    def fix_specific_department(self, shortname):
+        # Semi-arbitrary start date for historic import
+        from_date = datetime.datetime(2010, 1, 1, 0, 0)
+
         params = {
             'ActivationDate': self.from_date.strftime('%d.%m.%Y'),
             'DeactivationDate': '9999-12-31',
+            'DepartmentIdentifier': shortname,
+            'UUIDIndicator': 'true',
+            'DepartmentNameIndicator': 'true'
+        }
+        # TODO: We need to read this without caching!
+        department = sd_lookup('GetDepartment20111201', params)
+        validities = department['Department']
+        if isinstance(validities, dict):
+            validities = [validities]
+
+        for validity in validities:
+            assert shortname == validity['DepartmentIdentifier']
+            validity_date = datetime.datetime.strptime(validity['ActivationDate'],
+                                                       '%Y-%m-%d')
+            user_key = shortname
+            name = validity['DepartmentName']
+            unit_uuid = validity['DepartmentUUIDIdentifier']
+
+            for unit_type in self.unit_types:
+                if unit_type['user_key'] == validity['DepartmentLevelIdentifier']:
+                    unit_type_uuid = unit_type['uuid']
+
+            from_date = validity_date.strftime('%Y-%m-%d')
+            parent = self.get_parent(unit_uuid, validity_date)
+            payload = sd_payloads.edit_org_unit(user_key, name, unit_uuid, parent,
+                                                unit_type_uuid, from_date)
+            response = self.helper._mo_post('details/edit', payload)
+            if response.status_code == 400:
+                assert(response.text.find('raise to a new registration') > 0)
+            else:
+                response.raise_for_status()
+            logger.debug('Edit payload: {}'.format(payload))
+            logger.info('Edit unit {}'.format(user_key))
+            logger.debug('Response: {}'.format(response.text))
+
+    def get_parent(self, unit_uuid, validity_date):
+        params = {
+            'EffectiveDate': validity_date.strftime('%d.%m.%Y'),
+            'DepartmentUUIDIdentifier': unit_uuid
+        }
+        parent_response = sd_lookup('GetDepartmentParent20190701', params)
+        # NOTICE: THIS WILL FAIL FOR ROOT UNITS
+        parent = parent_response['DepartmentParent']['DepartmentUUIDIdentifier']
+        return parent
+
+    def fix_departments(self):
+        params = {
+            # 'ActivationDate': self.from_date.strftime('%d.%m.%Y'),
+            # 'DeactivationDate': '31.12.9999',
+            'ActivationDate': '01.01.2018',
+            'DeactivationDate': '01.01.2018',
             'UUIDIndicator': 'true'
         }
-        #TODO: We need to read this without caching!
+        # TODO: We need to read this without caching!
         organisation = sd_lookup('GetOrganization20111201', params)
         department_lists = organisation['Organization']
         if not isinstance(department_lists, list):
@@ -103,6 +193,9 @@ class FixDepartmentsSD(object):
 
             departments = department_list['DepartmentReference']
             for department in departments:
+                # print(department)
+                if str(department).find('5SE') == -1:
+                    continue
                 checked += 1
                 print('{}/{} {:.2f}%'.format(checked, total, 100.0 * checked/total))
                 departments = []
@@ -111,6 +204,7 @@ class FixDepartmentsSD(object):
                 logger.debug('Check for {}'.format(uuid))
 
                 if 'status' in ou:  # Unit does not exist
+                    print('klaf')
                     departments.append(department)
                     logger.info('{} is new in MO'.format(uuid))
                     parent_department = department
@@ -125,11 +219,34 @@ class FixDepartmentsSD(object):
 
                 # Create actual departments
                 while departments:
-                    self._create_department(departments.pop(), activation_date)
+                    self.create_department(departments.pop(), activation_date)
 
 
 if __name__ == '__main__':
     from_date = datetime.datetime(2019, 1, 1, 0, 0)
 
     unit_fixer = FixDepartmentsSD(from_date)
-    unit_fixer.fix_departments()
+
+    unit_fixer.fix_specific_department('5SE')
+
+    # from_date = datetime.datetime(2019, 10, 1, 0, 0)
+    # unit_fixer.create_single_department('5SE', from_date)
+
+    # unit_fixer.fix_specific_department('0P84')
+    # unit_fixer.fix_specific_department('4OS')
+    # unit_fixer.fix_specific_department('6JI')
+    # unit_fixer.fix_specific_department('8AR')
+    # unit_fixer.fix_specific_department('9ØP')
+    # unit_fixer.fix_specific_department('10V')
+
+    # unit_fixer.fix_departments()
+
+    # params = {
+    #     'ActivationDate': '01.08.2019',
+    #     'DeactivationDate': '01.08.2019',
+    #     'DepartmentIdentifier': '5SE',
+    #     'UUIDIndicator': 'true',
+    #     'DepartmentNameIndicator': 'true'
+    # }
+    # department = sd_lookup('GetDepartment20111201', params)
+    # unit_fixer.create_department(department['Department'], '01.08.2019')
