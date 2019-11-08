c3a0e134b0afa45004f8bef23fcaa57b41f2bd2b
Robert Jensen <jensen.robert@gmail.com>
2019-11-08 11:24:19 +0100

    More payloads for diff-importer.

--- integrations/opus/payloads.py
+++ integrations/opus/payloads.py
@@ -21,21 +21,74 @@ def edit_engagement(data, mo_engagement_uuid):
     return payload
 
 
-def create_engagement(org_unit, mo_person, job_function, engagement_type,
-                      job_id, engagement_info, validity):
-    try:
-        working_time = float(engagement_info['working_time'][0]['OccupationRate'])
-    except IndexError:
-        working_time = 0
+def create_engagement(employee, user_uuid, unit_uuid, job_function, engagement_type,
+                      validity):
     payload = {
         'type': 'engagement',
-        'org_unit': {'uuid': org_unit},
-        'person': {'uuid': mo_person['uuid']},
+        'org_unit': {'uuid': str(unit_uuid)},
+        'person': {'uuid': user_uuid},
         'job_function': {'uuid': job_function},
         'engagement_type': {'uuid': engagement_type},
-        'user_key': job_id,
-        'fraction': int(working_time * 1000000),
+        'user_key': employee['@id'],
         'validity': validity
+    }
+    return payload
+
+
+def create_org_unit(unit, unit_uuid, parent, unit_type, from_date):
+    payload = {
+        'uuid': unit_uuid,
+        'user_key': unit['shortName'],
+        'name': unit['longName'],
+        'parent': {
+            'uuid': parent
+        },
+        'org_unit_type': {'uuid': unit_type},
+        'validity': {
+            'from': from_date,
+            'to': None
+        }
+    }
+    return payload
+
+
+def edit_org_unit(unit, unit_uuid, parent, unit_type, from_date):
+    payload = {
+        'type': 'org_unit',
+        'data': {
+            'uuid': unit_uuid,
+            'user_key':  unit['shortName'],
+            'name': unit['longName'],
+            'parent': {
+                'uuid': parent
+            },
+            'org_unit_type': {
+                'uuid': unit_type
+            },
+            'validity': {
+                'from': from_date,
+                'to': None
+            }
+
+        }
+    }
+    return payload
+
+
+def terminate_engagement(uuid, terminate_date):
+    payload = {
+        'type': 'engagement',
+        'uuid': uuid,
+        'validity': {'to': terminate_date}
+    }
+    return payload
+
+
+def terminate_manager(uuid, terminate_date):
+    payload = {
+        'type': 'manager',
+        'uuid': uuid,
+        'validity': {'to': terminate_date}
     }
     return payload
 
@@ -44,11 +97,142 @@ def connect_it_system_to_user(username, it_system, person_uuid):
     payload = {
         'type': 'it',
         'user_key': username,
-        'it-system': {'uuid': it_system},
+        'itsystem': {'uuid': it_system},
         'person': {'uuid': person_uuid},
         'validity': {
-            'from': None,
+            'from': '1900-01-01',
             'to': None
         }
+    }
+    return payload
+
+
+def create_address(validity, address_type, value, unit_uuid=None, user_uuid=None):
+    if unit_uuid is None and user_uuid is None:
+        raise Exception('Either unit or user must be specified')
+    if unit_uuid and user_uuid:
+        raise Exception('Only a unit or a person can be specified')
+
+    payload = {
+        'type': 'address',
+        'value': value,
+        'address_type': address_type,
+        'validity': validity
+    }
+    if unit_uuid is not None:
+        payload['org_unit'] = {'uuid': unit_uuid}
+    if user_uuid is not None:
+        payload['person'] = {'uuid': user_uuid}
+    return payload
+
+
+def edit_address(data, mo_address_uuid):
+    payload = {
+        'type': 'address',
+        'uuid': mo_address_uuid,
+        'data': data
+    }
+    return payload
+
+
+def create_manager(unit, person, manager_type, level, responsibility, validity):
+    payload = {
+        'type': 'manager',
+        'org_unit': {
+            'uuid': unit
+        },
+        'person': {
+            'uuid': person
+        },
+        'manager_type': {
+            'uuid': manager_type
+        },
+        'manager_level': {
+            'uuid': level
+        },
+        'responsibility': [  # OPUS will never give more than one
+            {
+                'uuid': responsibility
+            }
+        ]
+    }
+    return payload
+
+
+def edit_manager(object_uuid, unit, person, manager_type, level, responsibility,
+                 validity):
+    payload = {
+        'type': 'manager',
+        'uuid': object_uuid,
+        'data': {
+            'org_unit': {
+                'uuid': unit
+            },
+            'person': {
+                'uuid': person
+            },
+            'manager_type': {
+                'uuid': manager_type
+            },
+            'manager_level': {
+                'uuid': level
+            },
+            'responsibility': [  # OPUS will never give more than one
+                {
+                    'uuid': responsibility
+                }
+            ],
+            'validity': validity
+        }
+    }
+    return payload
+
+
+# Exact same code is found in sd_payloads
+def profession(profession, org, job_function_facet):
+    validity = {
+        'from': '1900-01-01',
+        'to': 'infinity'
+    }
+
+    # "integrationsdata":  # TODO: Check this
+    properties = {
+        'brugervendtnoegle': profession,
+        'titel':  profession,
+        'omfang': 'TEXT',
+        "virkning": validity
+    }
+    attributter = {
+        'klasseegenskaber': [properties]
+    }
+    relationer = {
+        'ansvarlig': [
+            {
+                'objekttype': 'organisation',
+                'uuid': org,
+                'virkning': validity
+            }
+        ],
+        'facet': [
+            {
+                'objekttype': 'facet',
+                'uuid': job_function_facet,
+                'virkning': validity
+            }
+        ]
+    }
+    tilstande = {
+        'klassepubliceret': [
+            {
+                'publiceret': 'Publiceret',
+                'virkning': validity
+            }
+        ]
+    }
+
+    payload = {
+        "attributter": attributter,
+        "relationer": relationer,
+        "tilstande": tilstande
     }
     return payload
