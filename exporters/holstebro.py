8bfd318718bb7d6fc2d290a65ec95798e774e07d
Anders Sølbech Larsen <asl@holstebro.dk>
2019-11-25 13:35:06 +0100

    Lidt rettelser til planorama export format. Fikset csvwriter format så tomme linier undgaas

--- exporters/holstebro.py
+++ exporters/holstebro.py
@@ -1,22 +1,13 @@
-#
-# Copyright (c) 2017-2018, Magenta ApS
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
 """
-Make a number of pre-defined queries into MO.
-In the abcense of known information about the dataset, the deepest root
-tree is assumed to be the one to be exported.
-These are for the general case, specific version for each municipality
-is typically a better solution.
-
-Script requires environment variables to be set. 
-To get SAML_TOKEN you have to login to the OS2MO server (MORA_BASE) that is being used 
-and subsequently access <server>/saml/api-token 
+Holstebro Kommune specific queries into MO. 
 """
+from os2mo_helpers.mora_helpers import MoraHelper
+import common_queries as cq
 import os
 import queue
 import threading
@@ -25,32 +16,37 @@ import time
 import click
 from anytree import PreOrderIter
 
-import common_queries as cq
-from os2mo_helpers.mora_helpers import MoraHelper
-
-MORA_BASE = os.environ.get('MORA_BASE')
-MO_ROOT_UUID = os.environ.get('MO_ROOT_UUID')
-SAML_TOKEN = os.environ.get('SAML_TOKEN')
+# http:://os2mo-test.holstebro.dk
+#itdig_uuid = '9f981b4d-66c3-4100-b800-000001480001'
 
 
-def export_from_mo(root, threaded_speedup, hostname):
+@click.command()
+@click.option('--root', default=None, help='uuid of the root to be exported.')
+@click.option('--threaded-speedup', default=False, help='Run in multithreaded mode')
+@click.option('--hostname', envvar='MORA_BASE', default=None, required=True, help='MO hostname')
+@click.option('--api_token', envvar='SAML_TOKEN', default=None, required=True, help='SAML API Token')
+def export_from_mo(root, threaded_speedup, hostname, api_token):
     threaded_speedup = threaded_speedup
     t = time.time()
+
+    if api_token is None:
+        raise NameError('Ugyldigt argument')
 
     mh = MoraHelper(hostname=hostname, export_ansi=False)
 
     org = mh.read_organisation()
-    roots = mh.read_top_units(org)
 
-    itdig_uuid = '9f981b4d-66c3-4100-b800-000001480001'
+    # find Holstebro Kommune root uuid, if no uuid is specified
+    if root is None:
+        roots = mh.read_top_units(org)
+        for root in roots:
+            if root['name'] == 'Holstebro Kommune':
+                holstebro_uuid = root['uuid']
+    else:
+        holstebro_uuid = root
 
-    # print(roots)
-    for root in roots:
-        if root['name'] == 'Holstebro Kommune':
-            holstebro_uuid = root['uuid']
+    nodes = mh.read_ou_tree(holstebro_uuid)
 
-    #nodes = mh.read_ou_tree(holstebro_uuid)
-    nodes = mh.read_ou_tree(itdig_uuid)
     print('Read nodes: {}s'.format(time.time() - t))
 
     if threaded_speedup:
@@ -62,30 +58,6 @@ def export_from_mo(root, threaded_speedup, hostname):
     export_to_planorama(mh, nodes, filename_org, filename_persons)
     print('planorama_org.csv: {}s'.format(time.time() - t))
 
-    """
-    filename = 'org_incl-medarbejdere.csv'
-    cq.export_orgs(mh, nodes, filename)
-    print('org_incl-medarbejdere.csv: {}s'.format(time.time() - t))
-    
-    filename = 'alle_lederfunktioner_os2mo.csv'
-    cq.export_managers(mh, nodes, filename)
-    print('Alle ledere: {}s'.format(time.time() - t))
-    
-
-    filename = 'alle-medarbejdere-stilling-email_os2mo.csv'
-    cq.export_all_employees(mh, nodes, filename)
-    print('alle-medarbejdere-stilling-email_os2mo.csv: {}s'.format(time.time() - t))
-
-    
-    filename = 'adm-org-incl-start-og-stopdata-og-enhedstyper-os2mo.csv'
-    cq.export_adm_org(mh, nodes, filename)
-    print('adm-org-incl-start-stop: {}s'.format(time.time() - t))
-
-    filename = 'tilknytninger.csv'
-    cq.export_all_teams(mh, nodes, filename)
-    print('tilknytninger: {}s'.format(time.time() - t))
-    """
-
 
 def export_to_planorama(mh, nodes, filename_org, filename_persons):
     """ Traverses a tree of OUs, for each OU finds the manager of the OU.
@@ -93,7 +65,7 @@ def export_to_planorama(mh, nodes, filename_org, filename_persons):
     """
     fieldnames_persons = ['UUID', 'Username', 'Password', 'Name', 'Title',
                           'Address', 'Zip', 'Country', 'CPR', 'Email',
-                          'Number', 'Mobile', 'Telephone', 'Responsible', 'Company']
+                          'Number', 'Mobile', 'Telephone', 'Responsible', 'Responsible_UUID', 'Company']
     fieldnames_org = ['Root', 'Number', 'Name']
 
     rows_org = []
@@ -114,17 +86,19 @@ def export_to_planorama(mh, nodes, filename_org, filename_persons):
             employees = mh.read_organisation_people(node.name, 'engagement', False)
             # Does this node have a new name?
             manager = find_org_manager(mh, node)
+            manager_engagement = mh.read_user_engagement(manager['uuid'])
 
             for uuid, employee in employees.items():
                 row = {}
                 address = mh.read_user_address(uuid, username=True, cpr=True)
+                #manager = mh.read_engagement_manager(employee['Engagement UUID'])
                 # row.update(address)  # E-mail, Telefon, Brugernavn, CPR NR
                 # row.update(employee)  # Everything else
                 row = {'UUID': uuid,
                        'Username': employee['User Key'],
                        'Password': '',
                        'Name': employee['Navn'],
-                       'Title': employee['Stillingsbetegnelse'],
+                       'Title': employee['Stillingsbetegnelse'][0] if len(employee['Stillingsbetegnelse']) > 0 else '',
                        'Address': address['Lokation'] if 'Lokation' in address else '',
                        'Zip': '',
                        'Country': '',
@@ -134,7 +108,8 @@ def export_to_planorama(mh, nodes, filename_org, filename_persons):
                        'Number': '',
                        'Mobile': address['Mobiltelefon'] if 'Mobiltelefon' in address else '',
                        'Telephone': address['Telefon'] if 'Telefon' in address else '',
-                       'Responsible': manager['uuid'],
+                       'Responsible': manager_engagement[0]['user_key'],
+                       'Responsible_UUID': manager['uuid'],
                        'Company': employee['Org-enhed UUID']
                        }
 
@@ -146,11 +121,11 @@ def export_to_planorama(mh, nodes, filename_org, filename_persons):
 
 def find_org_manager(mh, node):
 
-    new_manager = mh.read_organisation_managers(node.name, True)
+    new_manager = mh.read_ou_manager(node.name, True)
     if new_manager:
         return new_manager
     elif node.depth != 0:
-        return mh.read_organisation_managers(node.parent.name, True)
+        return find_org_manager(node.parent, True)
     else:
         return {'uuid': ''}
 
@@ -174,4 +149,4 @@ def export_planorama_org(mh, nodes, filename):
 
 
 if __name__ == '__main__':
-    export_from_mo(MO_ROOT_UUID, False, MORA_BASE)
+    export_from_mo()
