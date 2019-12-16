442a7796b1294591c82ab93e9ce79ac2c7393fe8
Christoffer Moesgaard <christoffer@magenta.dk>
2019-12-16 14:48:52 +0100

    32759: Add support for deploying on subpath through script_name parameter
    Reformatted code

--- tools/sd_tool/app.py
+++ tools/sd_tool/app.py
@@ -1,24 +1,25 @@
-import logging
-import subprocess
-import uuid
+import os
 
 import flask
+import logging
+import subprocess
 import toml
+import uuid
 
 app = flask.Flask(__name__)
 
 logger = logging.getLogger(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template("index.html")
 
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def submit():
-    args = flask.request.form.get('uuid')
-    split_args = args.split(' ')
+    args = flask.request.form.get("uuid")
+    split_args = args.split(" ")
 
     if len(split_args) != 1:
         return "Parameter must be a single UUID"
@@ -28,7 +29,7 @@ def submit():
     except ValueError:
         return "UUID is malformed"
 
-    script = app.config.get('script').split(' ')
+    script = app.config.get("script").split(" ")
     script.append(str(uuid_arg))
 
     try:
@@ -38,15 +39,13 @@ def submit():
         logger.exception("Script error occurred")
         return "Script error occurred"
 
-    return flask.render_template('index.html')
+    return flask.render_template("index.html")
 
 
 def create_app():
-    with open('config.toml') as f:
+    with open("config.toml") as f:
         config = toml.loads(f.read())
         app.config.update(config)
+        os.environ["SCRIPT_NAME"] = config.get("script_name")
 
     return app
-
-
-
