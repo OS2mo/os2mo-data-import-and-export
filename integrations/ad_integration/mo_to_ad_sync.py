656d6a45890a8c877733db8cf93baeab8bfe3410
Robert Jensen <jensen.robert@gmail.com>
2020-01-30 14:14:20 +0100

    Read settings from settings.json, much improved error handling.

--- integrations/ad_integration/mo_to_ad_sync.py
+++ integrations/ad_integration/mo_to_ad_sync.py
@@ -1,5 +1,7 @@
 import os
+import json
 import logging
+import pathlib
 
 import ad_reader
 import ad_writer
@@ -12,21 +14,38 @@ logger = logging.getLogger('MoAdSync')
 
 
 def main():
+    cfg_file = pathlib.Path.cwd() / 'settings' / 'settings.json'
+    if not cfg_file.is_file():
+        raise Exception('No setting file')
+    settings = json.loads(cfg_file.read_text())
+
+    mo_uuid_field = settings['integrations.ad.write.uuid_field']
+
     ad_logger.start_logging(LOG_FILE)
     reader = ad_reader.ADParameterReader()
     writer = ad_writer.ADWriter()
 
     all_users = reader.read_it_all()
+
     for user in all_users:
+        if mo_uuid_field not in user:
+            msg = 'User does not have a {} field - skipping'
+            logger.info(msg.format(user['SamAccountName'], mo_uuid_field))
+            continue
         msg = 'Now syncing: {}, {}'.format(user['SamAccountName'],
-                                           user[MO_UUID_FIELD])
+                                           user[mo_uuid_field])
         logger.info(msg)
+        print(msg)
         try:
-            response = writer.sync_user(user[MO_UUID_FIELD], user)
+            response = writer.sync_user(user[mo_uuid_field], user)
             logger.debug('Respose to sync: {}'.format(response))
         except ad_exceptions.ManagerNotUniqueFromCprException:
-            msg = 'Did ot find a unique manager for {}'.format(user[MO_UUID_FIELD])
+            msg = 'Did ot find a unique manager for {}'.format(user[mo_uuid_field])
             logger.error(msg)
+        except ad_exceptions.UserNotFoundException:
+            msg = 'User {}, {} with uuid {} was not found i MO, unable to sync'
+            logger.error(msg.format(user['SamAccountName'], user['Name'],
+                                    user[mo_uuid_field]))
         except Exception as e:
             logger.error('Unhandled exception: {}'.format(e))
             logger.exception("Unhandled exception:")
