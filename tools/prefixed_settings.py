84742d8b04d7e510fc496d28c699ae2d5fb4d3fc
jgj-magenta-aps <jgj@magenta-aps.dk>
2020-02-05 14:37:52 +0100

    flake8

--- tools/prefixed_settings.py
+++ tools/prefixed_settings.py
@@ -1,4 +1,5 @@
-#copyright (c) 2017-2018, Magenta ApS
+#
+# Copyright (c) 2017-2018, Magenta ApS
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,26 +14,27 @@ import pathlib
 
 def extract_prefixed_envvars(settings, prefix):
     if not prefix.endswith("."):
-        prefix +="."
-    environment=[]
-    prefixlen=len(prefix)
-    for k,v in settings.items():
+        prefix += "."
+    environment = []
+    prefixlen = len(prefix)
+    for k, v in settings.items():
         if (
             k.startswith(prefix) and k.count(".") == prefix.count(".")
         ):
-            if isinstance(v,str):
-                environment.append("export %s=\"%s\"" %(k[prefixlen:],v))
-            elif isinstance(v,bool):
-                environment.append("export %s=%s" %(k[prefixlen:],str(v).lower()))
-            elif isinstance(v,float):
-                environment.append("export %s=%g" %(k[prefixlen:],v))
-            elif isinstance(v,int):
-                environment.append("export %s=%d" %(k[prefixlen:],v))
+            if isinstance(v, str):
+                environment.append("export %s=\"%s\"" % (k[prefixlen:], v))
+            elif isinstance(v, bool):
+                environment.append("export %s=%s" % (k[prefixlen:], str(v).lower()))
+            elif isinstance(v, float):
+                environment.append("export %s=%g" % (k[prefixlen:], v))
+            elif isinstance(v, int):
+                environment.append("export %s=%d" % (k[prefixlen:], v))
     return environment
+
 
 if __name__ == '__main__':
     customer_settings_path = pathlib.Path(os.environ["CUSTOMER_SETTINGS"])
     prefix = os.environ["SETTING_PREFIX"]
     settings = json.loads(customer_settings_path.read_text())
     environment = extract_prefixed_envvars(settings, prefix)
-    print ("\n".join(environment))
+    print("\n".join(environment))
--- tools/tests/test_prefixed_settings.py
+++ tools/tests/test_prefixed_settings.py
@@ -1,8 +1,9 @@
 import unittest
-
 from tools import prefixed_settings
+
+
 class Tests(unittest.TestCase):
-    
+
     def test_prefixed_settings_with_dot(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
             {
@@ -11,43 +12,43 @@ class Tests(unittest.TestCase):
                 "manuel.CYKEL": "Min",
                 "manuella": "Theirs"
             }, "manuel.")), [
-                'export BIL="Din"', 
+                'export BIL="Din"',
                 'export CYKEL="Min"'
             ])
 
     def test_prefixed_settings_no_dot(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
             {
-                "manuel.børnehave.CYKEL":"Børnehavens",
+                "manuel.børnehave.CYKEL": "Børnehavens",
                 "manuel.CYKEL": "Min",
                 "manuel.BIL": "Din",
                 "manuella": "Theirs"
             }, "manuel")), [
-                'export BIL="Din"', 
+                'export BIL="Din"',
                 'export CYKEL="Min"',
             ])
 
     def test_prefixed_settings_int(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
-            { "manuel.torsk": 0, }, "manuel")),
+            {"manuel.torsk": 0}, "manuel")),
             ['export torsk=0']
         )
 
     def test_prefixed_settings_float(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
-            { "manuel.torsk": 0.1, }, "manuel")),
+            {"manuel.torsk": 0.1}, "manuel")),
             ['export torsk=0.1']
         )
 
     def test_prefixed_settings_boolean(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
-            { "manuel.torsk": False, }, "manuel")),
+            {"manuel.torsk": False}, "manuel")),
             ['export torsk=false']
         )
- 
+
     def test_prefixed_settings_more_dots(self):
         self.assertEqual(sorted(prefixed_settings.extract_prefixed_envvars(
             {
-                "manuel.børnehave.CYKEL":"Børnehavens",
-                "manuel.BIL":"Din",
+                "manuel.børnehave.CYKEL": "Børnehavens",
+                "manuel.BIL": "Din",
             }, "manuel.børnehave")), ['export CYKEL="Børnehavens"'])
