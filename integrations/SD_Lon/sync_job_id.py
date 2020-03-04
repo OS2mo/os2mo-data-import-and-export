a7fe9cd5903ef6db147c7cd722b98569b0b7a2b8
Robert Jensen <jensen.robert@gmail.com>
2020-03-04 11:09:55 +0100

    Generalized the tool to accept both engagement_types and job_functions.

--- integrations/SD_Lon/sync_job_id.py
+++ integrations/SD_Lon/sync_job_id.py
@@ -1,5 +1,7 @@
 import json
+import atexit
 import pathlib
+import logging
 import argparse
 import requests
 import sd_payloads
@@ -7,38 +9,94 @@ from os2mo_helpers.mora_helpers import MoraHelper
 from integrations.SD_Lon.sd_common import sd_lookup
 from integrations.SD_Lon.sd_common import mora_assert
 
-# TODO: We now need to sync both engagement types and job_functions!
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = 'sync_job_id.log'
+
+logger = logging.getLogger('sdSyncJobId')
+
+for name in logging.root.manager.loggerDict:
+    if name in ('sdSyncJobId', 'sdCommon', 'mora-helper'):
+        logging.getLogger(name).setLevel(LOG_LEVEL)
+    else:
+        logging.getLogger(name).setLevel(logging.ERROR)
+
+logging.basicConfig(
+    format='%(levelname)s %(asctime)s %(name)s %(message)s',
+    level=LOG_LEVEL,
+    filename=LOG_FILE
+)
+
 
 class JobIdSync(object):
     def __init__(self):
+        logger.info('Start sync')
+        atexit.register(self.at_exit)
         cfg_file = pathlib.Path.cwd() / 'settings' / 'settings.json'
         if not cfg_file.is_file():
             raise Exception('No setting file')
         self.settings = json.loads(cfg_file.read_text())
 
-        helper = MoraHelper(hostname=self.settings['mora.base'],
-                            use_cache=False)
+        helper = MoraHelper(hostname=self.settings['mora.base'], use_cache=False)
         self.engagement_types = helper.read_classes_in_facet('engagement_type')
 
+        if self.settings[
+                'integrations.SD_Lon.job_function'] == 'JobPositionIdentifier':
+            logger.info('Read settings. Update job_functions and engagment types')
+            self.job_function_types = helper.read_classes_in_facet(
+                'engagement_job_function')
+            self.update_job_functions = True
+        else:
+            logger.info('Read settings. Do not update job_functions')
+            self.update_job_functions = False
+
+    def at_exit(self):
+        logger.info('*Sync ended*')
+
     def _find_engagement_type(self, job_pos_id):
+        """
+        Find a Klasse in facet engagement_type corresponding to job_pos_id in LoRa.
+        The search is performed both by direct search as well as with prefixed
+        string engagement_type (which is used in the municipalities who also use
+        the job_pos_ids as job_functions).
+        """
+        logger.info('Search MO for engagment_type {}'.format(job_pos_id))
+        found_type = None
+        user_keys = [str(job_pos_id), 'engagement_type' + str(job_pos_id)]
+        for engagement_type in self.engagement_types[0]:
+            if engagement_type['user_key'] in user_keys:
+                found_type = engagement_type
+        logger.info('Found {}'.format(found_type))
+        return found_type
+
+    def _find_job_function_type(self, job_pos_id):
         """
         Find the Klasse corresponding to job_pos_id in LoRa.
         """
         found_type = None
-        for engagement_type in self.engagement_types[0]:
-            if engagement_type['user_key'] == str(job_pos_id):
-                found_type = engagement_type
+        if not self.update_job_functions:
+            logger.info('Job functons not enabled in settings')
+            return found_type
+
+        logger.info('Search MO for job_function_type {}'.format(job_pos_id))
+        # Currently we do not use a prefix anywhere, list has only one element
+        user_keys = [str(job_pos_id)]
+        for job_function_type in self.job_function_types[0]:
+            if job_function_type['user_key'] in user_keys:
+                found_type = job_function_type
+        logger.info('Found {}'.format(found_type))
         return found_type
 
-    def _edit_engagement_type(self, uuid, title):
+    def _edit_klasse_title(self, uuid, title):
         """
         Change the title of an existing LoRa engagement type.
         """
-        payload = sd_payloads.edit_engagement_type(title)
+        logger.info('Edit {} to {}'.format(uuid, title))
+        payload = sd_payloads.edit_klasse_title(title)
         response = requests.patch(
             url=self.settings['mox.base'] + '/klassifikation/klasse/' + uuid,
             json=payload
         )
+        logger.info('Lora response: {}'.format(response.status_code))
         mora_assert(response)
         return response
 
@@ -46,6 +104,7 @@ class JobIdSync(object):
         """
         Return the textual value of a Job Position Identifier fro SD.
         """
+        logger.info('Search SD for {}'.format(job_pos_id))
         params = {
             'JobPositionIdentifier': job_pos_id,
         }
@@ -54,6 +113,7 @@ class JobIdSync(object):
             job_pos = job_pos_response['Profession']['JobPositionName']
         else:
             job_pos = None
+        logger.info('Found {}'.format(job_pos_id))
         return job_pos
 
     def sync_to_sd(self, job_pos_id):
@@ -61,26 +121,62 @@ class JobIdSync(object):
         Sync the titel of LoRa engagement type to the value current
         registred at SD.
         """
-        job_pos = self.get_job_pos_id_from_sd(job_pos_id)
-        if job_pos is None:
-            return 'Job position not found i SD'
+        logger.info('Sync {} to value found in SD'.format(job_pos_id))
+        return_status = [None, None]
+        sd_job_pos_text = self.get_job_pos_id_from_sd(job_pos_id)
+        if sd_job_pos_text is None:
+            logger.warning('Job position {} not found i SD'.format(job_pos_id))
+            return return_status
 
-        mo_type = self._find_engagement_type(job_pos_id)
-        if mo_type is None:
-            return 'Job position not found i MO'
+        mo_eng_type = self._find_engagement_type(job_pos_id)
+        if mo_eng_type is None:
+            return_status[0] = False
+            logger.info('Engagement type {} not found i MO'.format(job_pos_id))
+        else:
+            return_status[0] = True
+            self._edit_klasse_title(mo_eng_type['uuid'], sd_job_pos_text)
+            logger.info('Updated engagement type: {}'.format(job_pos_id))
 
-        self._edit_engagement_type(mo_type['uuid'], job_pos)
-        return 'Job position updated'
+        # Only run this part, if we are actually using
+        if self.update_job_functions:
+            mo_job_function_type = self._find_job_function_type(job_pos_id)
+            if mo_job_function_type is None:
+                return_status[1] = False
+                logger.info('job function type {} not found i MO'.format(job_pos_id))
+            else:
+                self._edit_klasse_title(mo_job_function_type['uuid'],
+                                        sd_job_pos_text)
+                return_status[1] = True
+                logger.info('Updated job function type type: {}'.format(job_pos_id))
 
-    def sync_manually(self, job_pos_id, titel):
+        logger.info('Return status: {}'.format(return_status))
+        return return_status
+
+    def sync_manually(self, job_pos_id, title):
         """
         Manually update the titel of an engagement type.
         """
+        logger.info('Sync {} to {}'.format(job_pos_id, title))
+        return_status = [None, None]
+
         mo_type = self._find_engagement_type(job_pos_id)
         if mo_type is None:
-            return 'Job position not found i MO'
+            return_status[0] = False
+            logger.info('Job position not found i MO')
+        else:
+            return_status[0] = True
+            self._edit_klasse_titel(mo_type['uuid'], title)
 
-        self._edit_engagement_type(mo_type['uuid'], titel)
+        if self.update_job_functions:
+            mo_job_function_type = self._find_job_function_type(job_pos_id)
+            if mo_job_function_type is None:
+                return_status[1] = False
+                logger.info('job function type {} not found i MO'.format(job_pos_id))
+            else:
+                return_status[1] = True
+                self._edit_klasse_title(mo_job_function_type['uuid'], title)
+                logger.info('Updated job function type type: {}'.format(job_pos_id))
+
         return 'Job position updated'
 
     def _cli(self):
