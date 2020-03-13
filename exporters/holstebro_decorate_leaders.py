17cc3dc220f8fb860d8bf21e92490ff2a1456044
Anders Sølbech Larsen <asl@holstebro.dk>
2020-03-13 11:58:59 +0100

    intermediate commit

--- exporters/holstebro_decorate_leaders.py
+++ exporters/holstebro_decorate_leaders.py
@@ -1,11 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-"""
-Holstebro Kommune specific queries into MO.
-"""
-import json
 import os
 import queue
 import threading
@@ -28,10 +20,7 @@ from os2mo_helpers.mora_helpers import MoraHelper
 @click.option('--test', 'hostname', flag_value='https://os2mo-test.holstebro.dk', help='https://os2mo-test.holstebro.dk')
 @click.option('--prod', 'hostname', flag_value='https://os2mo.holstebro.dk', help='https://os2mo.holstebro.dk')
 @click.option('--api_token', envvar='SAML_TOKEN', default=None, required=True, help='SAML API Token')
-def export_from_mo(root, threaded_speedup, hostname, api_token):
-    threaded_speedup = threaded_speedup
-    t = time.time()
-
+def decorate_leaders(root, threaded_speedup, hostname, api_token):
     if api_token is None:
         raise NameError('Ugyldigt argument')
 
@@ -48,28 +37,14 @@ def export_from_mo(root, threaded_speedup, hostname, api_token):
     else:
         holstebro_uuid = root
 
-    itdig_uuid = '9f981b4d-66c3-4100-b800-000001480001'
-    okit_uuid = '470ce14c-66c3-4100-ba00-0000014b0001'
-    bufl_uuid = '4105e152-66c3-4100-8c00-0000014b0001'
-
     nodes = mh.read_ou_tree(holstebro_uuid)
-
-    print('Read nodes: {}s'.format(time.time() - t))
 
     if threaded_speedup:
         cq.pre_cache_users(mh)
         print('Build cache: {}'.format(time.time() - t))
 
-    #hh.update_org_with_hk_managers(mh, nodes)
-
-    filename_org = 'planorama_org.csv'
-    filename_persons = 'planorama_persons.csv'
-    hh.export_to_planorama(mh, nodes, filename_org, filename_persons)
-    print('planorama_org.csv: {}s'.format(time.time() - t))
-
-    cq.export_orgs(mh, nodes, "hk_organisation.csv", False)
-    print('hk_organisation.csv: {}s'.format(time.time() - t))
+    hh.update_org_with_hk_managers(mh, nodes)
 
 
 if __name__ == '__main__':
-    export_from_mo()
+    decorate_leaders()
