bef98dcd92a2d5da8385e8faa6aaaccd9bf0054d
jgj-magenta-aps <jgj@magenta-aps.dk>
2020-05-28 11:47:44 +0200

    WIP-35275-multiple-ad-instanser

--- integrations/ad_integration/test_connectivity.py
+++ integrations/ad_integration/test_connectivity.py
@@ -60,6 +60,7 @@ def test_basic_connectivity():
 
 def test_ad_contact(index):
     from ad_reader import ADParameterReader
+    ad_reader = ADParameterReader()
     try:
         ad_reader = ADParameterReader(index)
     except Exception as e:
