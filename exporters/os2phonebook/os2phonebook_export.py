b0666809d610d4124680f59d7faa1ba81588cefd
Emil Madsen <emil@magenta.dk>
2020-06-05 11:24:24 +0200

    Async HTTP calls, bulk queries

--- exporters/os2phonebook/os2phonebook_export.py
+++ exporters/os2phonebook/os2phonebook_export.py
@@ -4,6 +4,9 @@ import logging
 import pathlib
 import urllib.parse
 import requests
+import time
+import asyncio
+from aiohttp import ClientSession
 
 import click
 
@@ -85,14 +88,11 @@ def generate_json():
     session = Session()
 
     total_number_of_employees = session.query(Bruger).count()
-    print(total_number_of_employees)
+    print("Total employees:", total_number_of_employees)
 
-    def get_org_unit_address_references(enhed):
-        return address_adapter(session.query(Adresse).filter(Adresse.enhed_uuid == enhed.uuid).all())
-
-    def get_org_unit_engagement_references(enhed):
+    def get_org_unit_engagement_references(uuid):
         queryset = session.query(Engagement, Bruger).filter(
-            Engagement.enhed_uuid == enhed.uuid
+            Engagement.enhed_uuid == uuid
         ).filter(
             Engagement.bruger_uuid == Bruger.uuid
         ).all()
@@ -105,9 +105,9 @@ def generate_json():
             } for engagement, bruger in queryset
         ]
 
-    def get_org_unit_association_references(enhed):
+    def get_org_unit_association_references(uuid):
         queryset = session.query(Tilknytning, Bruger).filter(
-            Tilknytning.enhed_uuid == enhed.uuid
+            Tilknytning.enhed_uuid == uuid
         ).filter(
             Tilknytning.bruger_uuid == Bruger.uuid
         ).all()
@@ -121,9 +121,9 @@ def generate_json():
             for tilknytning, bruger in queryset
         ]
 
-    def get_org_unit_manager_references(enhed):
+    def get_org_unit_manager_references(uuid):
         queryset = session.query(Leder, Bruger).filter(
-            Leder.enhed_uuid == enhed.uuid
+            Leder.enhed_uuid == uuid
         ).filter(
             Leder.bruger_uuid == Bruger.uuid
         ).all()
@@ -138,46 +138,34 @@ def generate_json():
         ]
 
     org_unit_map = {}
-    def get_org_unit(enhed):
+    def add_org_unit(enhed):
         if enhed.uuid in org_unit_map:
             return
 
         unit = {
             "uuid": enhed.uuid,
             "name": enhed.navn,
+            "parent": enhed.forældreenhed_uuid,
+            "engagements": [],
+            "associations": [],
+            "management": [],
+            "addresses": {
+                "DAR": [],
+                "PHONE": [],
+                "EMAIL": [],
+                "EAN": [],
+                "PNUMBER": [],
+                "WWW": [],
+            }
         }
-        unit["parent"] = enhed.forældreenhed_uuid
         if unit["parent"]:
-            get_org_unit(
+            add_org_unit(
                 session.query(Enhed).filter(
                     Enhed.uuid == unit["parent"]
                 ).first()
             )
 
-        unit["addresses"] = get_org_unit_address_references(enhed)
-        unit["engagements"] = get_org_unit_engagement_references(enhed)
-        unit["associations"] = get_org_unit_association_references(enhed)
-        unit["management"] = get_org_unit_manager_references(enhed)
-
         org_unit_map[enhed.uuid] = unit
-
-    def get_employee_engagement_references(uuid):
-        queryset = session.query(Engagement, Enhed).filter(
-            Engagement.bruger_uuid == uuid
-        ).filter(
-            Engagement.enhed_uuid == Enhed.uuid
-        ).all()
-
-        for _, enhed in queryset:
-            get_org_unit(enhed)
-
-        return [
-            {
-                "title": engagement.stillingsbetegnelse_titel,
-                "name": enhed.navn,
-                "uuid": enhed.uuid,
-            } for engagement, enhed in queryset
-        ]
 
     def address_adapter(address_list):
         address_types = {
@@ -232,82 +220,229 @@ def generate_json():
 
         return address_types
 
-    def get_employee_address_references(uuid):
-        return address_adapter(session.query(Adresse).filter(Adresse.bruger_uuid == uuid).all())
+    def fetch_employees(employee_map):
+        for employee in session.query(Bruger).all():
+            phonebook_entry = {
+                "uuid": employee.uuid,
+                "surname": employee.efternavn,
+                "givenname": employee.fornavn,
+                "name": employee.fornavn + " " + employee.efternavn,
+                "engagements": [],
+                "associations": [],
+                "management": [],
+                "addresses": {
+                    "DAR": [],
+                    "PHONE": [],
+                    "EMAIL": [],
+                    "EAN": [],
+                    "PNUMBER": [],
+                    "WWW": [],
+                }
+            }
+            employee_map[employee.uuid] = phonebook_entry
+        return employee_map
 
-    def get_employee_association_references(uuid):
-        queryset = session.query(Tilknytning, Enhed).filter(
-            Tilknytning.bruger_uuid == uuid
-        ).filter(Tilknytning.enhed_uuid == Enhed.uuid).all()
+    def enrich_employees_with_engagements(employee_map):
+        # Enrich with engagements
+        queryset = session.query(Engagement, Enhed).filter(
+            Engagement.enhed_uuid == Enhed.uuid
+        ).all()
 
         for _, enhed in queryset:
-            get_org_unit(enhed)
+            add_org_unit(enhed)
 
-        return [
-            {
+        for engagement, enhed in queryset:
+            engagement_entry = {
+                "title": engagement.stillingsbetegnelse_titel,
+                "name": enhed.navn,
+                "uuid": enhed.uuid,
+            }
+            employee_map[engagement.bruger_uuid]["engagements"].append(engagement_entry)
+        return employee_map
+
+    def enrich_employees_with_associations(employee_map):
+        # Enrich with associations
+        queryset = session.query(Tilknytning, Enhed).filter(Tilknytning.enhed_uuid == Enhed.uuid).all()
+
+        for _, enhed in queryset:
+            add_org_unit(enhed)
+
+        for tilknytning, enhed in queryset:
+            tilknytning_entry = {
                 "title": tilknytning.tilknytningstype_titel,
                 "name": enhed.navn,
                 "uuid": enhed.uuid,
             }
-            for tilknytning, enhed in queryset
-        ]
+            employee_map[tilknytning.bruger_uuid]["associations"].append(tilknytning_entry)
+        return employee_map
 
-    def get_employee_manager_references(uuid):
+    def enrich_employees_with_management(employee_map):
+        # Enrich with management
         queryset = session.query(Leder, Enhed).filter(
-            Leder.bruger_uuid == uuid
-        ).filter(
             Leder.enhed_uuid == Enhed.uuid
         ).all()
 
         for _, enhed in queryset:
-            get_org_unit(enhed)
+            add_org_unit(enhed)
 
-        return [
-            {"title": leder.ledertype_titel, "name": enhed.navn, "uuid": enhed.uuid,}
-            for leder, enhed in queryset
-        ]
+        for leder, enhed in queryset:
+            leder_entry = {"title": leder.ledertype_titel, "name": enhed.navn, "uuid": enhed.uuid}
+            employee_map[leder.bruger_uuid]["management"].append(leder_entry)
+        return employee_map
 
+    def filter_employees(employee_map):
+        def filter_function(phonebook_entry):
+            # Do NOT import employees without an engagement or association
+            # https://redmine.magenta-aps.dk/issues/34812
+
+            # We do however want to import employees with management roles.
+            # As an external employee may be a manager for an organisation unit.
+            if (
+                not phonebook_entry["associations"]
+                and not phonebook_entry["engagements"]
+                and not phonebook_entry["management"]
+            ):
+                logger.info(
+                    "OS2MO_IMPORT_ROUTINE Skip employee due to missing engagements, associations, management"
+                )
+
+                # Reference to the skipped employee to debug log
+                logger.debug(
+                    f"OS2MO_IMPORT_ROUTINE - NO_RELATIONS_TO_ORG_UNIT employee={phonebook_entry['uuid']}"
+                )
+                return False
+            return True
+
+        emplyee_map = {uuid: entry for uuid, entry in employee_map.items() if filter_function(entry)}
+        return employee_map
+
+    def enrich_org_units_with_addresses(org_unit_map):
+        # Enrich with adresses
+        queryset = session.query(Adresse).filter(
+            Adresse.enhed_uuid != None
+        ).all()
+
+        return address_helper(queryset, org_unit_map, lambda address: address.enhed_uuid)
+
+    def enrich_employees_with_addresses(employee_map):
+        # Enrich with adresses
+        queryset = session.query(Adresse).filter(
+            Adresse.bruger_uuid != None
+        ).all()
+
+        return address_helper(queryset, employee_map, lambda address: address.bruger_uuid)
+
+    def address_helper(queryset, entry_map, address_to_uuid):
+
+        da_address_types = {
+            "DAR": "DAR",
+            "Telefon": "PHONE",
+            "E-mail": "EMAIL",
+            "EAN": "EAN",
+            "P-nummer": "PNUMBER",
+            "Url": "WWW",
+        }
+
+        async def process_address(address, aiohttp_session):
+            entry_uuid = address_to_uuid(address)
+            if entry_uuid not in entry_map:
+                return
+
+            scope = address.adressetype_scope
+            if scope not in da_address_types:
+                logger.debug(f"Scope: {scope} does not exist")
+                return
+
+            visibility = address.synlighed_titel
+            if visibility == "Hemmelig":
+                return
+
+            if address.værdi:
+                value = address.værdi
+            elif address.dar_uuid is not None:
+                logger.debug("Firing HTTP request for dar_uuid")
+                url = "https://dawa.aws.dk/adresser/" + address.dar_uuid
+                async with aiohttp_session.get(url) as response:
+                    if response.status != 200:
+                        logger.warning("DAWA returned non-200 status code")
+                        return
+                    body = await response.json()
+                    value = body["adressebetegnelse"]
+            else:
+                logger.warning(f"Address: {address.uuid} does not have a value")
+                return
+
+            formatted_address = {
+                "description": address.adressetype_titel,
+                "value": value,
+            }
+
+            entry_map[entry_uuid]["addresses"][da_address_types[scope]].append(formatted_address)
+
+        async def run():
+            # Queue all processing
+            tasks = []
+            async with ClientSession() as aiohttp_session:
+                for address in queryset:
+                    task = asyncio.ensure_future(process_address(address, aiohttp_session))
+                    tasks.append(task)
+                # Await all tasks
+                await asyncio.gather(*tasks)
+
+        loop = asyncio.get_event_loop()
+        future = asyncio.ensure_future(run())
+        loop.run_until_complete(future)
+
+        return entry_map
+
+    class catchtime(object):
+        def __init__(self, operation, rounding=3):
+            self.operation = operation
+            self.rounding = rounding
+
+        def __enter__(self):
+            self.t = time.clock()
+            return self
+
+        def __exit__(self, type, value, traceback):
+            self.t = time.clock() - self.t
+            print(self.operation, "took", round(self.t, self.rounding), "seconds")
+
+    # Employees
+    #----------
     employee_map = {}
-    for employee in session.query(Bruger).all():
-        uuid = employee.uuid
+    with catchtime("fetch_employees"):
+        employee_map = fetch_employees(employee_map)
+    with catchtime("enrich_employees_with_engagements"):
+        employee_map = enrich_employees_with_engagements(employee_map)
+    with catchtime("enrich_employees_with_associations"):
+        employee_map = enrich_employees_with_associations(employee_map)
+    with catchtime("enrich_employees_with_management"):
+        employee_map = enrich_employees_with_management(employee_map)
+    with catchtime("filter_employees"):
+        employee_map = filter_employees(employee_map)
+    with catchtime("enrich_employees_with_addresses"):
+        employee_map = enrich_employees_with_addresses(employee_map)
 
-        phonebook_entry = {}
-        phonebook_entry["uuid"] = uuid
-        phonebook_entry["surname"] = employee.efternavn
-        phonebook_entry["givenname"] = employee.fornavn
-        phonebook_entry["name"] = employee.fornavn + " " + employee.efternavn
+    # Org Units
+    #----------
+    with catchtime("enrich_org_units_with_addresses"):
+        org_unit_map = enrich_org_units_with_addresses(org_unit_map)
 
-        # Enrich employee
-        phonebook_entry["engagements"] = get_employee_engagement_references(uuid)
-        phonebook_entry["associations"] = get_employee_association_references(uuid)
-        phonebook_entry["management"] = get_employee_manager_references(uuid)
+    with catchtime("enrich_org_units_with_engagements"):
+        for uuid, _ in org_unit_map.items():
+            org_unit_map[uuid]["engagements"] = get_org_unit_engagement_references(uuid)
 
-        # Do NOT import employees without an engagement or association
-        # https://redmine.magenta-aps.dk/issues/34812
+    with catchtime("enrich_org_units_with_associations"):
+        for uuid, _ in org_unit_map.items():
+            org_unit_map[uuid]["associations"] = get_org_unit_association_references(uuid)
 
-        # We do however want to import employees with management roles.
-        # As an external employee may be a manager for an organisation unit.
+    with catchtime("enrich_org_units_with_management"):
+        for uuid, _ in org_unit_map.items():
+            org_unit_map[uuid]["management"] = get_org_unit_manager_references(uuid)
 
-        if (
-            not phonebook_entry["associations"]
-            and not phonebook_entry["engagements"]
-            and not phonebook_entry["management"]
-        ):
-            logger.info(
-                "OS2MO_IMPORT_ROUTINE Skip employee due to missing engagements, associations, management"
-            )
-
-            # Reference to the skipped employee to debug log
-            logger.debug(
-                f"OS2MO_IMPORT_ROUTINE - NO_RELATIONS_TO_ORG_UNIT employee={uuid}"
-            )
-
-            continue
-
-        phonebook_entry["addresses"] = get_employee_address_references(uuid)
-
-        employee_map[uuid] = phonebook_entry
-
+    # Write files
+    #------------
     with open("tmp/employees.json", "w") as employees_out:
         json.dump(employee_map, employees_out)
 
@@ -335,16 +470,18 @@ def transfer_json():
     username = settings["exporters.os2phonebook_basic_auth_user"]
     password = settings["exporters.os2phonebook_basic_auth_pass"]
     employees_url = settings["exporters.os2phonebook_employees_uri"]
-    request = requests.get(employees_url, data=employee_map, auth=(username, password))
+    request = requests.post(employees_url, json=employee_map, auth=(username, password))
     if request.status_code != 200:
         logger.warning("OS2Phonebook returned non-200 status code")
         logger.warning(request.text)
+    print(request.text)
  
     org_units_url = settings["exporters.os2phonebook_org_units_uri"]
-    request = requests.get(org_units_url, data=org_unit_map, auth=(username, password))
+    request = requests.post(org_units_url, json=org_unit_map, auth=(username, password))
     if request.status_code != 200:
         logger.warning("OS2Phonebook returned non-200 status code")
         logger.warning(request.text)
+    print(request.text)
 
 
 if __name__ == "__main__":
