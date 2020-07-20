bf4a9c8d8551a6bfb1d2f20270ae0f583f152070
Emil Madsen <emil@magenta.dk>
2020-07-20 16:37:27 +0200

    First hard-coded test

--- integrations/ad_integration/tests/test_ad_sync.py
+++ integrations/ad_integration/tests/test_ad_sync.py
@@ -15,11 +15,44 @@ from test_utils import TestADMoSyncMixin, dict_modifier, mo_modifier
 
 class TestADMoSync(TestCase, TestADMoSyncMixin):
     def setUp(self):
-        self._setup_admosync()
+        def add_sync_mapping(settings):
+            settings["integrations.ad.ad_mo_sync_mapping"] = {
+                "user_addresses": {
+                    "telephoneNumber": ["telephone_uuid", "PUBLIC"],
+                    "mail": ["mail_uuid", None],
+                    "physicalDeliveryOfficeName": ["", 'INTERNAL'],
+                    "mobile": ["mobile_uuid", "SECRET"]
+                },
+            }
+            return settings
 
-    def test_user_edit(self):
-        """Test user edit ps_script code.
+        def add_ad_mail(ad_values):
+            ad_values['mail'] = 'hans@peter.dk'
+            return ad_values
 
-        The common code is not tested.
-        """
+        self._setup_admosync(
+            transform_settings=add_sync_mapping,
+            transform_ad_values=add_ad_mail,
+        )
+
+    def test_sync_email(self):
+        self.assertEqual(self.ad_sync.mo_post_calls, [])
+
         self.ad_sync.update_all_users()
+
+        mo_values = self.mo_values_func()
+        expected_sync = [
+            {
+                'force': True,
+                'payload': {
+                    'address_type': {'uuid': 'mail_uuid'},
+                    'org': {'uuid': 'org_uuid'},
+                    'person': {'uuid': mo_values['uuid']},
+                    'type': 'address',
+                    'validity': {'from': '2020-07-20', 'to': None},
+                    'value': 'hans@peter.dk'
+                },
+                'url': 'details/create'
+            },
+        ]
+        self.assertEqual(self.ad_sync.mo_post_calls, expected_sync)
