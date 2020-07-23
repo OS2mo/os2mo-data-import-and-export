a06bb3f03ed80df0a7cb1192e8d870eedb7056c8
jgj-magenta-aps <jgj@magenta-aps.dk>
2020-07-23 11:30:45 +0200

    37250-actual-state-lc-db-for-jobs
    read-only after export
    
    get_engine and test for it

---
+++ exporters/sql_export/tests/test_engine.py
@@ -0,0 +1,24 @@
+import unittest
+import sqlalchemy
+import tempfile
+import pathlib
+import random
+
+
+from exporters.sql_export.lc_for_jobs_db import get_engine
+
+
+class Test(unittest.TestCase):
+
+    def test_get_engine(self):
+        testnumber = random.randint(1, 1000)
+        with tempfile.TemporaryDirectory() as d:
+            pd  = pathlib.Path(d)
+            dbpath = pd / "testdb"
+            myengine = sqlalchemy.create_engine("sqlite:///{}.db".format(dbpath))
+            myengine.execute("create table x(x integer);")
+            myengine.execute("insert into x values(%d)" % testnumber)
+            myengine.dispose()
+            yourengine = get_engine(dbpath)
+            result = yourengine.execute("select x from x");
+            self.assertEqual(result.first(), (testnumber,))
