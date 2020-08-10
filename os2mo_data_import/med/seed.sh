56df3498ac77266d420bd529dcb170a21b826b2d
Emil Madsen <emil@magenta.dk>
2020-08-10 17:45:34 +0200

    Stashing

--- os2mo_data_import/med/seed.sh
+++ os2mo_data_import/med/seed.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}"
+
 CLI="python mox_util.py cli"
 
 $CLI ensure-facet-exists --bvn hoved_organisation --description Hovedorganisation
