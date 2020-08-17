8d23fd2d76234c89190b862c999edccb31dc4875
jgj-magenta-aps <jgj@magenta-aps.dk>
2020-08-17 09:39:37 +0200

    lint

--- integrations/os2sync/os2sync.py
+++ integrations/os2sync/os2sync.py
@@ -6,12 +6,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import requests
-import logging
-from integrations.os2sync import config
 import hashlib
 import json
+import logging
 
+import requests
+
+from integrations.os2sync import config
 
 settings = config.settings
 logger = logging.getLogger(config.loggername)
