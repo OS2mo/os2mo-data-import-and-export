ce7d416a6bd4e55056003d2f5ed7b89237d99ff6
jgj-magenta-aps <jgj@magenta-aps.dk>
2020-08-17 09:38:34 +0200

    lint

--- integrations/os2sync/os2mo.py
+++ integrations/os2sync/os2mo.py
@@ -5,10 +5,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import requests
 import logging
-from integrations.os2sync import config
 
+import requests
+
+from integrations.os2sync import config
 
 settings = config.settings
 logger = logging.getLogger(config.loggername)
@@ -70,7 +71,8 @@ def has_kle():
     try:
         os2mo_get("{BASE}/o/{ORG}/f/kle_aspect")
         os2mo_get("{BASE}/o/{ORG}/f/kle_number")
-        os2mo_get("{BASE}/ou/" +
+        os2mo_get(
+            "{BASE}/ou/" +
             settings["OS2MO_TOP_UNIT_UUID"] +
             "/details/kle"
         )
