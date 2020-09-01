aa69cfc3e4a4047b25d126f0ebac2d8de06e6dae
Emil Madsen <emil@magenta.dk>
2020-09-01 11:57:41 +0200

    Added alias to mo_values

---
+++ integrations/ad_integration/tests/test_mo_data_source.py
@@ -0,0 +1,50 @@
+# TODO: Fix imports in module
+import sys
+from os.path import dirname
+
+sys.path.append(dirname(__file__))
+sys.path.append(dirname(__file__) + "/..")
+
+from unittest import TestCase
+
+from parameterized import parameterized
+from utils import AttrDict
+from ad_writer import LoraCacheSource
+
+
+class TestLoraCacheSource(TestCase):
+
+    def setUp(self):
+        self.user = self.setup_user()
+        self.lc = AttrDict({
+            'users': {
+                self.user['uuid']: [self.user],
+            }
+        })
+        self.lc_historic = AttrDict({})
+        self.datasource = LoraCacheSource(self.lc, self.lc_historic)
+
+    def setup_user(self):
+        return {
+            'uuid': 'some_uuid_here',
+            'navn': 'some_name some_lastname',
+            'efternavn': 'some_lastname',
+            'fornavn': 'some_name',
+            'kaldenavn': 'some_alias some_lastname_alias',
+            'kaldenavn_fornavn': 'some_alias',
+            'kaldenavn_efternavn': 'some_lastname_alias',
+            'cpr': 'some_cpr',
+        }
+
+    def test_read_user(self):
+        result = self.datasource.read_user('some_uuid_here')
+        self.assertEqual(result, {
+            'uuid': 'some_uuid_here',
+            'name': 'some_name some_lastname',
+            'surname': 'some_lastname',
+            'givenname': 'some_name',
+            'alias': 'some_alias some_lastname_alias',
+            'alias_givenname': 'some_alias',
+            'alias_surname': 'some_lastname_alias',
+            'cpr_no': 'some_cpr',
+        })
