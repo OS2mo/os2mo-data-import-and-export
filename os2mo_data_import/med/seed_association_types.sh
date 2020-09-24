ba5aced57c3bdf309adc1dfb7127f16e0f508e1e
Emil Madsen <sovende@gmail.com>
2020-09-24 13:08:17 +0200

    Adapt seed_association_types script

--- os2mo_data_import/med/seed_association_types.sh
+++ os2mo_data_import/med/seed_association_types.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-CLI="python mox_util.py cli"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}"
+
+MOX_URL="${MOX_URL:-'http://localhost:8080'}"
+
+# CLI="echo python mox_util.py cli"
+CLI="python mox_util.py cli --mox-base ${MOX_URL}"
 ${CLI} ensure-class-exists --bvn "assoc_LR_leader" --title "LR, formand" --facet-bvn "association_type"
 ${CLI} ensure-class-exists --bvn "assoc_LR" --title "LR" --facet-bvn "association_type"
 ${CLI} ensure-class-exists --bvn "assoc_FTR_leader" --title "FTR, næstformand" --facet-bvn "association_type"
