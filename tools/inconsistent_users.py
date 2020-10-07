e94d354224421be772e23994c3b17268e44946a0
Christoffer Moesgaard <cmoesgaard@gmail.com>
2020-10-07 15:08:28 +0200

    WIP

--- tools/inconsistent_users.py
+++ tools/inconsistent_users.py
@@ -26,28 +26,37 @@ class UserComparison():
     def get_csv_mapping(self):
         with open(self.mapping_file_path, 'r') as f:
             csv_reader = csv.DictReader(f, delimiter=";")
-            mapping = {line["mo_uuid"]: line["ad_guid"] for line in csv_reader}
+            mapping = {line["sam_account_name"]: (line["ad_guid"], line['mo_uuid']) for line in csv_reader}
         return mapping
 
     def get_file_users(self) -> set:
-        with pd.ExcelFile(self.user_file_path) as xlsx_file:
-            sheet = xlsx_file.parse('Rettighedstildelinger')
-            xlsx_users = {row.Bruger for index, row in sheet.iterrows()}
-        return xlsx_users
+        with open(self.user_file_path, 'r') as f:
+            csv_reader = csv.DictReader(f, delimiter=";")
+            mapping = {line["Bruger ID"] for line in csv_reader}
+        return mapping
 
     def run(self):
-        mo_users = self.get_mo_users()
+        # mo_users = self.get_mo_users()
         mapping = self.get_csv_mapping()
         file_users = self.get_file_users()
 
-        mapped = [
-            (name, mo_users[name], mapping[mo_users[name]]) for name in file_users
-        ]
+        mapped = []
+        for name in file_users:
+            mapped_user = mapping.get(name)
+            if not mapped_user:
+                print("{} not found in mapping".format(name))
+                continue
+            mapped.append((name, mapped_user))
+
+        # mapped = [
+        #     (name, mapping[name]) for name in file_users
+        # ]
 
         print("{} users found in file".format(len(mapped)))
 
         def filter_fn(row):
-            name, mo_uuid, ad_guid = row
+            name, tup = row
+            mo_uuid, ad_guid = tup
             return mo_uuid != ad_guid
 
         inconsistent_users = list(filter(filter_fn, mapped))
