8f7a865c3becab07bed7deec5d254b7db1ad35aa
Emil Madsen <emil@magenta.dk>
2020-10-12 11:35:26 +0200

    Added snurrebasse to find duplicate it-systems

--- tools/snurrebasser/sd_changed_at_redo.py
+++ tools/snurrebasser/sd_changed_at_redo.py
@@ -1,63 +1,73 @@
-# Denne snurrebasse har til hensigt at rette op på manglende navn-komponenter
-# efter identificering af en fejl i sd changed at, som bevirkede at personer, som
-# havde ændret navn (men kun fornavn ELLER efternavn vill stå med en blank
-# værdi i den del af navnet, der IKKE var blevet opdateret.
-#
-# angiv uuider for de berørte på kommandolinien
+import json
+from collections import ChainMap
+from functools import partial
+from operator import attrgetter
 
-import sys
-import datetime
-from integrations.SD_Lon.sd_changed_at import ChangeAtSD, SETTINGS
-from integrations.SD_Lon.sd_common import sd_lookup
+from exporters.sql_export.lc_for_jobs_db import get_engine
+from exporters.sql_export.sql_table_defs import ItForbindelse
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
 
 
-def read_person(cat, cpr):
-    params = {
-        'EffectiveDate': cat.from_date.strftime('%d.%m.%Y'),
-        'PersonCivilRegistrationIdentifier': cpr,
-        'StatusActiveIndicator': 'true',
-        'StatusPassiveIndicator': 'true',
-        'ContactInformationIndicator': 'false',
-        'PostalAddressIndicator': 'false'
-    }
-    url = 'GetPerson20111201'
-    response = sd_lookup(url, params=params)
-    person = response.get('Person', [])
-
-    if not isinstance(person, list):
-        person = [person]
-    return person
+def find_duplicate_it_connections(session):
+    # These columns specify uniqueness
+    unique_columns = (
+        ItForbindelse.it_system_uuid,
+        ItForbindelse.bruger_uuid,
+        ItForbindelse.enhed_uuid,
+        ItForbindelse.brugernavn,
+    )
+    return (
+        session.query(*unique_columns, func.count(ItForbindelse.id))
+        .group_by(*unique_columns)
+        .having(func.count(ItForbindelse.id) > 1)
+        .all()
+    )
 
 
-def run(uuids):
-    SETTINGS["integrations.SD_Lon.use_ad_integration"] = False
-    cat = ChangeAtSD(from_date=datetime.date.today())
-    mh = cat.helper
-    for uuid in uuids:
-        mh = cat.helper
-        mouser = mh.read_user(uuid)
-        if mouser is None:
-            raise ValueError
-        sdnow = read_person(cat, mouser["cpr_no"])
-        if len(sdnow) > 1:
-            print(sdnow)
-        else:
-            person = sdnow[0]
-            given_name = person.get('PersonGivenName', mouser.get("givenname", ""))
-            sur_name = person.get('PersonSurnameName', mouser.get("surname", ""))
-            payload = {
-                'uuid': uuid,
-                'givenname': given_name,
-                'surname': sur_name,
-                'cpr_no': mouser["cpr_no"],
-                'org': {
-                    'uuid': mouser["org"]["uuid"]
-                }
-            }
-
-            print(payload)
-            mh._mo_post('e/create', payload).json()
+def find_duplicate_rows(session, duplicate_entry):
+    # Unpack row, corresponds to find_duplicate_it_connections select
+    it_system_uuid, bruger_uuid, enhed_uuid, brugernavn, _ = duplicate_entry
+    # Find duplicate rows by filtering on the duplicated values
+    return (
+        session.query(ItForbindelse)
+        .filter(
+            ItForbindelse.it_system_uuid == it_system_uuid,
+            ItForbindelse.bruger_uuid == bruger_uuid,
+            ItForbindelse.enhed_uuid == enhed_uuid,
+            ItForbindelse.brugernavn == brugernavn,
+        )
+        .order_by(ItForbindelse.startdato)
+        .all()
+    )
 
 
-if __name__ == '__main__':
-    run(sys.argv[1:])
+def construct_duplicate_dict(session, duplicate_entry):
+    duplicate_rows = find_duplicate_rows(session, duplicate_entry)
+    # Remove all but newest entry, as we want to keep 1
+    duplicate_rows = duplicate_rows[:-1]
+    # Build dict from id --> uuid for all other rows
+    row_dict = dict(map(attrgetter("id", "uuid"), duplicate_rows))
+    return row_dict
+
+
+def main():
+    engine = get_engine()
+
+    # Prepare session
+    Session = sessionmaker(bind=engine, autoflush=False)
+    session = Session()
+
+    # List of tuples, it_sys_uuid, bruger_uuid, enhed_uuid, brugernavn, count
+    duplicates = find_duplicate_it_connections(session)
+    # List of dicts from id --> uuid (for rows to be deleted)
+    duplicate_maps = map(partial(construct_duplicate_dict, session), duplicates)
+    # One combined dict from id --> uuid (for rows to be deleted)
+    output = dict(ChainMap(*duplicate_maps))
+
+    # Output delete-map
+    print(json.dumps(output, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
