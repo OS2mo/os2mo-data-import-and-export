c7eda55d93cdb5c3c0f5d6acbfcb49dd919c9bb6
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2020-12-08 11:19:38 +0100

    isort

--- reports/Frederikshavn_MED.py
+++ reports/Frederikshavn_MED.py
@@ -14,7 +14,8 @@ from sqlalchemy import or_
 from sqlalchemy.orm import sessionmaker
 
 from exporters.sql_export.lc_for_jobs_db import get_engine
-from exporters.sql_export.sql_table_defs import Adresse, Bruger, Enhed, Tilknytning
+from exporters.sql_export.sql_table_defs import (Adresse, Bruger, Enhed,
+                                                 Tilknytning)
 
 logger = logging.getLogger("Frederikshavn_MED")
 for name in logging.root.manager.loggerDict:
