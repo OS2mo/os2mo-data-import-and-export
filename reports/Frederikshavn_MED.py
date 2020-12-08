db937cc46fac83efd2e2c640e7eeb3e343716346
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2020-12-08 11:16:31 +0100

    Lint and add check for lint

--- reports/Frederikshavn_MED.py
+++ reports/Frederikshavn_MED.py
@@ -10,21 +10,20 @@ import pathlib
 
 import xlsxwriter
 import xlsxwriter.worksheet
-from exporters.sql_export.lc_for_jobs_db import get_engine
-from exporters.sql_export.sql_table_defs import (Adresse, Bruger, Enhed,
-                                                 Tilknytning)
 from sqlalchemy import or_
 from sqlalchemy.orm import sessionmaker
 
+from exporters.sql_export.lc_for_jobs_db import get_engine
+from exporters.sql_export.sql_table_defs import Adresse, Bruger, Enhed, Tilknytning
+
 logger = logging.getLogger("Frederikshavn_MED")
 for name in logging.root.manager.loggerDict:
-    if name in ('Frederikshavn_MED', ):
+    if name in ("Frederikshavn_MED",):
         logging.getLogger(name).setLevel(logging.DEBUG)
     else:
         logging.getLogger(name).setLevel(logging.ERROR)
 logging.basicConfig(
-    format='%(levelname)s %(asctime)s %(name)s %(message)s',
-    level=logging.DEBUG
+    format="%(levelname)s %(asctime)s %(name)s %(message)s", level=logging.DEBUG
 )
 
 
@@ -32,7 +31,7 @@ def get_session(engine):
     return sessionmaker(bind=engine, autoflush=False)()
 
 
-class XLSXExporter():
+class XLSXExporter:
     # Lånt og tilrettet fra os2mo-data-import-and-export/integrations/kle/kle_xlsx.py
     def __init__(self, xlsx_file: str):
         self.xlsx_file = xlsx_file
@@ -49,83 +48,104 @@ class XLSXExporter():
 
     def add_sheet(self, workbook, sheet: str, data: list):
         worksheet = workbook.add_worksheet(name=sheet)
-        worksheet.autofilter('A1:D51')
+        worksheet.autofilter("A1:D51")
 
         for index, key in enumerate(data[0]):
             worksheet.set_column(
-                index, index, width=max(len(val[index]) for val in data))
+                index, index, width=max(len(val[index]) for val in data)
+            )
 
-        bold = workbook.add_format({'bold': 1})
+        bold = workbook.add_format({"bold": 1})
         worksheet.set_row(0, cell_format=bold)
 
         self.write_rows(worksheet, data)
 
 
 class Report_MED:
-
     def __init__(self, session, org_name: str):
         self.session = session
         self.org_name = org_name
 
-    def run(self) -> list: 
+    def run(self) -> list:
         # Find MED organisation
-        hoved_enhed = self.session.query(Enhed).filter(
-            Enhed.navn == self.org_name).one()
+        hoved_enhed = (
+            self.session.query(Enhed).filter(Enhed.navn == self.org_name).one()
+        )
         # Find under-enheder og læg deres uuid'er i et 2 sæt, et til at finde de næste underenheder og et til at samle alle
-        under_enheder = self.session.query(Enhed).filter(
-            Enhed.forældreenhed_uuid == hoved_enhed.uuid).all()
+        under_enheder = (
+            self.session.query(Enhed)
+            .filter(Enhed.forældreenhed_uuid == hoved_enhed.uuid)
+            .all()
+        )
         under_enheder = set(enheder.uuid for enheder in under_enheder)
 
         alle_MED_enheder = under_enheder.copy()
 
         # Så længe der kan findes nye underenheder lægges de i alle_MED_enheder
         while len(under_enheder):
-            under_enheder = self.session.query(Enhed).filter(
-                Enhed.forældreenhed_uuid.in_(under_enheder)).all()
+            under_enheder = (
+                self.session.query(Enhed)
+                .filter(Enhed.forældreenhed_uuid.in_(under_enheder))
+                .all()
+            )
             under_enheder = set(enheder.uuid for enheder in under_enheder)
             alle_MED_enheder.update(under_enheder)
 
         # Så slår vi op i databasen på alle de relevante tabeller og knytter dem sammen med filtre.
         # Desuden filtreres på uuid'erne fundet ovenfor.
-        query = self.session.query(Enhed, Tilknytning, Bruger)\
-            .filter(Enhed.uuid == Tilknytning.enhed_uuid)\
-            .filter(Tilknytning.enhed_uuid.in_(alle_MED_enheder))\
-            .filter(Tilknytning.bruger_uuid == Bruger.uuid)\
+        query = (
+            self.session.query(Enhed, Tilknytning, Bruger)
+            .filter(Enhed.uuid == Tilknytning.enhed_uuid)
+            .filter(Tilknytning.enhed_uuid.in_(alle_MED_enheder))
+            .filter(Tilknytning.bruger_uuid == Bruger.uuid)
             .order_by(Bruger.efternavn)
+        )
 
-        #Nu laves en liste med lister hvori data placeres. For hver bruger laves et opslag for at finde email.
+        # Nu laves en liste med lister hvori data placeres. For hver bruger laves et opslag for at finde email.
         data = []
         for i, row in enumerate(query.all()):
-            email = self.session.query(Adresse).filter(
-                Adresse.bruger_uuid == row.Bruger.uuid,
-                Adresse.adressetype_titel == "Email",
-                or_(Adresse.synlighed_titel == None, Adresse.synlighed_titel != "Hemmelig")).first()
+            email = (
+                self.session.query(Adresse)
+                .filter(
+                    Adresse.bruger_uuid == row.Bruger.uuid,
+                    Adresse.adressetype_titel == "Email",
+                    or_(
+                        Adresse.synlighed_titel == None,
+                        Adresse.synlighed_titel != "Hemmelig",
+                    ),
+                )
+                .first()
+            )
             if email is not None:
                 email = email.værdi
             else:
-                email = ''
-        
+                email = ""
+
             data.append([])
-            data[i] = ["{} {}".format(row.Bruger.fornavn, row.Bruger.efternavn),
-                       email, row.Tilknytning.tilknytningstype_titel, row.Enhed.navn]
-        #indsæt titel række
-        data.insert(0, ["Navn", "Email", "Tilknytningstype",  "Enhed"])
+            data[i] = [
+                "{} {}".format(row.Bruger.fornavn, row.Bruger.efternavn),
+                email,
+                row.Tilknytning.tilknytningstype_titel,
+                row.Enhed.navn,
+            ]
+        # indsæt titel række
+        data.insert(0, ["Navn", "Email", "Tilknytningstype", "Enhed"])
 
         return data
 
 
-if __name__ == '__main__':
-    #Læs fra settins
+if __name__ == "__main__":
+    # Læs fra settins
     settings = json.loads((pathlib.Path(".") / "settings/settings.json").read_text())
-    org_name=settings["report.MED_org_name"]
+    org_name = settings["report.MED_org_name"]
     xlsx_file = settings["report.MED_members_file"]
-    #Lav sqlalchemy session - databasen er sat i settings
+    # Lav sqlalchemy session - databasen er sat i settings
     session = get_session(get_engine())
-    #Udtræk MED medlemmer fra databasen
+    # Udtræk MED medlemmer fra databasen
     data = Report_MED(session, org_name).run()
-    
-    #Skriv MED medlemmernes data i en xlsx fil
+
+    # Skriv MED medlemmernes data i en xlsx fil
     workbook = xlsxwriter.Workbook(xlsx_file)
     excel = XLSXExporter(xlsx_file)
-    excel.add_sheet(workbook, 'MED', data)
+    excel.add_sheet(workbook, "MED", data)
     workbook.close()
--- reports/os2mo_tests/test_frederikshavn_MED_XLSX.py
+++ reports/os2mo_tests/test_frederikshavn_MED_XLSX.py
@@ -1,66 +1,109 @@
 import tempfile
 import unittest
 
-from exporters.sql_export.sql_table_defs import (Adresse, Base, Bruger, Enhed,
-                                                 Tilknytning)
 from openpyxl import load_workbook
-from reports.Frederikshavn_MED import *
 from sqlalchemy import create_engine
+
+from exporters.sql_export.sql_table_defs import (
+    Adresse,
+    Base,
+    Bruger,
+    Enhed,
+    Tilknytning,
+)
+from reports.Frederikshavn_MED import *
 
 
 class Tests_xlxs(unittest.TestCase):
     def setUp(self):
         f = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False)
         self.xlsfilename = f.name
-        self.data = [["Navn", "Email", 'Tilknytningstype', 'Enhed'], [
-            "Fornavn Efternavn", "email@email.com", "Formand", 'Testenhed']]
+        self.data = [
+            ["Navn", "Email", "Tilknytningstype", "Enhed"],
+            ["Fornavn Efternavn", "email@email.com", "Formand", "Testenhed"],
+        ]
 
         workbook = xlsxwriter.Workbook(self.xlsfilename)
         excel = XLSXExporter(self.xlsfilename)
-        excel.add_sheet(workbook, 'MED', self.data)
+        excel.add_sheet(workbook, "MED", self.data)
         workbook.close()
 
     def test_read(self):
         wb = load_workbook(filename=self.xlsfilename)
-        ws = wb['MED']
+        ws = wb["MED"]
         header = [i[0].value for i in ws.columns]
         content = [i[1].value for i in ws.columns]
-        self.assertEqual(header, ['Navn', 'Email', 'Tilknytningstype', 'Enhed'])
-        self.assertEqual(content, ['Fornavn Efternavn',
-                                   'email@email.com', 'Formand', 'Testenhed'])
+        self.assertEqual(header, ["Navn", "Email", "Tilknytningstype", "Enhed"])
+        self.assertEqual(
+            content, ["Fornavn Efternavn", "email@email.com", "Formand", "Testenhed"]
+        )
 
 
 class Tests_db(unittest.TestCase):
     def setUp(self):
-        self.engine = get_engine(dbpath=':memory:')
+        self.engine = get_engine(dbpath=":memory:")
         self.session = get_session(self.engine)
         # Sikrer at der startes fra en tom database
         Base.metadata.drop_all(self.engine)
 
         # Lav tables via tabledefs fra LoraCache og fyld dataen ind
         Base.metadata.create_all(self.engine)
-        enhed = Enhed(navn='Hoved-MED', uuid='E1', enhedstype_titel='org_unit_type')
+        enhed = Enhed(navn="Hoved-MED", uuid="E1", enhedstype_titel="org_unit_type")
         self.session.add(enhed)
-        enhed = Enhed(navn='Under-MED', uuid='E2',
-                      enhedstype_titel='org_unit_type', forældreenhed_uuid='E1')
+        enhed = Enhed(
+            navn="Under-MED",
+            uuid="E2",
+            enhedstype_titel="org_unit_type",
+            forældreenhed_uuid="E1",
+        )
         self.session.add(enhed)
-        enhed = Enhed(navn='Under-under-MED', uuid='E3',
-                      enhedstype_titel='org_unit_type', forældreenhed_uuid='E2')
+        enhed = Enhed(
+            navn="Under-under-MED",
+            uuid="E3",
+            enhedstype_titel="org_unit_type",
+            forældreenhed_uuid="E2",
+        )
         self.session.add(enhed)
-        bruger = Bruger(fornavn='fornavn', efternavn='efternavn',
-                        uuid='b1', bvn='b1bvn', cpr='cpr')
+        bruger = Bruger(
+            fornavn="fornavn",
+            efternavn="efternavn",
+            uuid="b1",
+            bvn="b1bvn",
+            cpr="cpr",
+        )
         self.session.add(bruger)
-        tilknytning = Tilknytning(uuid='t1', bvn='t1bvn', bruger_uuid='b1', enhed_uuid='E2',
-                                  tilknytningstype_titel='titel')
+        tilknytning = Tilknytning(
+            uuid="t1",
+            bvn="t1bvn",
+            bruger_uuid="b1",
+            enhed_uuid="E2",
+            tilknytningstype_titel="titel",
+        )
         self.session.add(tilknytning)
-        bruger = Bruger(fornavn='fornavn2', efternavn='efternavn2',
-                        uuid='b2', bvn='b2bvn', cpr='cpr2')
+        bruger = Bruger(
+            fornavn="fornavn2",
+            efternavn="efternavn2",
+            uuid="b2",
+            bvn="b2bvn",
+            cpr="cpr2",
+        )
         self.session.add(bruger)
-        tilknytning = Tilknytning(uuid='t2', bvn='t2bvn', bruger_uuid='b2', enhed_uuid='E3',
-                                  tilknytningstype_titel='titel2')
+        tilknytning = Tilknytning(
+            uuid="t2",
+            bvn="t2bvn",
+            bruger_uuid="b2",
+            enhed_uuid="E3",
+            tilknytningstype_titel="titel2",
+        )
         self.session.add(tilknytning)
-        adresse = Adresse(uuid='A1', bruger_uuid='b1', adressetype_scope='scope',
-                          adressetype_titel='Email', værdi='test@email.dk', synlighed_titel="Hemmelig")
+        adresse = Adresse(
+            uuid="A1",
+            bruger_uuid="b1",
+            adressetype_scope="scope",
+            adressetype_titel="Email",
+            værdi="test@email.dk",
+            synlighed_titel="Hemmelig",
+        )
         self.session.add(adresse)
         self.session.commit()
 
@@ -70,13 +113,14 @@ class Tests_db(unittest.TestCase):
     def test_data(self):
         hoved_enhed = self.session.query(Enhed).all()
         data = Report_MED(self.session, "Hoved-MED").run()
-        self.assertEqual(data[0], ["Navn", "Email", "Tilknytningstype",  "Enhed"])
+        self.assertEqual(data[0], ["Navn", "Email", "Tilknytningstype", "Enhed"])
 
         self.assertEqual(data[1], ["fornavn efternavn", "", "titel", "Under-MED"])
 
-        self.assertEqual(data[2], ["fornavn2 efternavn2",
-                                   "", "titel2", "Under-under-MED"])
+        self.assertEqual(
+            data[2], ["fornavn2 efternavn2", "", "titel2", "Under-under-MED"]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
