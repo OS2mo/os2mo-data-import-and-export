4ee60f126c704b5d3e28b8034c584d98c1dd9c75
Emil Madsen <emil@magenta.dk>
2021-01-13 17:09:12 +0100

    Added missing import

--- reports/viborg_managers.py
+++ reports/viborg_managers.py
@@ -13,6 +13,7 @@ import pathlib
 import json
 from anytree import PostOrderIter, PreOrderIter
 from os2mo_helpers.mora_helpers import MoraHelper
+from exporters.utils.priority_by_class import choose_public_address
 
 """
 Rapportens opdrag: Tæl lederes medarbejdere, og den har udviklet sig lidt:
