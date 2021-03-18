fc7463f7246130c14ec7af265e833dd6dcaca3f6
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-03-18 14:13:13 +0100

    support for deleting and reimporting opus subtree

--- integrations/opus/clear_and_import_opus.py
+++ integrations/opus/clear_and_import_opus.py
@@ -14,12 +14,17 @@ from integrations.ad_integration import ad_reader
 from integrations.ad_integration.utils import apply
 from integrations.opus import opus_helpers
 from integrations.opus.opus_diff_import import OpusDiffImport
-from tools.default_mo_classes import ensure_default_classes, create_new_root_and_it
+from tools.default_mo_setup import ensure_default_classes, create_new_root_and_it
+from tools.subtreedeleter import SubtreeDeleter
 
 
-def truncate_db(MOX_BASE="http://localhost:8080"):
+def truncate_db(MOX_BASE : str = "http://localhost:8080") -> None:
     r = requests.get(MOX_BASE + "/db/truncate")
     r.raise_for_status()
+
+def delete_opus_tree(api_token: str, org_unit_uuid: str, delete_functions: bool = False) -> None:
+    deleter = SubtreeDeleter(api_token, org_unit_uuid)
+    deleter.run(org_unit_uuid, delete_functions)
 
 def read_all_files(filter_ids):
     """Create full list of data to write to MO.
@@ -43,21 +48,24 @@ def read_all_files(filter_ids):
     return map(lookup_units_and_employees, tqdm(pairwise(export_dates)))
 
 
-def setup_new_mo(settings=load_settings()):
+def setup_new_mo(settings : list = None, opus_uuid:str = None, truncate: bool = None):
     """Create fresh MO setup with nessecary classes
 
     Clear MO database and read through all files to find roles and engagement_types
     """
-    filter_ids = settings.get("integrations.opus.units.filter_ids", [])
-
-    truncate_db(settings.get("mox.base"))
-
-    # Setup classes and root organisation
-    create_new_root_and_it()
+    settings = settings or load_settings()
+    api_token = settings.get('crontab.SAML_TOKEN')
+    if truncate:
+        truncate_db(settings.get("mox.base"))
+        #create root org and it systems
+        create_new_root_and_it()
+    elif opus_uuid:
+        delete_opus_tree(api_token, opus_uuid, delete_functions=True)
+        
     ensure_default_classes()
 
 
-def import_all(ad_reader=None):
+def import_opus(ad_reader=None, import_amount:str = 'one'):
     """Do a clean import from all opus files.
     Removes current OS2MO data and creates new installation with all the required classes etc.
     Then reads all opus files from the path defined in settings
@@ -65,7 +73,7 @@ def import_all(ad_reader=None):
     settings = load_settings()
     filter_ids = settings.get("integrations.opus.units.filter_ids", [])
 
-    setup_new_mo(settings=settings)
+
     employee_mapping = opus_helpers.read_cpr_mapping()
     date_units_and_employees = read_all_files(filter_ids)
     for date, units, employees in date_units_and_employees:
@@ -75,26 +83,29 @@ def import_all(ad_reader=None):
         diff.start_import(units, employees, include_terminations=True)
         # Write latest successful import to rundb so opus_diff_import can continue from where this ended
         opus_helpers.local_db_insert((date, "Diff update ended: {}"))
+        if import_amount == 'one':
+            break
 
 
 @click.command()
 @click.option(
     "--import-amount",
-    type=click.Choice(["none", "all"], case_sensitive=False),
+    type=click.Choice(["one", "all"], case_sensitive=False),
     required=True,
 )
+@click.option("--opus-uuid", type=click.STRING, help="Delete only Opus subtree with the given uuid")
+@click.option("--truncate", is_flag=True, help="Truncate all MO tables - be carefull!")
 @click.option(
     "--use-ad", is_flag=True, type=click.BOOL, default=False, help="Read from AD"
 )
-def clear_and_reload(import_amount, use_ad):
-    if import_amount == "all":
-        AD = None
-        if use_ad:
-            AD = ad_reader.ADParameterReader()
-            AD.cache_all()
-        import_all(ad_reader=AD)
-    else:
-        setup_new_mo()
+def clear_and_reload(import_amount, opus_uuid, truncate, use_ad):
+    settings = load_settings()
+    setup_new_mo(settings=settings, opus_uuid=opus_uuid, truncate=truncate)
+    AD = None
+    if use_ad:
+        AD = ad_reader.ADParameterReader()
+        AD.cache_all()
+    import_opus(ad_reader=AD, import_amount=import_amount)
 
 if __name__ == "__main__":
     clear_and_reload()
