d8a6b1da38458181a7cdff75ded3c93ca78e5cb4
Emil Madsen <emil@magenta.dk>
2021-04-20 17:48:16 +0200

    Reformatting

--- exporters/sql_export/sql_table_defs.py
+++ exporters/sql_export/sql_table_defs.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Boolean, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
