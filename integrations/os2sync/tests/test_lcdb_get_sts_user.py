9f50b4610db8adb2c5e8f5571a6fd368449aee9c
Mads Riis-Weitling <mrw@magenta.dk>
2021-05-12 14:08:55 +0200

    [#43471] OS2Sync: allow a Jinja template for `person.user_id`

--- integrations/os2sync/tests/test_lcdb_get_sts_user.py
+++ integrations/os2sync/tests/test_lcdb_get_sts_user.py
@@ -5,7 +5,7 @@ from alchemy_mock.mocking import UnifiedAlchemyMagicMock
 from parameterized import parameterized
 
 from exporters.sql_export.sql_table_defs import Bruger
-from integrations.os2sync.lcdb_os2mo import get_sts_user as lcdb_get_sts_user
+from integrations.os2sync import lcdb_os2mo
 from integrations.os2sync.tests.helpers import NICKNAME_TEMPLATE
 
 
@@ -21,6 +21,7 @@ _lcdb_mock_users = [
                 efternavn="Testesen",
                 kaldenavn_fornavn="",
                 kaldenavn_efternavn="",
+                bvn="mo-user-key",
             ),
         ],
     ),
@@ -34,6 +35,7 @@ _lcdb_mock_users = [
                 efternavn="Testesen",
                 kaldenavn_fornavn="Teste",
                 kaldenavn_efternavn="Testersen",
+                bvn="mo-user-key",
             ),
         ],
     ),
@@ -67,15 +69,15 @@ class TestGetStsUser(unittest.TestCase):
             ),
         ]
     )
-    def test_get_sts_user(self, template, uuid, expected_name):
+    def test_person_template_nickname(self, template, uuid, expected_name):
         if template:
             # Run with template
             with patch.dict("integrations.os2sync.config.settings") as settings:
                 settings["OS2SYNC_TEMPLATES"]["person.name"] = template
-                sts_user = lcdb_get_sts_user(self._session, uuid, [])
+                sts_user = lcdb_os2mo.get_sts_user(self._session, uuid, [])
         else:
             # Run without template
-            sts_user = lcdb_get_sts_user(self._session, uuid, [])
+            sts_user = lcdb_os2mo.get_sts_user(self._session, uuid, [])
 
         self.assertDictEqual(
             sts_user,
@@ -89,3 +91,72 @@ class TestGetStsUser(unittest.TestCase):
                 },
             },
         )
+
+    @parameterized.expand(
+        [
+            # Test without an AD BVN and without template
+            (
+                None,  # template config
+                None,  # return value of `try_get_ad_user_key`
+                "name only",  # expected value of `UserId` (MO UUID)
+            ),
+            # Test without an AD BVN, and template which uses `user_key`
+            (
+                {"person.user_id": "{{ user_key }}"},  # template config
+                None,  # return value of `try_get_ad_user_key`
+                "mo-user-key",  # expected value of `UserId` (MO BVN)
+            ),
+            # Test without an AD BVN, and template which uses `uuid`
+            (
+                {"person.user_id": "{{ uuid }}"},  # template config
+                None,  # return value of `try_get_ad_user_key`
+                "name only",  # expected value of `UserId` (MO UUID)
+            ),
+            # Test with an AD BVN, but without template
+            (
+                None,  # template config
+                "mock-ad-bvn",  # return value of `try_get_ad_user_key`
+                "mock-ad-bvn",  # expected value of `UserId` (AD BVN)
+            ),
+            # Test with an AD BVN, and template which uses `user_key`
+            (
+                {"person.user_id": "{{ user_key }}"},  # template config
+                "mock-ad-bvn",  # return value of `try_get_ad_user_key`
+                "mock-ad-bvn",  # expected value of `UserId` (AD BVN)
+            ),
+            # Test with an AD BVN, and template which uses `uuid`
+            (
+                {"person.user_id": "{{ uuid }}"},  # template config
+                "mock-ad-bvn",  # return value of `try_get_ad_user_key`
+                "mock-ad-bvn",  # expected value of `UserId` (AD BVN)
+            ),
+
+        ]
+    )
+    def test_user_template_user_id(
+        self,
+        os2sync_templates,
+        given_ad_user_key,
+        expected_user_id,
+    ):
+        mo_user_uuid = "name only"
+        with patch.dict("integrations.os2sync.config.settings") as settings:
+            settings["OS2SYNC_TEMPLATES"] = os2sync_templates or {}
+            with self._patch("try_get_ad_user_key", given_ad_user_key):
+                sts_user = lcdb_os2mo.get_sts_user(self._session, mo_user_uuid, [])
+
+        self.assertDictEqual(
+            sts_user,
+            {
+                "Uuid": mo_user_uuid,
+                "UserId": expected_user_id,
+                "Positions": [],
+                "Person": {
+                    "Name": "Test Testesen",
+                    "Cpr": None,
+                },
+            },
+        )
+
+    def _patch(self, name, return_value):
+        return patch.object(lcdb_os2mo, name, return_value=return_value)
