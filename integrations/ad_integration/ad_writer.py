d1331fab1a1c2a8790bb2ad60ccd4beb98256a42
Emil Madsen <emil@magenta.dk>
2021-06-04 12:54:04 +0200

    Change universe start to 1930

--- integrations/ad_integration/ad_writer.py
+++ integrations/ad_integration/ad_writer.py
@@ -119,11 +119,11 @@ class MODataSource(ABC):
         """
         start_dates, end_dates = self.get_engagement_dates(uuid)
 
-        start_dates = map(lambda date: date if date else "1800-01-01", start_dates)
+        start_dates = map(lambda date: date if date else "1930-01-01", start_dates)
         start_date = min(start_dates, default="9999-12-31")
 
         end_dates = map(lambda date: date if date else "9999-12-31", end_dates)
-        end_date = max(end_dates, default="1800-01-01")
+        end_date = max(end_dates, default="1930-01-01")
 
         return start_date, end_date
 
