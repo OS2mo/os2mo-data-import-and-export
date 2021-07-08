2d41c1fe04486e2b72d855109877222a81368f08
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-07-08 11:49:27 +0200

    Remove cronhook system

--- tools/cronhook.post.d/unmount_opus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-[ "${mount_opus_mountpoint}" = "" ] && echo "ERROR: mount_opus_mountpoint not in settings" && exit 1
-
-/bin/umount "${mount_opus_mountpoint}"
--- tools/job-runner.sh
+++ tools/job-runner.sh
@@ -101,19 +101,6 @@ imports_test_sd_connectivity(){
 }
 
 imports_test_opus_connectivity(){
-    (
-        SETTING_PREFIX="cronhook" source ${DIPEXAR}/tools/prefixed_settings.sh
-        if [ "${mount_opus_on}" = "true" ] ; then
-            echo testing opus mountpoint settings
-            mountpoint ${mount_opus_mountpoint} || exit 1
-            (
-                # hvis opus er mountet, skal xml_path, hvor man læser opus fra være == mountpointet
-                SETTING_PREFIX="integrations.opus.import" source ${DIPEXAR}/tools/prefixed_settings.sh
-                [ ! "${mount_opus_mountpoint}" = "${xml_path}" ] && echo "xml-path skal være == mountpoint" && exit 1
-                exit 0
-            )
-        fi
-    ) || return 1
     echo running imports_test_ops_connectivity
     ${VENV}/bin/python3 integrations/opus/test_opus_connectivity.py --test-diff-import
 }
