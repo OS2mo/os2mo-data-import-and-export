87a5c5dca2bd34825f2680e046fcee4007c41ee0
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-07-08 13:24:36 +0200

    Add script to delete users from fk-org

---
+++ integrations/os2sync/delete_user.py
@@ -0,0 +1,16 @@
+import click
+from integrations.os2sync.os2sync import delete_user
+import config
+
+settings = config.settings
+
+BASE = settings["OS2MO_SERVICE_URL"]
+
+@click.command()
+@click.option('--uuid', type=click.UUID)
+def delete_fk_org_user(uuid):
+
+    delete_user(str(uuid))
+
+if __name__ == '__main__':
+    delete_fk_org_user()
