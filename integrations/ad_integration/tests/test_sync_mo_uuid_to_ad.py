d275de973837c6bec96253b0ceb16231cc6dcc7e
Mads Riis-Weitling <mrw@magenta.dk>
2021-07-14 10:30:48 +0200

    [#44581] Add test for `SyncMoUuidToAd.sync_all`

--- integrations/ad_integration/tests/test_sync_mo_uuid_to_ad.py
+++ integrations/ad_integration/tests/test_sync_mo_uuid_to_ad.py
@@ -13,7 +13,25 @@ _MO_UUID = "not-a-uuid"
 _AD_UUID_FIELD = "uuidField"
 
 
+class _MockMoraHelper(MoraHelper):
+    def __init__(self, cpr):
+        self._mo_user = {"cpr_no": cpr, "uuid": _MO_UUID}
+        super().__init__()
+
+    def read_all_users(self):
+        return [self._mo_user]
+
+
 class _SyncMoUuidToAd(sync_mo_uuid_to_ad.SyncMoUuidToAd):
+    """Testable subclass of the real `SyncMoUuidToAd` class."""
+
+    def __init__(self, ad_cpr_no):
+        self._ad_cpr_no = ad_cpr_no
+        super().__init__()
+
+    def _configure_mora_helper(self):
+        return _MockMoraHelper(self._ad_cpr_no), "not-a-org-uuid"
+
     def _create_session(self):
         return mock.MagicMock()
 
@@ -47,39 +65,58 @@ class TestSyncMoUuidToAd(TestCase):
 
     def test_sync_one(self):
         instance = self._get_instance()
-        ad_cpr_no = instance.reader.read_user()["extensionAttribute1"]
-        instance.sync_one(ad_cpr_no)
-        # Assert we tried to execute one script with the proper contents
-        self.assertEqual(len(instance._scripts), 1)
-        self.assertIn(
-            '-Replace @{"%s"="%s"}' % (_AD_UUID_FIELD, _MO_UUID),
-            instance._scripts[0],
-        )
+        instance.sync_one(instance._ad_cpr_no)
+        self._assert_script_contents_ok(instance)
+
+    def test_sync_all(self):
+        instance = self._get_instance()
+        instance.sync_all()
+        self._assert_script_contents_ok(instance)
 
     def _get_instance(self, settings=None):
         _settings = {
             "integrations.ad.write.uuid_field": _AD_UUID_FIELD,
             "integrations.ad": [{"properties": [_AD_UUID_FIELD]}],
-            "global": {"mora.base": None},
             "primary": {"cpr_field": "extensionAttribute1", "cpr_separator": "-"},
+            "global": {},
         }
 
         if settings:
             _settings.update(settings)
 
-        with mock.patch.object(ad_common, "read_settings", return_value=_settings):
-            with mock.patch.object(
-                sync_mo_uuid_to_ad, "load_settings", return_value=_settings
-            ):
-                with mock.patch.object(
-                    sync_mo_uuid_to_ad,
-                    "MoraHelper",
-                    spec=MoraHelper,
-                    read_organisation=lambda: "123",
-                ):
-                    with mock.patch.object(
-                        sync_mo_uuid_to_ad,
-                        "ADParameterReader",
-                        new=MockADParameterReader,
-                    ):
-                        return _SyncMoUuidToAd()
+        read_settings_mock = mock.patch.object(
+            ad_common,
+            "read_settings",
+            return_value=_settings,
+        )
+
+        load_settings_mock = mock.patch.object(
+            sync_mo_uuid_to_ad,
+            "load_settings",
+            return_value=_settings,
+        )
+
+        reader = MockADParameterReader()
+
+        reader_mock = mock.patch.object(
+            sync_mo_uuid_to_ad,
+            "ADParameterReader",
+            new=lambda: reader,
+        )
+
+        with read_settings_mock:
+            with load_settings_mock:
+                with reader_mock:
+                    instance = _SyncMoUuidToAd(
+                        ad_cpr_no=reader.read_user()["extensionAttribute1"]
+                    )
+                    return instance
+
+    def _assert_script_contents_ok(self, instance):
+        # Assert we tried to execute exactly one script, with the proper
+        # contents.
+        self.assertEqual(len(instance._scripts), 1)
+        self.assertIn(
+            '-Replace @{"%s"="%s"}' % (_AD_UUID_FIELD, _MO_UUID),
+            instance._scripts[0],
+        )
