85599ba0df3fc123e7978e91aebe04b56a97eb11
Emil Madsen <emil@magenta.dk>
2021-07-21 16:38:56 +0200

    Added documentation

--- integrations/os2sync/delete_user.py
+++ integrations/os2sync/delete_user.py
@@ -1,15 +1,21 @@
+from uuid import UUID
+
 import click
+
 from integrations.os2sync.os2sync import delete_user
-import config
 
-settings = config.settings
-
-BASE = settings["OS2MO_SERVICE_URL"]
 
 @click.command()
-@click.option('--uuid', type=click.UUID)
-def delete_fk_org_user(uuid):
+@click.option(
+    "--uuid",
+    type=click.UUID,
+    required=True,
+    help="UUID of the MO user to delete in FK org.",
+)
+def delete_fk_org_user(uuid: UUID) -> None:
+    """Delete a MO user in FK org."""
     delete_user(str(uuid))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     delete_fk_org_user()
