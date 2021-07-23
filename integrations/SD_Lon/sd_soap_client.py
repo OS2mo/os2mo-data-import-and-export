9ea1d6a99dce8a23812a8b38294e02573ae34f17
Emil Madsen <emil@magenta.dk>
2021-07-23 17:31:25 +0200

    Added async client

--- integrations/SD_Lon/sd_soap_client.py
+++ integrations/SD_Lon/sd_soap_client.py
@@ -1,11 +1,22 @@
 # type: ignore
+from abc import ABC
+from abc import abstractmethod
+from functools import partial
 from io import StringIO
+from typing import Any
+from typing import Callable
+from typing import Iterator
+from typing import Tuple
 
+import httpx
 from requests import Session
 from requests.auth import HTTPBasicAuth
+from zeep import AsyncClient
 from zeep import Client
 from zeep.cache import SqliteCache
+from zeep.proxy import AsyncServiceProxy
 from zeep.proxy import ServiceProxy
+from zeep.transports import AsyncTransport
 from zeep.transports import Transport
 
 
@@ -127,31 +138,83 @@ WSDL_TOP = """<?xml version="1.0"?>
 """
 
 
-class SDSoapClient:
+class SDSoapClientBase(ABC):
     def __init__(self, username: str, password: str):
+        # Load our top-level wsdl (specifying all endpoints) into the client
+        wsdl_top = StringIO(WSDL_TOP)
+        client = self._create_client(wsdl_top, username, password)
+        # Fetch all services from all definition endpoints
+        services = self._yield_services(client)
+        # Build a method-map from services (method_name, callable)
+        method_map = dict(map(partial(self._service2method, client), services))
+        # Export every method in the method map directly on ourself
+        for method_name, cally in method_map.items():
+            assert hasattr(self, method_name) is False
+            setattr(self, method_name, cally)
+
+    def _yield_services(self, client: Any) -> Iterator:
+        definitions = list(client.wsdl._definitions.values())
+        for definition in definitions:
+            for service in definition.services.values():
+                yield service
+
+    def _service2method(self, client: Any, service: Any) -> Tuple[str, Callable]:
+        assert len(service.ports) == 1
+        ((port_name, port),) = service.ports.items()
+
+        service = self._create_service_proxy(
+            client, port.binding, **port.binding_options
+        )
+        assert len(service._operations) == 1
+
+        ((operation_name, operation),) = service._operations.items()
+        return port_name, operation
+
+    @abstractmethod
+    def _create_client(self, wsdl: StringIO, username: str, password: str) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _create_service_proxy(self, client: Any, binding: Any, **kwargs) -> Any:
+        raise NotImplementedError
+
+
+class AsyncSDSoapClient(SDSoapClientBase):
+    def _create_async_client(self, username: str, password: str):
+        self.httpx_client = httpx.AsyncClient(auth=(username, password))
+        return self.httpx_client
+
+    def _create_client(
+        self, wsdl: StringIO, username: str, password: str
+    ) -> AsyncClient:
+        httpx_client = self._create_async_client(username, password)
+        # httpx_client = httpx.AsyncClient(auth=(username, password))
+        client = AsyncClient(
+            wsdl, transport=AsyncTransport(client=httpx_client, cache=SqliteCache())
+        )
+        return client
+
+    def _create_service_proxy(self, client: AsyncClient, binding: Any, **kwargs) -> Any:
+        return AsyncServiceProxy(client, binding, **kwargs)
+
+    async def aclose(self):
+        await self.httpx_client.aclose()
+
+
+class SDSoapClient(SDSoapClientBase):
+    def _create_client(self, wsdl: StringIO, username: str, password: str) -> Client:
         session = Session()
         session.auth = HTTPBasicAuth(username, password)
-        wsdl_top = StringIO(WSDL_TOP)
-        client = Client(
-            wsdl_top, transport=Transport(session=session, cache=SqliteCache())
-        )
-        definitions = list(client.wsdl._definitions.values())
+        client = Client(wsdl, transport=Transport(session=session, cache=SqliteCache()))
+        return client
 
-        for definition in definitions:
-            for service_name, service in definition.services.items():
-                assert len(service.ports) == 1
-                ((port_name, port),) = service.ports.items()
-
-                service = ServiceProxy(client, port.binding, **port.binding_options)
-                assert len(service._operations) == 1
-
-                ((operation_name, operation),) = service._operations.items()
-                assert hasattr(self, port_name) is False
-                setattr(self, port_name, operation)
+    def _create_service_proxy(self, client: Client, binding: Any, **kwargs) -> Any:
+        return ServiceProxy(client, binding, **kwargs)
 
 
 if __name__ == "__main__":
     import click
+    from ra_utils.async_to_sync import async_to_sync
 
     @click.command()
     @click.option(
@@ -166,16 +229,28 @@ if __name__ == "__main__":
         prompt=True,
         hide_input=True,
     )
-    def main(username: str, password: str):
+    @click.option(
+        "--institution-identifier",
+        required=True,
+        help="SD identifier for the institution",
+    )
+    @async_to_sync
+    async def main(username: str, password: str, institution_identifier: str):
+        params = {
+            "InstitutionIdentifier": institution_identifier,
+            "AdministrationIndicator": False,
+            "ContactInformationIndicator": False,
+            "PostalAddressIndicator": False,
+            "ProductionUnitIndicator": False,
+            "UUIDIndicator": True,
+        }
         soap_client = SDSoapClient(username, password)
-        result = soap_client.GetInstitution20111201(
-            InstitutionIdentifier="BR",
-            AdministrationIndicator=False,
-            ContactInformationIndicator=False,
-            PostalAddressIndicator=False,
-            ProductionUnitIndicator=False,
-            UUIDIndicator=True,
-        )
+        result = soap_client.GetInstitution20111201(**params)
+        print(result)
+
+        asoap_client = AsyncSDSoapClient(username, password)
+        result = await asoap_client.GetInstitution20111201(**params)
+        await asoap_client.aclose()
         print(result)
 
     main()
