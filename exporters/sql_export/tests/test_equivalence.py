ece856b59d5324594012897ced9341678ca47063
Casper V. Kristensen <vk@magenta.dk>
2021-08-23 11:14:21 +0200

    [#44359] Black format exporters/sql_export/tests/test_equivalence.py

--- exporters/sql_export/tests/test_equivalence.py
+++ exporters/sql_export/tests/test_equivalence.py
@@ -2,6 +2,7 @@ import pytest
 import json
 from tqdm import tqdm
 from exporters.sql_export.lora_cache import LoraCache
+import urllib.parse
 
 
 skip = pytest.mark.skip
@@ -12,17 +13,17 @@ skip = pytest.mark.skip
 
 def _old_cache_lora_facets(self):
     # Facets are eternal i MO and does not need a historic dump
-    params = {'bvn': '%'}
-    url = '/klassifikation/facet'
+    params = {"bvn": "%"}
+    url = "/klassifikation/facet"
     facet_list = self._perform_lora_lookup(url, params, skip_history=True, unit="facet")
 
     facets = {}
     for facet in tqdm(facet_list, desc="Processing facet", unit="facet"):
-        uuid = facet['id']
-        reg = facet['registreringer'][0]
-        user_key = reg['attributter']['facetegenskaber'][0]['brugervendtnoegle']
+        uuid = facet["id"]
+        reg = facet["registreringer"][0]
+        user_key = reg["attributter"]["facetegenskaber"][0]["brugervendtnoegle"]
         facets[uuid] = {
-            'user_key': user_key,
+            "user_key": user_key,
         }
     return facets
 
@@ -40,23 +41,23 @@ def test_facet_equivalence(full_history, skip_past):
 def _old_cache_lora_classes(self):
     # MO itself will not read historic information on classes,
     # currently we replicate this behaviour here.
-    params = {'bvn': '%'}
-    url = '/klassifikation/klasse'
+    params = {"bvn": "%"}
+    url = "/klassifikation/klasse"
     class_list = self._perform_lora_lookup(url, params, skip_history=True, unit="class")
 
     classes = {}
     for oio_class in tqdm(class_list, desc="Processing class", unit="class"):
-        uuid = oio_class['id']
-        reg = oio_class['registreringer'][0]
-        user_key = reg['attributter']['klasseegenskaber'][0]['brugervendtnoegle']
-        scope = reg['attributter']['klasseegenskaber'][0].get('omfang')
-        title = reg['attributter']['klasseegenskaber'][0]['titel']
-        facet = reg['relationer']['facet'][0]['uuid']
+        uuid = oio_class["id"]
+        reg = oio_class["registreringer"][0]
+        user_key = reg["attributter"]["klasseegenskaber"][0]["brugervendtnoegle"]
+        scope = reg["attributter"]["klasseegenskaber"][0].get("omfang")
+        title = reg["attributter"]["klasseegenskaber"][0]["titel"]
+        facet = reg["relationer"]["facet"][0]["uuid"]
         classes[uuid] = {
-            'user_key': user_key,
-            'title': title,
-            'scope': scope,
-            'facet': facet
+            "user_key": user_key,
+            "title": title,
+            "scope": scope,
+            "facet": facet,
         }
     return classes
 
@@ -73,22 +74,22 @@ def test_class_equivalence(full_history, skip_past):
 
 def old_cache_lora_itsystems(self):
     # IT-systems are eternal i MO and does not need a historic dump
-    params = {'bvn': '%'}
-    url = '/organisation/itsystem'
-    itsystem_list = self._perform_lora_lookup(url, params, skip_history=True, unit="itsystem")
+    params = {"bvn": "%"}
+    url = "/organisation/itsystem"
+    itsystem_list = self._perform_lora_lookup(
+        url, params, skip_history=True, unit="itsystem"
+    )
 
     itsystems = {}
     for itsystem in tqdm(itsystem_list, desc="Processing itsystem", unit="itsystem"):
-        uuid = itsystem['id']
-        reg = itsystem['registreringer'][0]
-        user_key = (reg['attributter']['itsystemegenskaber'][0]
-        ['brugervendtnoegle'])
-        name = (reg['attributter']['itsystemegenskaber'][0]
-        ['itsystemnavn'])
+        uuid = itsystem["id"]
+        reg = itsystem["registreringer"][0]
+        user_key = reg["attributter"]["itsystemegenskaber"][0]["brugervendtnoegle"]
+        name = reg["attributter"]["itsystemegenskaber"][0]["itsystemnavn"]
 
         itsystems[uuid] = {
-            'user_key': user_key,
-            'name': name,
+            "user_key": user_key,
+            "name": name,
         }
     return itsystems
 
@@ -103,8 +104,8 @@ def test_itsystems_equivalence(full_history, skip_past):
 
 
 def old_cache_lora_users(self):
-    params = {'bvn': '%'}
-    url = '/organisation/bruger'
+    params = {"bvn": "%"}
+    url = "/organisation/bruger"
     user_list = self._perform_lora_lookup(url, params, unit="user")
 
     relevant = {
@@ -115,7 +116,7 @@ def old_cache_lora_users(self):
 
     users = {}
     for user in tqdm(user_list, desc="Processing user", unit="user"):
-        uuid = user['id']
+        uuid = user["id"]
         users[uuid] = []
 
         effects = list(self._get_effects(user, relevant))
@@ -125,40 +126,41 @@ def old_cache_lora_users(self):
                 continue
             reg = effect[2]
 
-            tilknyttedepersoner = reg['relationer']['tilknyttedepersoner']
+            tilknyttedepersoner = reg["relationer"]["tilknyttedepersoner"]
             if len(tilknyttedepersoner) == 0:
                 continue
-            cpr = tilknyttedepersoner[0]['urn'][-10:]
+            cpr = tilknyttedepersoner[0]["urn"][-10:]
 
-            egenskaber = reg['attributter']['brugeregenskaber']
+            egenskaber = reg["attributter"]["brugeregenskaber"]
             if len(egenskaber) == 0:
                 continue
             egenskaber = egenskaber[0]
 
-            udv = reg['attributter']['brugerudvidelser']
+            udv = reg["attributter"]["brugerudvidelser"]
             if len(udv) == 0:
                 continue
             udv = udv[0]
 
-            user_key = egenskaber.get('brugervendtnoegle', '')
-            fornavn = udv.get('fornavn', '')
-            efternavn = udv.get('efternavn', '')
-            kaldenavn_fornavn = udv.get('kaldenavn_fornavn', '')
-            kaldenavn_efternavn = udv.get('kaldenavn_efternavn', '')
+            user_key = egenskaber.get("brugervendtnoegle", "")
+            fornavn = udv.get("fornavn", "")
+            efternavn = udv.get("efternavn", "")
+            kaldenavn_fornavn = udv.get("kaldenavn_fornavn", "")
+            kaldenavn_efternavn = udv.get("kaldenavn_efternavn", "")
             users[uuid].append(
                 {
-                    'uuid': uuid,
-                    'cpr': cpr,
-                    'user_key': user_key,
-                    'fornavn': fornavn,
-                    'efternavn': efternavn,
-                    'navn': ' '.join([fornavn, efternavn]).strip(),
-                    'kaldenavn_fornavn': kaldenavn_fornavn,
-                    'kaldenavn_efternavn': kaldenavn_efternavn,
-                    'kaldenavn': ' '.join([kaldenavn_fornavn,
-                                           kaldenavn_efternavn]).strip(),
-                    'from_date': from_date,
-                    'to_date': to_date
+                    "uuid": uuid,
+                    "cpr": cpr,
+                    "user_key": user_key,
+                    "fornavn": fornavn,
+                    "efternavn": efternavn,
+                    "navn": " ".join([fornavn, efternavn]).strip(),
+                    "kaldenavn_fornavn": kaldenavn_fornavn,
+                    "kaldenavn_efternavn": kaldenavn_efternavn,
+                    "kaldenavn": " ".join(
+                        [kaldenavn_fornavn, kaldenavn_efternavn]
+                    ).strip(),
+                    "from_date": from_date,
+                    "to_date": to_date,
                 }
             )
     return users
@@ -174,22 +176,24 @@ def test_users_equivalence(full_history, skip_past):
 
 
 def old_cache_lora_units(self):
-    params = {'bvn': '%'}
+    params = {"bvn": "%"}
     skip_history = False
     if not self.full_history:
-        params['gyldighed'] = 'Aktiv'
+        params["gyldighed"] = "Aktiv"
         skip_history = True
-    url = '/organisation/organisationenhed'
+    url = "/organisation/organisationenhed"
     relevant = {
-        'relationer': ('overordnet', 'enhedstype', 'niveau'),
-        'attributter': ('organisationenhedegenskaber',)
+        "relationer": ("overordnet", "enhedstype", "niveau"),
+        "attributter": ("organisationenhedegenskaber",),
     }
 
-    unit_list = self._perform_lora_lookup(url, params, skip_history=skip_history, unit="unit")
+    unit_list = self._perform_lora_lookup(
+        url, params, skip_history=skip_history, unit="unit"
+    )
 
     units = {}
     for unit in tqdm(unit_list, desc="Processing unit", unit="unit"):
-        uuid = unit['id']
+        uuid = unit["id"]
         units[uuid] = []
 
         effects = self._get_effects(unit, relevant)
@@ -197,33 +201,32 @@ def old_cache_lora_units(self):
             from_date, to_date = self._from_to_from_effect(effect)
             if from_date is None and to_date is None:
                 continue
-            relationer = effect[2]['relationer']
+            relationer = effect[2]["relationer"]
 
-            orgegenskaber = (effect[2]['attributter']
-            ['organisationenhedegenskaber'])
+            orgegenskaber = effect[2]["attributter"]["organisationenhedegenskaber"]
             if len(orgegenskaber) == 0:
                 continue
             egenskaber = orgegenskaber[0]
-            parent_raw = relationer['overordnet'][0]['uuid']
+            parent_raw = relationer["overordnet"][0]["uuid"]
             if parent_raw == self.org_uuid:
                 parent = None
             else:
                 parent = parent_raw
 
-            if 'niveau' in relationer and len(relationer['niveau']) > 0:
-                level = relationer['niveau'][0]['uuid']
+            if "niveau" in relationer and len(relationer["niveau"]) > 0:
+                level = relationer["niveau"][0]["uuid"]
             else:
                 level = None
             units[uuid].append(
                 {
-                    'uuid': uuid,
-                    'user_key': egenskaber['brugervendtnoegle'],
-                    'name': egenskaber['enhedsnavn'],
-                    'unit_type': relationer['enhedstype'][0]['uuid'],
-                    'level': level,
-                    'parent': parent,
-                    'from_date': from_date,
-                    'to_date': to_date
+                    "uuid": uuid,
+                    "user_key": egenskaber["brugervendtnoegle"],
+                    "name": egenskaber["enhedsnavn"],
+                    "unit_type": relationer["enhedstype"][0]["uuid"],
+                    "level": level,
+                    "parent": parent,
+                    "from_date": from_date,
+                    "to_date": to_date,
                 }
             )
     return units
@@ -240,19 +243,24 @@ def test_units_equivalence(full_history, skip_past):
 
 
 def _old_cache_lora_it_connections(self):
-    params = {'gyldighed': 'Aktiv', 'funktionsnavn': 'IT-system'}
-    url = '/organisation/organisationfunktion'
+    params = {"gyldighed": "Aktiv", "funktionsnavn": "IT-system"}
+    url = "/organisation/organisationfunktion"
     it_connection_list = self._perform_lora_lookup(url, params, unit="it connection")
 
     it_connections = {}
-    for it_connection in tqdm(it_connection_list, desc="Processing it connection", unit="it connection"):
-        uuid = it_connection['id']
+    for it_connection in tqdm(
+        it_connection_list, desc="Processing it connection", unit="it connection"
+    ):
+        uuid = it_connection["id"]
         it_connections[uuid] = []
 
         relevant = {
-            'relationer': ('tilknyttedeenheder', 'tilknyttedebrugere',
-                           'tilknyttedeitsystemer'),
-            'attributter': ('organisationfunktionegenskaber',),
+            "relationer": (
+                "tilknyttedeenheder",
+                "tilknyttedebrugere",
+                "tilknyttedeitsystemer",
+            ),
+            "attributter": ("organisationfunktionegenskaber",),
             # "tilstande": ("organisationfunktiongyldighed",)  # bug in old cache; is needed for equivalence
         }
 
@@ -261,30 +269,29 @@ def _old_cache_lora_it_connections(self):
             from_date, to_date = self._from_to_from_effect(effect)
             if from_date is None and to_date is None:
                 continue
-            user_key = (
-                effect[2]['attributter']['organisationfunktionegenskaber']
-                [0]['brugervendtnoegle']
-            )
+            user_key = effect[2]["attributter"]["organisationfunktionegenskaber"][0][
+                "brugervendtnoegle"
+            ]
 
-            rel = effect[2]['relationer']
-            itsystem = rel['tilknyttedeitsystemer'][0]['uuid']
+            rel = effect[2]["relationer"]
+            itsystem = rel["tilknyttedeitsystemer"][0]["uuid"]
 
-            if 'tilknyttedeenheder' in rel:
-                unit_uuid = rel['tilknyttedeenheder'][0]['uuid']
+            if "tilknyttedeenheder" in rel:
+                unit_uuid = rel["tilknyttedeenheder"][0]["uuid"]
                 user_uuid = None
             else:
-                user_uuid = rel['tilknyttedebrugere'][0]['uuid']
+                user_uuid = rel["tilknyttedebrugere"][0]["uuid"]
                 unit_uuid = None
 
             it_connections[uuid].append(
                 {
-                    'uuid': uuid,
-                    'user': user_uuid,
-                    'unit': unit_uuid,
-                    'username': user_key,
-                    'itsystem': itsystem,
-                    'from_date': from_date,
-                    'to_date': to_date
+                    "uuid": uuid,
+                    "user": user_uuid,
+                    "unit": unit_uuid,
+                    "username": user_key,
+                    "itsystem": itsystem,
+                    "from_date": from_date,
+                    "to_date": to_date,
                 }
             )
     return it_connections
@@ -301,18 +308,15 @@ def test_itconnections_equivalence(full_history, skip_past):
 
 
 def _old_cache_lora_related(self):
-    params = {'gyldighed': 'Aktiv', 'funktionsnavn': 'Relateret Enhed'}
-    url = '/organisation/organisationfunktion'
+    params = {"gyldighed": "Aktiv", "funktionsnavn": "Relateret Enhed"}
+    url = "/organisation/organisationfunktion"
     related_list = self._perform_lora_lookup(url, params, unit="related")
     related = {}
     for relate in tqdm(related_list, desc="Processing related", unit="related"):
-        uuid = relate['id']
+        uuid = relate["id"]
         related[uuid] = []
 
-        relevant = {
-            'relationer': ('tilknyttedeenheder',),
-            'attributter': ()
-        }
+        relevant = {"relationer": ("tilknyttedeenheder",), "attributter": ()}
 
         effects = self._get_effects(relate, relevant)
         for effect in effects:
@@ -320,17 +324,17 @@ def _old_cache_lora_related(self):
             if from_date is None and to_date is None:
                 continue
 
-            rel = effect[2]['relationer']
-            unit1_uuid = rel['tilknyttedeenheder'][0]['uuid']
-            unit2_uuid = rel['tilknyttedeenheder'][1]['uuid']
-            print(len(rel['tilknyttedeenheder']))
+            rel = effect[2]["relationer"]
+            unit1_uuid = rel["tilknyttedeenheder"][0]["uuid"]
+            unit2_uuid = rel["tilknyttedeenheder"][1]["uuid"]
+            print(len(rel["tilknyttedeenheder"]))
             related[uuid].append(
                 {
-                    'uuid': uuid,
-                    'unit1_uuid': unit1_uuid,
-                    'unit2_uuid': unit2_uuid,
-                    'from_date': from_date,
-                    'to_date': to_date
+                    "uuid": uuid,
+                    "unit1_uuid": unit1_uuid,
+                    "unit2_uuid": unit2_uuid,
+                    "from_date": from_date,
+                    "to_date": to_date,
                 }
             )
     return related
