e9ab72e3f122e5bae1dd6a7700e44cb55f417a15
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-09-06 15:05:33 +0200

    Simplify api_cli

--- tools/api_cli.py
+++ tools/api_cli.py
@@ -1,35 +1,27 @@
-from pprint import pprint
+import json
 
 import click
 import requests
 from ra_utils.headers import TokenSettings
-from ra_utils.load_settings import load_settings
 
 
 @click.command()
 @click.argument("url", nargs=1)
-@click.option(
-    "--client-secret", envvar="CLIENT_SECRET", help="Keycloak secret for the dipex id"
-)
-def cli(url, client_secret):
+def cli(url):
     """CLI tool to call MOs api with keycloak token
 
     Takes an URL as argument, and prints the json returned by MO.
-    Uses the envvar CLIENT_SECRET or fetches from the value
-    crontab.client_secret in settings.json
-
+    Uses the envvar CLIENT_SECRET
     Example:
         metacli api_cli http://localhost:5000/service/o/
     """
 
-    session = requests.session()
+    headers = TokenSettings().get_headers()
 
-    client_secret = client_secret or load_settings().settings["crontab.client_secret"]
-    session.headers = TokenSettings().get_headers()
-
-    r = session.get(url)
+    r = requests.get(url, headers=headers)
     r.raise_for_status()
-    pprint(r.json())
+    print(r.json())
+    click.echo(json.dumps(r.json(), indent=2))
 
 
 if __name__ == "__main__":
