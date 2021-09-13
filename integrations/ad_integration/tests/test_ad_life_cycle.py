0b383752dc2292a2514a196a32ef05a717761127
Mads Riis-Weitling <mrw@magenta.dk>
2021-09-13 15:34:33 +0200

    [#44726] Log MO users skipped by `AdLifeCycle.create_ad_accounts` in `stats` dictionary
    This commit also updates the tests accordingly, and adds a test for the
    case when a MO user is filtered by a "create filter" (custom filter
    expressed as a Jinja template.)
    
    [#44726] Use decorator `log_skipped` instead, as per review suggestion
    
    [#44726] Incorporate review suggestions; add tests for `disable_ad_accounts`
    
    This also rewrites the tests for `create_ad_accounts` as one
    parameterized test.

--- integrations/ad_integration/tests/test_ad_life_cycle.py
+++ integrations/ad_integration/tests/test_ad_life_cycle.py
@@ -1,7 +1,11 @@
+from typing import Callable
+from typing import Dict
+from typing import Optional
 from unittest import mock
 from unittest import TestCase
 
 from more_itertools import first_true
+from parameterized import parameterized
 
 from .. import ad_life_cycle
 from ..ad_exceptions import NoPrimaryEngagementException
@@ -25,62 +29,216 @@ def default_find_primary_engagement(mo_user_uuid):
 class TestAdLifeCycle(TestCase, TestADWriterMixin):
     """Test `ad_life_cycle`"""
 
+    maxDiff = None
+
     def setUp(self):
         super().setUp()
         self._setup_adwriter()
 
-    def test_create_ad_accounts_empty_ad(self):
-        """An AD user must be created when the AD is empty.
-
-        In this test, we mock an empty AD. Therefore we expect exactly one AD
-        write (`New-ADUser`) to be performed.
-        """
-        instance = self._get_instance()
-        stats = instance.create_ad_accounts()
-        self._assert_stats_equal(
-            stats,
-            created_users=1,
-            users={self._prepare_static_person()["uuid"]},
-        )
-        # Assert the proper AD writes were issued: 2 reads (?) and 1 write.
-        self._assert_ad_write(num_scripts=3, expected_script_content="New-ADUser")
-
-    def test_create_ad_accounts_empty_ad_and_no_primary_engagement(self):
-        """An AD user must *not* be created when the MO user has no primary
-        engagement.
-
-        In this test, the AD is empty, but the MO user has no primary
-        engagements. Consequently, no AD users are created.
-        """
-        instance = self._get_instance(
-            find_primary_engagement=mock.Mock(
-                side_effect=NoPrimaryEngagementException()
+    @parameterized.expand(
+        [
+            # 1. An AD user must be created when the AD is empty.
+            # In this test, we mock an empty AD. Therefore we expect exactly
+            # one AD write (`New-ADUser`) to be performed.
+            (
+                # Empty `_get_instance` kwargs
+                {},
+                # Expected stats
+                lambda instance: {
+                    "created_users": 1,
+                    "users": {instance._prepare_static_person()["uuid"]},
+                },
+                # Assert the proper AD writes were issued: 2 reads (?) and 1
+                # write
+                {"num_scripts": 3, "expected_script_content": "New-ADUser"},
             ),
+            # 2. An AD user must *not* be created when the MO user has no
+            # primary engagement.
+            # In this test, the AD is empty, but the MO user has no primary
+            # engagements. Consequently, no AD users are created.
+            (
+                # Mock a MO user without a primary engagement
+                {
+                    "find_primary_engagement": mock.Mock(
+                        side_effect=NoPrimaryEngagementException()
+                    ),
+                },
+                # Expected stats
+                lambda instance: {
+                    "not_in_user_tree": 1,
+                    "skipped": instance._get_expected_skipped_entry(
+                        "filter_users_outside_unit_tree"
+                    ),
+                },
+                # Assert no AD writes were done
+                None,
+            ),
+            # 3. An AD user must *not* be created when `users_with_engagements`
+            # is empty.
+            (
+                # Mock a MO user without *any* engagements
+                {"users_with_engagements": {}},
+                # Expected stats
+                lambda instance: {
+                    "no_active_engagements": 1,
+                    "skipped": instance._get_expected_skipped_entry(
+                        "filter_user_without_engagements"
+                    ),
+                },
+                # Assert no AD writes were done
+                None,
+            ),
+            # 4. An AD user must *not* be created if the AD user already
+            # exists.
+            (
+                # In this test, the MO user has already been "created" in AD,
+                # as the `MockADReader` always returns the same "person"
+                # regardless of the CPR number looked up.
+                {"reader": MockADParameterReader()},
+                # Expected stats
+                lambda instance: {
+                    "already_in_ad": 1,
+                    "skipped": instance._get_expected_skipped_entry(
+                        "filter_user_already_in_ad"
+                    ),
+                },
+                # Assert no AD writes were done
+                None,
+            ),
+            # 5. An AD user must *not* be created if at least one "create
+            # filter" returns `True` for the corresponding MO user.
+            (
+                # A "create filter" is a Jinja template evaluating to `True` or
+                # `False`. Here is a "create filter" which never returns `True`
+                # for our test MO user.
+                {"create_filters": ["{{ employee['employment_number'] == '1st' }}"]},
+                # Expected stats
+                lambda instance: {
+                    "create_filtered": 1,
+                    "skipped": instance._get_expected_skipped_entry(
+                        "create_filters_num_0"
+                    ),
+                },
+                # Assert no AD writes were done
+                None,
+            ),
+        ]
+    )
+    def test_create_ad_accounts(
+        self,
+        instance_kwargs: Dict,
+        expected_stats_func: Callable,
+        expected_ad_writes: Optional[Dict],
+    ):
+        instance = self._get_instance(**instance_kwargs)
+        stats = instance.create_ad_accounts()
+        self._assert_stats_equal(stats, **expected_stats_func(self))
+        if expected_ad_writes:
+            self._assert_ad_write(**expected_ad_writes)
+        else:
+            self._assert_no_ad_writes()
+
+    @parameterized.expand(
+        [
+            # 1. If the AD user exists, and the MO user has *no* engagements,
+            # the AD user should be disabled.
+            (
+                {
+                    # Pretend AD user already exists
+                    "reader": MockADParameterReader(),
+                    # Pretend the MO user has no engagements
+                    "users_with_engagements": {},
+                },
+                # Expected stats
+                lambda instance: {
+                    "disabled_users": 1,
+                    "users": {instance._prepare_static_person()["uuid"]},
+                },
+                # Assert one AD write is done, which disables the AD user
+                {"num_scripts": 1, "expected_script_content": "Disable-ADAccount"},
+            ),
+            # 2. If the AD user exists, and the MO user has *no* engagements,
+            # *but* a "disable filter" filters out the MO user, the AD user
+            # should *not* be disabled.
+            (
+                {
+                    # Pretend AD user already exists
+                    "reader": MockADParameterReader(),
+                    # Pretend the MO user has no engagements
+                    "users_with_engagements": {},
+                    # Add a "disable filter" which evaluates to `False`, which
+                    # means this user should be skipped in
+                    # `AdLifeCycle.disable_ad_accounts`.
+                    "disable_filters": ["{{ employee['employment_number'] == '1st' }}"],
+                },
+                # Expected stats
+                lambda instance: {
+                    "skipped": instance._get_expected_skipped_entry(
+                        "disable_filters_num_0"
+                    ),
+                },
+                # Assert no AD writes were done
+                None,
+            ),
+            # 3. If the AD user exists, but the MO user *has* engagements, the
+            # AD user should *not* be disabled.
+            (
+                {
+                    # Pretend AD user already exists
+                    "reader": MockADParameterReader(),
+                    # Pretend the MO user *has* engagements
+                    "users_with_engagements": None,
+                },
+                # Expected stats
+                lambda instance: {"disabled_users": 0, "users": set()},
+                # Assert no AD writes were done
+                None,
+            ),
+            # 4. If the AD user does not exist, and the MO does not have any
+            # engagements, the AD user should *not* be disabled.
+            (
+                {
+                    # Pretend AD does not exist
+                    "reader": None,
+                    # Pretend the MO user has no engagements
+                    "users_with_engagements": {},
+                },
+                # Expected stats
+                lambda instance: {"disabled_users": 0, "users": set()},
+                # Assert no AD writes were done
+                None,
+            ),
+        ]
+    )
+    def test_disable_ad_accounts(
+        self,
+        instance_kwargs: Dict,
+        expected_stats_func: Callable,
+        expected_ad_writes: Optional[Dict],
+    ):
+        """An AD user must *not* be disabled if present in
+        `users_with_engagements`.
+        An AD user can only be disabled if already present in the AD.
+        """
+        instance = self._get_instance(**instance_kwargs)
+        stats = instance.disable_ad_accounts()
+        self._assert_stats_equal(stats, **expected_stats_func(self))
+        if expected_ad_writes:
+            self._assert_ad_write(**expected_ad_writes)
+        else:
+            self._assert_no_ad_writes()
+
+    def test_disable_ad_accounts_handles_status(self):
+        """If disabling an AD user fails, the status should be recorded."""
+        # Get an `AdLifeCycle` instance which is set up to disable one AD user
+        instance = self._get_instance(
+            reader=MockADParameterReader(), users_with_engagements={}
         )
-        stats = instance.create_ad_accounts()
-        self._assert_stats_equal(stats, not_in_user_tree=1)
-        self._assert_no_ad_writes()
-
-    def test_create_ad_accounts_filter_user_without_engagements(self):
-        """An AD user must *not* be created when `users_with_engagements` is
-        empty.
-        """
-        instance = self._get_instance(users_with_engagements={})
-        stats = instance.create_ad_accounts()
-        self._assert_stats_equal(stats, no_active_engagements=1)
-        self._assert_no_ad_writes()
-
-    def test_create_ad_accounts_user_already_in_ad(self):
-        """An AD user must *not* be created if the AD user already exists.
-
-        In this test, the MO user has already been "created" in AD, as the
-        `MockADReader` always returns the same "person" regardless of the CPR
-        number looked up.
-        """
-        instance = self._get_instance(reader=MockADParameterReader())
-        stats = instance.create_ad_accounts()
-        self._assert_stats_equal(stats, already_in_ad=1)
-        self._assert_no_ad_writes()
+        # But replace `ADWriter.enable_user` with a function indicating a
+        # failed AD update.
+        instance.ad_writer.enable_user = lambda **kwargs: (False, "message")
+        stats = instance.disable_ad_accounts()
+        self._assert_stats_equal(stats, disabled_users=0, critical_errors=1)
 
     def test_gen_filtered_employees(self):
         """The lazy dicts produced by `_gen_filtered_employees` must be able
@@ -95,15 +253,33 @@ class TestAdLifeCycle(TestCase, TestADWriterMixin):
                 # properties.
                 self.assertIsInstance(dict(engagement), dict)
 
+    def test_skip_occupied_names_check(self):
+        """If `skip_occupied_names_check` is passed, don't call
+        `ADParameterReader.cache_all`.
+        """
+        reader = MockADParameterReader()
+        reader.cache_all = mock.Mock()
+        self._get_instance(reader=reader, skip_occupied_names_check=True)
+        reader.cache_all.assert_not_called()
+
     def _get_instance(
         self,
         reader=None,
         find_primary_engagement=None,
         users_with_engagements=None,
+        create_filters=None,
+        disable_filters=None,
+        **kwargs,
     ):
         settings = {
             "integrations.ad.write.create_user_trees": [MO_ROOT_ORG_UNIT_UUID],
         }
+
+        if create_filters:
+            settings["integrations.ad.lifecycle.create_filters"] = create_filters
+
+        if disable_filters:
+            settings["integrations.ad.lifecycle.disable_filters"] = disable_filters
 
         # Replace `find_primary_engagement` with our mocked version
         self.ad_writer.datasource.find_primary_engagement = (
@@ -138,7 +314,7 @@ class TestAdLifeCycle(TestCase, TestADWriterMixin):
             with lora_cache_mock:
                 with reader_mock:
                     with writer_mock:
-                        instance = ad_life_cycle.AdLifeCycle()
+                        instance = ad_life_cycle.AdLifeCycle(**kwargs)
                         # Replace `users_with_engagements` dict attr with our
                         # mocked version.
                         if users_with_engagements is not None:
@@ -170,3 +346,7 @@ class TestAdLifeCycle(TestCase, TestADWriterMixin):
             self.ad_writer.scripts,
             pred=lambda script: expected_script_content in script,
         )
+
+    def _get_expected_skipped_entry(self, filtername):
+        mo_user = self._prepare_static_person()
+        return {filtername: {mo_user["uuid"]: " ".join(mo_user["name"])}}
