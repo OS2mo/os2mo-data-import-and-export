570f5fdc2696b6a582f01a2b5555690c449126f2
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-09-28 15:06:36 +0200

    lint

--- integrations/opus/tests/test_opus_helpers.py
+++ integrations/opus/tests/test_opus_helpers.py
@@ -1,18 +1,15 @@
-from copy import deepcopy
 import unittest
 from collections import OrderedDict
-from datetime import datetime
+from copy import deepcopy
 from pathlib import Path
 from unittest import TestCase
-from unittest.mock import MagicMock, call, patch
 
-import xmltodict
 from hypothesis import given
-from hypothesis.strategies import datetimes, dictionaries, text
-from parameterized import parameterized, parameterized_class
+from hypothesis.strategies import text
+from parameterized import parameterized
 
 from integrations.opus import opus_helpers
-from integrations.opus.opus_diff_import import start_opus_diff
+
 
 testfile1 = Path.cwd() / "integrations/opus/tests/ZLPETESTER_delta.xml"
 testfile2 = Path.cwd() / "integrations/opus/tests/ZLPETESTER2_delta.xml"
@@ -37,7 +34,9 @@ class test_opus_helpers(TestCase):
         self.assertEqual(employees, [])
 
     def test_file_diff(self):
-        units, employees = opus_helpers.file_diff(testfile1, testfile2, disable_tqdm=True)
+        units, employees = opus_helpers.file_diff(
+            testfile1, testfile2, disable_tqdm=True
+        )
         self.assertNotEqual(units, [])
         self.assertNotEqual(employees, [])
 
@@ -74,8 +73,6 @@ class test_opus_helpers(TestCase):
         ]
     )
     def test_split_employees_active(self, inputfile, expected):
-        filter_ids = ["1"]
-        all_filtered_ids = opus_helpers.find_all_filtered_ids(inputfile, filter_ids)
         units, employees = opus_helpers.file_diff(None, inputfile, disable_tqdm=True)
         emplyees, leaves = opus_helpers.split_employees_leaves(employees)
         employees = list(employees)
@@ -92,14 +89,13 @@ class test_opus_helpers(TestCase):
         data = opus_helpers.read_and_transform_data(None, testfile1, filter_ids)
         for i, x in enumerate(data):
             this = list(x)
-            l = len(this)
-            self.assertEqual(l, expected[i])
-    
-    
+            length = len(this)
+            self.assertEqual(length, expected[i])
+
     def test_find_changed_parent(self):
         org1, _ = opus_helpers.parser(testfile1)
         org2 = deepcopy(org1)
-        org2[2]['parentOrgUnit'] = "CHANGED!"
+        org2[2]["parentOrgUnit"] = "CHANGED!"
         diffs = opus_helpers.find_changes(before=org1, after=org2, disable_tqdm=True)
         self.assertEqual(diffs, [org2[2]])
 
