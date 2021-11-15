290c6337273ee0513e5545b97e366ffa9e66a184
Andreas Kring <andreas@magenta.dk>
2021-11-15 15:09:33 +0100

    WIP: Re-introduced batch comparing

--- tools/data_watchers/sd_employment_checker.py
+++ tools/data_watchers/sd_employment_checker.py
@@ -11,19 +11,28 @@ mora_helper = MoraHelper(use_cache=False)
 # def get_mo_employees(limit: int = 0, start: int = 0) -> List[dict]:
 #     return mora_helper.read_all_users(limit=limit, start=start)
 
+def compare_mo_to_sd(mo_employee_batch: List[dict]):
+    for mo_employee in mo_employee_batch:
+        mo_engagement = mora_helper.read_user_engagement(user=mo_employee["uuid"])
+        sd_employment = fetch_sd_employments(mo_employee)
+
+        print(mo_engagement)
+        print(sd_employment)
+
+
 @click.group()
 def cli():
     pass
 
 
 @cli.command()
-# @click.option(
-#     "--bulk-size",
-#     type=click.INT,
-#     help="The number of employees to fetch per request from MO",
-#     default=50
-# )
-def check_employment():
+@click.option(
+    "--bulk-size",
+    type=click.INT,
+    help="The number of employees to fetch per request from MO",
+    default=50
+)
+def check_employment(bulk_size):
     """
     Compare MO engagement end dates with SD employment end dates for all users
     and log any inconsistencies.
@@ -32,22 +41,14 @@ def check_employment():
     #       bulk_size: The number of employees to fetch from MO at a time.
     """
 
-    # start = 0
-    # mo_employee_batch = mora_helper.read_all_users(limit=bulk_size, start=start)
-    # while mo_employee_batch:
-    #     print(start)
-    #     start += bulk_size
-    #     mo_employee_batch = mora_helper.read_all_users(
-    #         limit=bulk_size, start=start
-    #     )
-
-    mo_employees = mora_helper.read_all_users()
-    for mo_employee in mo_employees[1:2]:
-        mo_engagement = mora_helper.read_user_engagement(user=mo_employee["uuid"])
-        sd_employment = fetch_sd_employments(mo_employee)
-
-        print(mo_engagement)
-        print(sd_employment)
+    start = 0
+    mo_employee_batch = mora_helper.read_all_users(limit=bulk_size, start=start)
+    while mo_employee_batch:
+        compare_mo_to_sd(mo_employee_batch)
+        start += bulk_size
+        mo_employee_batch = mora_helper.read_all_users(
+            limit=bulk_size, start=start
+        )
 
 
 if __name__ == "__main__":
