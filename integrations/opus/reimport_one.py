7444037bba7d147a092b01ccc90356b39b677818
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2021-12-29 10:04:14 +0100

    Automatically find MO uuid and object type.

--- integrations/opus/reimport_one.py
+++ integrations/opus/reimport_one.py
@@ -1,32 +1,65 @@
 import click
+from more_itertools import only
+from os2mo_helpers.mora_helpers import MoraHelper
 from ra_utils.load_settings import load_setting
 
+from integrations.opus import opus_helpers
 from integrations.opus.clear_and_import_opus import import_opus
+from integrations.opus.opus_file_reader import get_opus_filereader
 from tools.data_fixers.remove_from_lora import delete_object_and_orgfuncs
 
-#TODO: Would be nice to be able to drop object-type and to make it able to reimport based on cpr or uuid also.
+
+def find_type(opus_id, full_history):
+    dumps = get_opus_filereader().list_opus_files()
+    # Default is to read only from latest file
+    opus_files = [max(dumps)]
+    if full_history:
+        opus_files = sorted(dumps, reverse=True)
+    for f in opus_files:
+        org, emp = opus_helpers.parser(dumps[f], opus_id=opus_id)
+        if org:
+            return "organisationenhed", only(org)
+        elif emp:
+            return "bruger", only(emp)
+
+    # No object was found
+    return None, None
+
+
 @click.command()
 @click.option("--mox-base", default=load_setting("mox.base"))
-@click.option("--delete-uuid", type=click.UUID)
+@click.option("--mora-base", default=load_setting("mora.base"))
+@click.option("--delete", is_flag=True)
+@click.option("--full-history", is_flag=True)
 @click.option("--opus-id", type=int, required=True)
-@click.option(
-    "--object-type",
-    type=click.Choice(["user", "org_unit"], case_sensitive=False),
-    required=True,
-)
-def cli(mox_base, delete_uuid, opus_id, object_type):
-    """Reimport object from opus
-    Reads all the dumps from opus and imports object with given ID to MO
-    Optionally deletes the object with given uuid directly from Lora.
+@click.option("--dry-run", is_flag=True)
+def cli(mox_base, mora_base, delete, full_history, opus_id, dry_run):
+    """Reimport object from opus with given opus-ID to MO
+    Optionally deletes the object and all related orgfuncs directly from Lora.
+    Defaults to reading latest file only, but supports reading full history
     """
+    helper = MoraHelper(hostname=mora_base)
+    object_type, obj = find_type(opus_id, full_history)
+    assert obj, f"No object with {opus_id=} found"
+    if object_type == "bruger":
+        cpr = opus_helpers.read_cpr(obj)
+        uuid = helper.read_user(user_cpr=cpr)["uuid"]
+    else:
+        uuid = opus_helpers.generate_uuid(obj["@id"])
 
-    if delete_uuid:
-        delete_object_and_orgfuncs(
-            delete_uuid,
-            mox_base,
-            "bruger" if object_type == "user" else "organisationsenhed",
+    if delete and not dry_run:
+        delete_object_and_orgfuncs(uuid, mox_base, object_type)
+    if dry_run:
+        click.echo(
+            f"Dry-run: {'Delete and reimport' if delete else 'Reimport'} '{object_type}' with {uuid=}"
         )
-    import_opus(ad_reader=None, import_all=True, opus_id=opus_id)
+    else:
+        import_opus(
+            ad_reader=None,
+            import_all=full_history,
+            import_last=not full_history,
+            opus_id=opus_id,
+        )
 
 
 if __name__ == "__main__":
