be8d19216bcd10a2ec12b3c7ae202082b6d103ef
Emil Madsen <emil@magenta-aps.dk>
2022-01-18 10:57:12 +0100

    Update calculate_primary.py

--- integrations/calculate_primary/calculate_primary.py
+++ integrations/calculate_primary/calculate_primary.py
@@ -27,7 +27,7 @@ def setup_logging():
 
 def get_engagement_updater(integration):
     if integration == "DEFAULT":
-        from integrations.calculate_primary.sd import DefaultPrimaryEngagementUpdater
+        from integrations.calculate_primary.default import DefaultPrimaryEngagementUpdater
 
         return DefaultPrimaryEngagementUpdater
     if integration == "SD":
