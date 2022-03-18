8fa2b7f6279b6eb77bf9c15ab4122a9850644f7c
Andreas Kring <andreas@magenta.dk>
2022-03-18 10:55:50 +0100

    [#49146] Fixed broken test_sd_connectivity script

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+Release type: patch
+
+[#49146] Fixed broken test-sd-connectivity script
--- integrations/SD_Lon/test_sd_connectivity.py
+++ integrations/SD_Lon/test_sd_connectivity.py
@@ -1,10 +1,10 @@
 import logging
-import pathlib
 from uuid import UUID
 
 import click
+from pydantic import ValidationError
 
-from integrations.SD_Lon.sd_common import load_settings
+from integrations.SD_Lon.config import get_changed_at_settings
 from integrations.SD_Lon.sd_common import sd_lookup
 
 
@@ -29,126 +29,35 @@ def setup_logging():
 
 class TestSdConnectivity(object):
     def __init__(self):
+        self.validation_error = None
         try:
-            self.settings = load_settings()
-        except Exception as e:
-            print("Syntax error in settings file: {}".format(e))
-            exit(1)
+            self.settings = get_changed_at_settings()
+        except ValidationError as err:
+            self.validation_error = err
 
-    def _check_existens_of_keys(self):
-        print("Check for tilstedeværlse af konfigurationsøgler")
-        needed_keys = [
-            "integrations.SD_Lon.sd_user",
-            "integrations.SD_Lon.sd_password",
-            "integrations.SD_Lon.institution_identifier",
-            "integrations.SD_Lon.import.too_deep",
-            "integrations.SD_Lon.global_from_date",
-            "integrations.SD_Lon.monthly_hourly_divide",
-            "integrations.SD_Lon.job_function",
-            "integrations.SD_Lon.import.run_db",
-        ]
+    def _check_sd_settings(self):
+        print("Check settings...")
 
-        missing_keys = []
-        for key in needed_keys:
-            if self.settings.get(key) is None:
-                missing_keys.append(key)
-        if missing_keys:
-            print(" * Manglende nøgler: {}".format(missing_keys))
-            exit(1)
+        if self.validation_error is None:
+            print("SD settings OK")
         else:
-            print(" * Alle nødvendige nøgler fundet")
+            print("The following SD settings errors were detected:")
+            print(self.validation_error)
 
-        nice_to_have_keys = [
-            "integrations.SD_Lon.employment_field",
-            "integrations.SD_Lon.import.manager_file",
-            "integrations.SD_Lon.skip_employment_types",
-        ]
-        wanted_keys = []
-        for key in nice_to_have_keys:
-            if self.settings.get(key) is None:
-                wanted_keys.append(key)
-        if wanted_keys:
-            print("Disse ikke-kritiske nøgler mangler: {}".format(missing_keys))
-        else:
-            print(" * Alle ikke-kritiske nøgler fundet")
-        print()
-
-    def _check_legal_values(self):
-        print("Tjekker at settings-værdier er gyldige:")
-
-        legal_job_functions = ["EmploymentName", "JobPositionIdentifier"]
-        legal_extensions = [
-            "extension_1",
-            "extension_2",
-            "extension_3",
-            "extension_4",
-            "extension_5",
-            "extension_6",
-            "extension_7",
-            "extension_8",
-            "extension_9",
-            "extension_10",
-        ]
-
-        job_function = self.settings["integrations.SD_Lon.job_function"]
-        if job_function in legal_job_functions:
-            print(" * Job function nøgle er korrekt")
-        else:
-            print(" * Job function nøgle skal være{}".format(legal_job_functions))
-            exit(1)
-
-        employment_field = self.settings.get("integrations.SD_Lon.employment_field")
-        if employment_field is None:
-            print(" * Der skrives ikke stillingsbetegnelse til udvidelsesfelt")
-        else:
-            if employment_field in legal_extensions:
-                print(" * Udvidelsesfelt til stillingsbetegnelse er korrekt")
-            else:
-                msg = " * Udvidelsesfelt til stillingsbetegnelse skal være {}"
-                print(msg.format(legal_extensions))
-                exit(1)
-        print()
-
-        print("Tjekker indstillinger for ledere")
-        manager_file = self.settings.get("integrations.SD_Lon.import.manager_file")
-        if manager_file is not None:
-            manager_path = pathlib.Path(manager_file)
-            if manager_path.is_file():
-                print(" * Specificeret lederfil er fundet")
-            else:
-                print(" * Specificeret lederfil er ikke fundet")
-                exit(1)
-            if manager_path.suffix == ".csv":
-                print(" * Specificeret lederfil er korrekt en csv-fil")
-            else:
-                print(" * Specificeret lederfil skal være en csv-fil")
-                exit(1)
-        else:
-            print(" * Der er ikke specificeret en lederfil")
-        print()
-
-        skip_job_functions = self.settings.get(
-            "integrations.SD_Lon.skip_employment_types"
-        )
-        if skip_job_functions is not None:
-            if not isinstance(skip_job_functions, list):
-                print("skip_employment_types skal være en liste")
-                exit(1)
-            for job_function in skip_job_functions:
-                try:
-                    int(job_function)
-                except ValueError:
-                    print("All elementer i skip_employment_types skal være tal")
-                    exit(1)
-            print(" * skip_employment_types er korrekt")
+        print("Done checking SD settings\n\n")
 
     def _check_contact_to_sd(self):
         print("Tjekker at vi har kontakt til SD:")
-        inst_id = self.settings["integrations.SD_Lon.institution_identifier"]
-        params = {"UUIDIndicator": "true", "InstitutionIdentifier": inst_id}
+        params = {
+            "UUIDIndicator": "true",
+            "InstitutionIdentifier": self.settings.sd_institution_identifier,
+        }
         try:
             institution_info = sd_lookup(
-                "GetInstitution20111201", params, use_cache=False
+                "GetInstitution20111201",
+                settings=self.settings,
+                params=params,
+                use_cache=False,
             )
         except Exception as e:
             print("Fejl i kontakt til SD Løn: {}".format(e))
@@ -165,8 +74,7 @@ class TestSdConnectivity(object):
             exit(1)
 
     def sd_check(self):
-        self._check_existens_of_keys()
-        self._check_legal_values()
+        self._check_sd_settings()
         self._check_contact_to_sd()
 
 
