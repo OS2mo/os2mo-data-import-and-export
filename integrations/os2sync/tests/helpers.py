badf71110f311a648b1939eabc3f5e6d0f60f1ce
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-03-24 11:15:28 +0100

    Patch dummy config to ignore settings.json during unittests

--- integrations/os2sync/tests/helpers.py
+++ integrations/os2sync/tests/helpers.py
@@ -1,8 +1,13 @@
+from unittest.mock import patch
+
 from integrations.os2sync.config import get_os2sync_settings
 
-dummy_settings = get_os2sync_settings(
-    municipality="1234", os2sync_top_unit_uuid="269a0339-0c8b-472d-9514-aef952a2b4df"
-)
+# Create dummy settings ignoring any settings.json file.
+with patch("integrations.os2sync.config.load_settings", return_value={}):
+    dummy_settings = get_os2sync_settings(
+        municipality="1234",
+        os2sync_top_unit_uuid="269a0339-0c8b-472d-9514-aef952a2b4df",
+    )
 
 NICKNAME_TEMPLATE = "{% if nickname -%}{{ nickname }}{%- else %}{{ name }}{%- endif %}"
 
