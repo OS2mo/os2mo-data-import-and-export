4a29709054e89904b0231390f78de752c9bc3537
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-04-01 09:59:48 +0200

    Split into two commands

--- integrations/os2sync/cli.py
+++ integrations/os2sync/cli.py
@@ -6,45 +6,42 @@ from requests import HTTPError
 from operator import itemgetter
 from integrations.os2sync import os2sync
 
-@click.command()
-@click.argument("uuid", type=click.UUID)
-@click.option("--mora-base", default=load_setting("mora.base"))
-@click.option("--dry-run", is_flag=True)
-def cli(uuid, mora_base, dry_run):
-    #lookup uuid, try person, then org_unit if person wasn't found
-    settings = get_os2sync_settings()
-    obj_type = None
-    try:
-        obj = os2mo_get(f"{{BASE}}/e/{str(uuid)}/").json()
-        obj_type = "person"
-        
-    except HTTPError as e:
-        assert e.response.status_code == 404
-        obj = os2mo_get(f"{{BASE}}/ou/{str(uuid)}/").json()
-        obj_type = "orgunit"
+@click.group()
+def cli():
+    pass
     
 
-    if obj_type == "person":
+
+
+@cli.command()
+@click.argument("uuid", type=click.UUID)
+@click.option("--dry-run", is_flag=True)
+def update_user(uuid, dry_run):
+    settings = get_os2sync_settings()
+
+    sts_user = get_sts_user(str(uuid),  settings=settings)
         
-        sts_user = get_sts_user(obj["uuid"],  settings=settings)
-        
-        if dry_run:
-            click.echo(sts_user)
-            return
-        
-        if sts_user["Positions"]:
-            os2sync.upsert_user(sts_user)    
-        else:
-            os2sync.delete_user(uuid)
-    elif obj_type == "orgunit":
-        sts_org_unit = get_sts_orgunit(obj["uuid"], settings=settings)
+    if dry_run:
+        click.echo(sts_user)
+        return
     
-        if dry_run:
-            click.echo(sts_org_unit)
-            return
-        
+    if sts_user["Positions"]:
+        os2sync.upsert_user(sts_user)    
     else:
-        raise ValueError("WTF happend here!?")
+        os2sync.delete_user(str(uuid))
+
+@cli.command()
+@click.argument("uuid", type=click.UUID)
+@click.option("--dry-run", is_flag=True)
+def update_org_unit(uuid, dry_run):     
+    settings = get_os2sync_settings()
+
+    sts_org_unit = get_sts_orgunit(str(uuid), settings=settings)
+
+    if dry_run:
+        click.echo(sts_org_unit)
+        return
+        
 
 
 if __name__ == "__main__":
