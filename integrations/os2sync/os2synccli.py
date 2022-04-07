2ea45568d68f663c5d241cb02045c774b120ac04
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-04-07 14:31:53 +0200

    Rename and add comments to improve connection with metacli

--- integrations/os2sync/os2synccli.py
+++ integrations/os2sync/os2synccli.py
@@ -15,6 +15,7 @@ def cli():
 @click.argument("uuid", type=click.UUID)
 @click.option("--dry-run", is_flag=True)
 def update_user(uuid, dry_run):
+    """Send os2sync payload for a single user"""
     settings = get_os2sync_settings()
 
     sts_user = get_sts_user(str(uuid), settings=settings)
@@ -33,6 +34,7 @@ def update_user(uuid, dry_run):
 @click.argument("uuid", type=click.UUID)
 @click.option("--dry-run", is_flag=True)
 def update_org_unit(uuid, dry_run):
+    """Send os2sync payload for a single org_unit"""
     settings = get_os2sync_settings()
 
     sts_org_unit = get_sts_orgunit(str(uuid), settings=settings)
