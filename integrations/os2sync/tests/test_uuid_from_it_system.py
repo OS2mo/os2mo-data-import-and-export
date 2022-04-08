967e3dd5781a1a0cb14c9f81f2a279a248b14e75
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-04-08 13:20:42 +0200

    Adds comments to individual testcases

--- integrations/os2sync/tests/test_uuid_from_it_system.py
+++ integrations/os2sync/tests/test_uuid_from_it_system.py
@@ -11,21 +11,35 @@ class TestUUIDFromITSystem():
     ad_uuid = uuid4()
     
     @parameterized.expand([
-        # With no list in settings we allways use mo_uuid
+        # Paramerers are:
+        # settings, it-accounts, expected chosen uuid
+        # With no list in settings and no it-accounts, choose mo_uuid
         ([], [], mo_uuid),
+        # With no list in settings, choose mo_uuid whatever it-accounts exist
         ([], [{"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}],mo_uuid),
         ([], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}], mo_uuid),
         ([], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}, {"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}], mo_uuid),
-        # With one it-system in settings we use the uuid from that it-system or from MO.
+
+        # With one it-system in settings we use the uuid from that it-system or from MO if there are no it-account in that it-system.
+        # No it-accounts - use mo_uuid
         ([FK_org_uuid_it_system], [],mo_uuid),
+        # There is an it-account, it is in settings - chose the value from that
         ([FK_org_uuid_it_system], [{"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}],fk_org_uuid),
+        # There is an it-account, it is not in settings - chose mo_uuid.
         ([FK_org_uuid_it_system], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}], mo_uuid),
+        # There are two it-accounts - chose the value from the correct one.
         ([FK_org_uuid_it_system], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}, {"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}], fk_org_uuid),
+        
         # With two it-systems in settings we use the uuid from the first, if it exists, then second, else from MO.
+        # There are no it-accounts, use mo_uuid
         ([FK_org_uuid_it_system, AD_objectguid_it_system], [], mo_uuid),
+        # There is one account, it is in settings, use the uuid from that.
         ([FK_org_uuid_it_system, AD_objectguid_it_system], [{"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}],fk_org_uuid),
+        # There is one account, it is in settings, but second in the list, use the uuid from that it account.
         ([FK_org_uuid_it_system, AD_objectguid_it_system], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}], ad_uuid),
+        # There are two it-accounts, both it-systems are in settings, use the value from the first in the settings list.
         ([FK_org_uuid_it_system, AD_objectguid_it_system], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}, {"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}], fk_org_uuid),
+        # Same as above, but with settings list reversed to test we use the value from the first in the settings list.
         ([AD_objectguid_it_system, FK_org_uuid_it_system], [{"uuid": ad_uuid, "itsystem": {"name": AD_objectguid_it_system}}, {"uuid": fk_org_uuid, "itsystem": {"name": FK_org_uuid_it_system}}], ad_uuid),
     ])
     def test_get_uuid(self, prioritized_list, it_systems, expected):
