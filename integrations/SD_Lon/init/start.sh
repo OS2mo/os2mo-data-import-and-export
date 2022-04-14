34913538ffde79370a485d62d196becba37cc825
Emil Madsen <emil@magenta.dk>
2022-04-14 17:49:52 +0200

    [#49668] Fix bad dateformat produced by init container

--- integrations/SD_Lon/init/start.sh
+++ integrations/SD_Lon/init/start.sh
@@ -5,5 +5,5 @@ SD_IMPORT_RUN_DB=${SD_IMPORT_RUN_DB:-/mnt/dipex/run_db.sqlite}
 if [ ! -f ${SD_IMPORT_RUN_DB} ]; then
     echo "${SD_IMPORT_RUN_DB} not found, creating!"
     sqlite3 ${SD_IMPORT_RUN_DB} "CREATE TABLE runs (id INTEGER PRIMARY KEY, from_date timestamp, to_date timestamp, status text);"
-    sqlite3 ${SD_IMPORT_RUN_DB} "INSERT INTO runs VALUES (1, \"${SD_GLOBAL_FROM_DATE}\", \"${SD_GLOBAL_FROM_DATE}\", \"Initializing RunDB\")"
+    sqlite3 ${SD_IMPORT_RUN_DB} "INSERT INTO runs VALUES (1, \"${SD_GLOBAL_FROM_DATE} 00:00:00.000\", \"${SD_GLOBAL_FROM_DATE} 00:00:00.001\", \"Initializing RunDB\")"
 fi
