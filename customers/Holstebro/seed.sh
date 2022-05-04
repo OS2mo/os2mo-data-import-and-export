eaa8e0af15b3db9e4827677d01b3e632cae5e5cc
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-05-04 20:05:06 +0200

    #49640 Holstebro MED klasser

---
+++ customers/Holstebro/seed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MOX_URL="${MOX_URL:-http://localhost:8080}"
+MORA_URL="${MORA_URL:-http://localhost:5000}"
+CLI="python os2mo_data_import/mox_helpers/mox_util.py cli --mox-base ${MOX_URL}"
+
+${CLI} bulk-ensure customers/Holstebro/holstebro.json
+
+# Configure MO to utilize newly created facet
+TOP_LEVEL_UUID=$($CLI ensure-facet-exists --bvn "hovedorganisation" | cut -f1 -d' ')
+echo "${TOP_LEVEL_UUID} - Put this as association_dynamic_facets in OS2MOs configuration"
+
