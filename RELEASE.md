095123f861d91f10da75eb40880fee8b1c4873e9
Mads Riis-Weitling <mrw@magenta.dk>
2022-08-25 15:58:09 +0200

    [#51764] LOS import: remove creation of "AD" IT users in MO

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+Release type: minor
+
+[#51764] LOS import: remove creation of "AD" IT users in MO
--- integrations/aarhus/tests/test_los_pers.py
+++ integrations/aarhus/tests/test_los_pers.py
@@ -12,10 +12,28 @@ class TestPersonImporter:
 
     @given(st.builds(los_pers.Person))
     def test_generate_employee_az_id_payload_uses_setting(self, person):
-        importer = self._get_configured_importer()
+        with mock_config(azid_it_system_uuid=self._azid_it_system_uuid):
+            importer = los_pers.PersonImporter()
         payload = importer.generate_employee_az_id_payload(person, "2021-11-17")
         assert payload["itsystem"]["uuid"] == str(self._azid_it_system_uuid)
 
-    def _get_configured_importer(self):
+    @given(
+        st.builds(los_pers.Person),
+        st.one_of(st.just("Ja"), st.just("Nej")),
+    )
+    def test_ad_accounts_are_not_created(self, person, primary):
+        person.primary = primary
         with mock_config(azid_it_system_uuid=self._azid_it_system_uuid):
-            return los_pers.PersonImporter()
+            importer = los_pers.PersonImporter()
+        detail_payloads = importer.create_detail_payloads([person], None)
+        it_user_payloads = [
+            payload for payload in detail_payloads if payload["type"] == "it"
+        ]
+        if person.az_id:
+            assert len(it_user_payloads) == 1
+            assert it_user_payloads[0]["user_key"] == person.az_id
+            assert it_user_payloads[0]["itsystem"]["uuid"] == str(
+                self._azid_it_system_uuid
+            )
+        else:
+            assert len(it_user_payloads) == 0
