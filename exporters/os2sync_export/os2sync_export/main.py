b5fa8ddb2b7406bc1fba6c18d62240a57b78912f
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-11-14 09:15:36 +0100

    feat: add endpoints to trigger updating one user/orgunit

--- exporters/os2sync_export/os2sync_export/main.py
+++ exporters/os2sync_export/os2sync_export/main.py
@@ -1,6 +1,11 @@
+from uuid import UUID
+
 from fastapi import FastAPI
+from fastapi import Query
 from os2sync_export.__main__ import main
 from os2sync_export.config import get_os2sync_settings
+from os2sync_export.os2synccli import update_single_orgunit
+from os2sync_export.os2synccli import update_single_user
 
 app = FastAPI()
 
@@ -11,7 +16,27 @@ async def index() -> dict[str, str]:
 
 
 @app.post("/trigger")
-async def trigger() -> dict[str, str]:
+async def trigger_all() -> dict[str, str]:
     settings = get_os2sync_settings()
     main(settings=settings)
     return {"triggered": "OK"}
+
+
+@app.post("/trigger/user/{uuid}")
+async def trigger_user(
+    uuid: UUID = Query(..., description="UUID of the organisation unit to recalculate"),
+    dry_run: bool = False,
+) -> dict[str, str]:
+    settings = get_os2sync_settings()
+
+    return update_single_user(uuid, settings, dry_run)
+
+
+@app.post("/trigger/orgunit/{uuid}")
+async def trigger_orgunit(
+    uuid: UUID = Query(..., description="UUID of the organisation unit to recalculate"),
+    dry_run: bool = False,
+) -> dict[str, str]:
+    settings = get_os2sync_settings()
+
+    return update_single_orgunit(uuid, settings, dry_run)
--- exporters/os2sync_export/os2sync_export/os2synccli.py
+++ exporters/os2sync_export/os2sync_export/os2synccli.py
@@ -1,8 +1,36 @@
+from typing import Dict
+from uuid import UUID
+
 import click
 from os2sync_export import os2sync
 from os2sync_export.config import get_os2sync_settings
+from os2sync_export.config import Settings
 from os2sync_export.os2mo import get_sts_orgunit
 from os2sync_export.os2mo import get_sts_user
+
+
+def update_single_user(uuid: UUID, settings: Settings, dry_run: bool) -> Dict:
+    sts_user = get_sts_user(str(uuid), settings=settings)
+
+    if dry_run:
+        return sts_user
+
+    if sts_user["Positions"]:
+        os2sync.upsert_user(sts_user)
+    else:
+        os2sync.delete_user(str(uuid))
+    return sts_user
+
+
+def update_single_orgunit(uuid: UUID, settings: Settings, dry_run: bool) -> Dict:
+
+    sts_org_unit = get_sts_orgunit(str(uuid), settings=settings)
+
+    if dry_run:
+        return sts_org_unit
+
+    os2sync.upsert_orgunit(sts_org_unit)
+    return sts_org_unit
 
 
 @click.group()
@@ -13,36 +41,26 @@ def cli():
 @cli.command()
 @click.argument("uuid", type=click.UUID)
 @click.option("--dry-run", is_flag=True)
-def update_user(uuid, dry_run):
+def update_user(uuid: UUID, dry_run: bool):
     """Send os2sync payload for a single user"""
     settings = get_os2sync_settings()
 
-    sts_user = get_sts_user(str(uuid), settings=settings)
-
-    if dry_run:
-        click.echo(sts_user)
-        return
-
-    if sts_user["Positions"]:
-        os2sync.upsert_user(sts_user)
-    else:
-        os2sync.delete_user(str(uuid))
+    click.echo(update_single_user(uuid, settings, dry_run))
 
 
 @cli.command()
 @click.argument("uuid", type=click.UUID)
 @click.option("--dry-run", is_flag=True)
-def update_org_unit(uuid, dry_run):
+def update_org_unit(uuid: UUID, dry_run: bool):
     """Send os2sync payload for a single org_unit"""
     settings = get_os2sync_settings()
-
-    sts_org_unit = get_sts_orgunit(str(uuid), settings=settings)
-
-    if dry_run:
-        click.echo(sts_org_unit)
-        return
-
-    os2sync.upsert_orgunit(sts_org_unit)
+    click.echo(
+        update_org_unit(
+            uuid,
+            settings,
+            dry_run,
+        )
+    )
 
 
 if __name__ == "__main__":
