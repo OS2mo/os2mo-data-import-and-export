fcb1043460de1154e6f88db74d70d295f7dbc5ef
Emil Madsen <emil@magenta.dk>
2022-11-16 16:29:54 +0100

    feat: [#40524] Lock /trigger for non-parallel access

--- exporters/sql_export/trigger.py
+++ exporters/sql_export/trigger.py
@@ -3,9 +3,12 @@
 # SPDX-License-Identifier: MPL-2.0
 """Integration endpoints."""
 import logging
+from threading import Lock
 from typing import Any
 
 from fastapi import APIRouter
+from fastapi import BackgroundTasks
+from fastapi import HTTPException
 from fastapi import Query
 from pydantic import AnyHttpUrl
 from pydantic import BaseSettings
@@ -16,6 +19,8 @@ from .sql_export import SqlExport
 
 logger = logging.getLogger(__name__)
 trigger_router = APIRouter()
+# Concurrency lock to ensure that only 1 operation is running at a time
+lock = Lock()
 
 
 class DatabaseConfiguration(BaseSettings):
@@ -77,26 +82,40 @@ class DatabaseSettings(BaseSettings):
         return settings
 
 
+def refresh_db(
+    resolve_dar: bool, historic: bool, read_from_cache: bool, lock: Lock
+) -> None:
+    try:
+        logger.info("*SQL export started*")
+        database_settings = DatabaseSettings()
+
+        sql_export = SqlExport(
+            force_sqlite=False,
+            historic=historic,
+            settings=database_settings.to_old_settings(),
+        )
+        sql_export.perform_export(
+            resolve_dar=resolve_dar,
+            use_pickle=read_from_cache,
+        )
+
+        sql_export.swap_tables()
+        logger.info("*SQL export ended*")
+    finally:
+        # Another operation can start now
+        lock.release()
+
+
 @trigger_router.post("/trigger")
 def trigger(
+    background_tasks: BackgroundTasks,
     resolve_dar: bool = Query(False),
     historic: bool = Query(False),
     read_from_cache: bool = Query(False),
 ) -> dict[str, str]:
-    database_settings = DatabaseSettings()
+    acquired = lock.acquire(blocking=False)
+    if not acquired:
+        raise HTTPException(409, "Already running")
 
-    sql_export = SqlExport(
-        force_sqlite=False,
-        historic=historic,
-        settings=database_settings.to_old_settings(),
-    )
-    sql_export.perform_export(
-        resolve_dar=resolve_dar,
-        use_pickle=read_from_cache,
-    )
-
-    sql_export.swap_tables()
-
-    logger.info("*SQL export ended*")
-
-    return {"status": "OK"}
+    background_tasks.add_task(refresh_db, resolve_dar, historic, read_from_cache, lock)
+    return {"detail": "Triggered"}
