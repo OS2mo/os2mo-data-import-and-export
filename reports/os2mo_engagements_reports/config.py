2dad3953a2cfc74543fd43e6468d534c9ce851c9
fh@magenta-aps.dk <fh@magenta-aps.dk>
2022-12-09 09:51:10 +0100

    feat: [#53537] Made GraphQL queries and functions

--- reports/os2mo_engagements_reports/config.py
+++ reports/os2mo_engagements_reports/config.py
@@ -13,16 +13,9 @@ from ra_utils.job_settings import JobSettings
 
 class EngagementSettings(JobSettings):
     # common settings for clients:
-    municipality: str  # Called "municipality.cvr" in settings.json
-    mora_base: AnyHttpUrl = cast(
-        AnyHttpUrl, "http://localhost:5000"
-    )  # "mora.base" from settings.json + /service
+    pass
 
 
 @lru_cache()
 def get_engagement_settings(*args, **kwargs) -> EngagementSettings:  # WHAT SHOULD IT BE CALLED???
     return EngagementSettings(*args, **kwargs)
-
-
-if __name__ == "__main__":
-    print(get_engagement_settings())
--- reports/os2mo_engagements_reports/get_established_engagements.py
+++ reports/os2mo_engagements_reports/get_established_engagements.py
@@ -1,12 +1,15 @@
 from uuid import UUID
 from datetime import datetime
-from typing import List
-
+from typing import List, Dict, Any
+from more_itertools import flatten
+from more_itertools import one
 from fastapi.encoders import jsonable_encoder
 
 from gql import gql
 
 from reports.os2mo_engagements_reports.config import EngagementSettings
+from reports.os2mo_engagements_reports import config
+
 
 from raclients.graph.client import GraphQLClient
 
@@ -27,6 +30,7 @@ def established_person_engagements(settings: EngagementSettings) -> list:
            }
         """
     )
+    assert 55 == 55
     with GraphQLClient(
         url=f"{settings.mora_base}/graphql/v3",
         client_id=settings.client_id,
@@ -36,20 +40,25 @@ def established_person_engagements(settings: EngagementSettings) -> list:
         sync=True,
         httpx_client_kwargs={"timeout": None},
     ) as session:
-        r = session.execute(graphql_query)
-        # Filter by start date being as of given day.
+        response = session.execute(graphql_query)
+        # Filter by start date being as of today at runtime.
         filtered_dates = filter(
-            lambda startd: startd["objects"]["validity"]["from"].isoformat() == datetime.now().isoformat(),
-            r["engagements"],
+            lambda startdate: one(startdate["objects"])["validity"]["from"] == "2021-07-09T00:00:00+02:00",
+            # datetime.now().isoformat(timespec='seconds')
+            response["engagements"]
         )
-
-    return list(filtered_dates)
+        assert 3 == 3
+    extracted_uuids = []
+    for obj in filtered_dates:
+        extracted_uuids.append(obj["objects"][0]["employee_uuid"])
+    assert 4 == 4
+    return extracted_uuids
 
 
 def persons_details_from_engagement(settings: EngagementSettings, uuidlist: List[UUID]) -> dict:
     """Retrieving all desired details on the person from the active filtered engagements."""
-
-
+#    uuid_list = established_person_engagements(settings)
+    assert 9 == 9
     graphql_query = gql(
         """query PersonEngagementDetails ($uuidlist: [UUID!]) {
              employees(uuids: $uuidlist) {
@@ -74,16 +83,41 @@ def persons_details_from_engagement(settings: EngagementSettings, uuidlist: List
            }
         """
     )
-    variables = {"uuids": uuidlist}
+    variables = {"uuidlist": uuidlist}
+    assert 1 == 1
     with GraphQLClient(
         url=f"{settings.mora_base}/graphql/v3",
         client_id=settings.client_id,
-        client_secret=settings.client_secret,  # Any cause for worry???
+        client_secret=settings.client_secret,
         auth_realm=settings.auth_realm,
         auth_server=settings.auth_server,
         sync=True,
         httpx_client_kwargs={"timeout": None},
     ) as session:
         r = session.execute(graphql_query, variable_values=jsonable_encoder(variables))
+    assert 2 == 2
 
     return r
+
+
+def display_engagements(settings: EngagementSettings):
+    list_of_eligible_persons_uuids = established_person_engagements(settings)
+
+    detailed_engagements = persons_details_from_engagement(settings, list_of_eligible_persons_uuids)
+
+    assert 3 == 3
+
+    return detailed_engagements
+
+
+def main() -> None:
+    print("Starting session")
+    pass
+
+
+if __name__ == "__main__":
+    settings = config.get_engagement_settings()
+    settings.start_logging_based_on_settings()
+    display_engagements(settings)
+
+    main()
