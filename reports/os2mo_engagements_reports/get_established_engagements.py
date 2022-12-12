a50adb7cef7c7b1e678fa8b88feee821ecea22a1
fh@magenta-aps.dk <fh@magenta-aps.dk>
2022-12-12 17:34:09 +0100

    feat: [#53537] Added helper functions

--- reports/os2mo_engagements_reports/get_established_engagements.py
+++ reports/os2mo_engagements_reports/get_established_engagements.py
@@ -8,15 +8,16 @@ from fastapi.encoders import jsonable_encoder
 
 from gql import gql
 
-# from reports.os2mo_engagements_reports.config import EngagementSettings
 from reports.os2mo_engagements_reports import config
 
 from raclients.graph.client import GraphQLClient
 from ra_utils.job_settings import JobSettings
 
 
-def established_person_engagements(settings: JobSettings) -> list:
-    """Reading all of active engagements with the persons uuids engagement start date."""
+def established_person_engagements(settings: JobSettings) -> dict:
+    """Reading all of active engagements with the persons uuid(s) engagement start date.
+    Returns an object of active engagements persons uuid(s).
+    """
 
     graphql_query = gql(
         """query EstablishedEngagements ($engagement_date_to_query_from: DateTime) {
@@ -33,33 +34,44 @@ def established_person_engagements(settings: JobSettings) -> list:
     )
     variables = {"engagement_date_to_query_from": date.today().isoformat()}
     with GraphQLClient(
-        url=f"{settings.mora_base}/graphql/v3",
-        client_id=settings.client_id,
-        client_secret=settings.client_secret,
-        auth_realm=settings.auth_realm,
-        auth_server=settings.auth_server,
-        sync=True,
-        httpx_client_kwargs={"timeout": None},
+            url=f"{settings.mora_base}/graphql/v3",
+            client_id=settings.client_id,
+            client_secret=settings.client_secret,
+            auth_realm=settings.auth_realm,
+            auth_server=settings.auth_server,
+            sync=True,
+            httpx_client_kwargs={"timeout": None},
     ) as session:
         response = session.execute(
             graphql_query, variable_values=jsonable_encoder(variables)
         )
-        # Filter by start date being as of today at runtime.
-        filtered_dates = filter(
-            lambda startdate: datetime.fromisoformat(
-                one(startdate["objects"])["validity"]["from"]
-            ).replace(tzinfo=None)
-            == utils.today(),
-            response["engagements"],
-        )
-    extracted_uuids = []
-    for obj in filtered_dates:
-        extracted_uuids.append(obj["objects"][0]["employee_uuid"])
+    return response
+
+
+def get_objects_from_gql(gql_query_dict: dict) -> list:
+    """Filtering GraphQL payload on dates.
+    Returns a list of objects with valid engagement dates and person uuid(s).
+    """
+    filtered_dates = filter(
+        lambda startdate: one(startdate["objects"])["validity"]["from"], gql_query_dict["engagements"])
+
+    return list(filtered_dates)
+
+
+def get_filtered_dates_from_today(gql_query_response: list) -> List[UUID]:
+    """Applying filter to get uuids of persons which have started engagements as of today.
+    Returns a list of eligible uuid(s) of the person(s).
+    """
+
+    filtered_d = filter(lambda startd: startd["objects"][0]["validity"]["from"], gql_query_response)
+    assert 99 == 99
+    extracted_uuids = [one(obj["objects"])["employee_uuid"] for obj in filtered_d if datetime.fromisoformat(one(
+        obj["objects"])["validity"]["from"]).replace(tzinfo=None) == utils.today()]
     return extracted_uuids
 
 
 def persons_details_from_engagement(
-    settings: JobSettings, uuidlist: List[UUID]
+        settings: JobSettings, uuidlist: List[UUID]
 ) -> dict:
     """Retrieving all desired details on the person from the active filtered engagements."""
     graphql_query = gql(
@@ -88,35 +100,39 @@ def persons_details_from_engagement(
     )
     variables = {"uuidlist": uuidlist}
     with GraphQLClient(
-        url=f"{settings.mora_base}/graphql/v3",
-        client_id=settings.client_id,
-        client_secret=settings.client_secret,
-        auth_realm=settings.auth_realm,
-        auth_server=settings.auth_server,
-        sync=True,
-        httpx_client_kwargs={"timeout": None},
+            url=f"{settings.mora_base}/graphql/v3",
+            client_id=settings.client_id,
+            client_secret=settings.client_secret,
+            auth_realm=settings.auth_realm,
+            auth_server=settings.auth_server,
+            sync=True,
+            httpx_client_kwargs={"timeout": None},
     ) as session:
         r = session.execute(graphql_query, variable_values=jsonable_encoder(variables))
     return r
 
 
-def display_engagements(settings: JobSettings):
-    list_of_eligible_persons_uuids = established_person_engagements(settings)
+def display_engagements(settings: JobSettings) -> dict:
+    payload_of_active_engagements_objects = established_person_engagements(settings)
+
+    list_of_filtered_dates = get_objects_from_gql(payload_of_active_engagements_objects)
+
+    list_of_person_uuids = get_filtered_dates_from_today(list_of_filtered_dates)
 
     detailed_engagements = persons_details_from_engagement(
-        settings, list_of_eligible_persons_uuids
+        settings, list_of_person_uuids
     )
+
+    assert 5 == 5
 
     return detailed_engagements
 
 
 def main() -> None:
-    pass
-
-
-if __name__ == "__main__":
     settings = config.get_engagement_settings()
     settings.start_logging_based_on_settings()
     display_engagements(settings)
 
+
+if __name__ == "__main__":
     main()
