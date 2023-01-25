223a4c8601625ae6e7e0d4f4eefd1f3ca7c3ec87
Mads Riis-Weitling <mrw@magenta.dk>
2023-01-25 15:09:58 +0100

    feat: [#53611] Opus import: add `skip_employee_phone` feature flag

--- integrations/opus/opus_diff_import.py
+++ integrations/opus/opus_diff_import.py
@@ -218,10 +218,12 @@ class OpusDiffImport(object):
         ):
             opus_addresses["email"] = employee["email"]
 
-        opus_addresses["phone"] = None
+        skip_employee_phone = self.settings.get(
+            "integrations.opus.skip_employee_phone", False
+        )
         if employee["workPhone"] is not None:
             phone = opus_helpers.parse_phone(employee["workPhone"])
-            if phone is not None:
+            if (phone is not None) and (not skip_employee_phone):
                 opus_addresses["phone"] = phone
 
         if (
