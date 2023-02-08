b27a90288b964fca590a885bd74a17a37604e158
fh@magenta-aps.dk <fh@magenta-aps.dk>
2023-02-08 10:45:44 +0100

    feat: [#54244] Set temporary path for reports

--- customers/Holstebro/Holstebro_reports.py
+++ customers/Holstebro/Holstebro_reports.py
@@ -37,33 +37,33 @@ if __name__ == "__main__":
 
     path = Path("/opt/docker/os2mo/queries/")
 
-    filename = "/mora/folder/query_export/Test_Alle_lederfunktioner_os2mo.csv"
+    filename = "/opt/docker/os2mo/queries/Test_Alle_lederfunktioner_os2mo.csv"
     cq.export_managers(mh, nodes, filename, empty_manager_fields=True)
 
-    filename = "/mora/folder/query_export/Test_AlleBK-stilling-email_os2mo.csv"
+    filename = "/opt/docker/os2mo/queries/Test_AlleBK-stilling-email_os2mo.csv"
     cq.export_all_employees(mh, nodes, filename)
 
     filename = (
-        "/mora/folder/query_export/Test_Holstebro_org_incl-medarbejdere_os2mo.csv"
+        "/opt/docker/os2mo/queries/Test_Holstebro_org_incl-medarbejdere_os2mo.csv"
     )
     cq.export_orgs(mh, nodes, filename)
 
-    filename = "/mora/folder/query_export/Test_Adm-org-incl-start-og-stopdata-og-enhedstyper-os2mo.csv"
+    filename = "/opt/docker/os2mo/queries/Test_Adm-org-incl-start-og-stopdata-og-enhedstyper-os2mo.csv"
     cq.export_adm_org(mh, nodes, filename)
 
     print("IT REACHES THIS POINT 1")
-    filename = "Test_teams_teamteams-tilknyttede-os2mo.csv"
+    filename = "/opt/docker/os2mo/queries/Test_teams_teamteams-tilknyttede-os2mo.csv"
     cq.export_all_teams(mh, nodes, filename)
     print("IT REACHES THIS POINT 2")
 
     # Check if this one even works - does Holstebro have AMR?
     nodes = mh.read_ou_tree(test_med)
-    filename = "/mora/folder/query_export/Test_AMR-udvalgsmedlemer_i_hieraki.csv"
+    filename = "/opt/docker/os2mo/queries/Test_AMR-udvalgsmedlemer_i_hieraki.csv"
     fieldnames = ["Hoved-MED", "Center-MED", "Lokal-MED", "AMR-Gruppe"]
     org_types = ["AMR"]
     export_udvalg(mh, nodes, filename, fieldnames, org_types)
 
-    filename = "/mora/folder/query_export/Test_MED-udvalgsmedlemer_i_hieraki.csv"
+    filename = "/opt/docker/os2mo/queries/Test_MED-udvalgsmedlemer_i_hieraki.csv"
     fieldnames = ["Hoved-MED", "Center-MED", "Lokal-MED", "AMR-Gruppe"]
     org_types = ["Hoved-MED"]  # What kind of org_types is included? We have
     # "F-MED Børn og Unge", "F-MED Kultur, Erhverv og Arbejdsmarks" etc.
@@ -73,18 +73,18 @@ if __name__ == "__main__":
         list_MED_members,
         "MED-Organisation",
         {"løn": "Holstebro Kommune", "MED": "MED-organisationen"},
-        "/mora/folder/query_export/MED_medlemmer.xlsx",
+        "/opt/docker/os2mo/queries/MED_medlemmer.xlsx",
     )
     run_report(
         list_employees,
         "Ansatte",
         "Holstebro Kommune",
-        "/mora/folder/query_export/Ansatte.xlsx",
+        "/opt/docker/os2mo/queries/Ansatte.xlsx",
     )
 
     run_report(
         list_org_units,
         "Organsiationsenheder",
         "Svendborg Kommune",
-        "/mora/folder/query_export/Organisationsenheder.xlsx",
+        "/opt/docker/os2mo/queries/Organisationsenheder.xlsx",
     )
