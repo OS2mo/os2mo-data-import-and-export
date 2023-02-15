d058b43bdb06b697c3d45d3495941b87bd36e43b
Kari <kp@magenta-aps.dk>
2023-02-15 11:37:19 +0100

    fix: [#54871] los imp utils retries submit_payloads

--- integrations/aarhus/tests/test_util.py
+++ integrations/aarhus/tests/test_util.py
@@ -1,10 +1,15 @@
 import contextlib
+import typing
 from datetime import datetime
 from operator import itemgetter
 from typing import Optional
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
+import aiohttp.client
 import los_files
 import pytest
+import pytest_aioresponses
 import util
 from aiohttp import ClientResponseError
 from aiohttp import ClientSession
@@ -75,11 +80,11 @@ class TestParseFilenames:
 
 @contextlib.asynccontextmanager
 async def _mock_mo_response(
-    aioresponses,
+    aioresponses: pytest_aioresponses,
     mo_endpoint: str,
     mo_http_status: int,
     mo_response: Optional[dict] = None,
-) -> ClientSession:
+) -> typing.AsyncGenerator[ClientSession, None]:
     mora_base = "http://example.com:8080"
     # Mock a response from MO
     aioresponses.post(
@@ -93,13 +98,14 @@ async def _mock_mo_response(
         max_concurrent_requests=1,
         os2mo_chunk_size=1,
     ):
-        session = util.get_client_session()
-        yield session
-        session.close()
+        async with util.get_client_session() as session:
+            yield session
 
 
 @pytest.mark.asyncio
-async def test_terminate_details_handles_404_response(aioresponses):
+async def test_terminate_details_handles_404_response(
+    aioresponses: pytest_aioresponses,
+):
     async def _run_test(ignored_http_statuses):
         # Mock a 404 response from MO "terminate" API, and run `terminate_details`
         async with _mock_mo_response(
@@ -121,7 +127,7 @@ async def test_terminate_details_handles_404_response(aioresponses):
 
 
 @pytest.mark.asyncio
-async def test_unhandled_status_400(aioresponses):
+async def test_unhandled_status_400(aioresponses: pytest_aioresponses):
     async with _mock_mo_response(
         aioresponses, "/service/details/create", 400, {}
     ) as client_session:
@@ -130,7 +136,7 @@ async def test_unhandled_status_400(aioresponses):
 
 
 @pytest.mark.asyncio
-async def test_handled_status_400(aioresponses):
+async def test_handled_status_400(aioresponses: pytest_aioresponses):
     async with _mock_mo_response(
         aioresponses,
         "/service/details/create",
@@ -138,3 +144,19 @@ async def test_handled_status_400(aioresponses):
         {"error_key": "V_DUPLICATED_IT_USER"},
     ) as client_session:
         await util.create_details(client_session, [{"foo": "bar"}])
+
+
+@pytest.mark.asyncio
+@patch("aiohttp.client.ClientSession.post")
+async def test_connection_error(
+    mock_post: MagicMock, aioresponses: pytest_aioresponses
+):
+    mock_post.side_effect = aiohttp.client.ServerDisconnectedError
+    async with _mock_mo_response(
+        aioresponses, "/service/details/create", 500, {}
+    ) as client_session:
+        with pytest.raises(aiohttp.ServerDisconnectedError):
+
+            await util.create_details(client_session, [{"foo": "bar"}])
+
+        assert mock_post.call_count == util.retry_attempts
