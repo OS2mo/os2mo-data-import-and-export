e1f6ab7861e95c146e7413200af761431e426909
Mads Riis-Weitling <mrw@magenta.dk>
2023-03-13 17:15:58 +0100

    feat: [#55253] `ADSletInaktive`: log each deleted AD user

--- integrations/ad_integration/powershell/ADSletInaktive.ps1
+++ integrations/ad_integration/powershell/ADSletInaktive.ps1
@@ -5,11 +5,42 @@
 #       ('Enabled' er 'false')
 #   og som findes i eller under det relevante OU
 #       ('-SearchBase' sættes til 'OU=Svendborg Kommune,DC=Svendborg,DC=net')
-$SearchBase = "OU=Kapel,OU=OS2MO,DC=Svendborg,DC=net"
-$EarliestDate = ((Get-Date).AddMonths(-1)).ToString('yyyy-MM-dd')
 
-Get-ADUser `
--SearchBase $SearchBase `
--Filter 'extensionAttribute9 -le $EarliestDate -and Enabled -eq "false"' `
--Properties * `
-| Remove-ADUser -Confirm:$false -Verbose
+# Svendborg settings
+#$DeactivationDateField = "extensionAttribute9"
+#$SearchBase = "OU=Kapel,OU=OS2MO,DC=Svendborg,DC=net"
+
+# Magenta internal AD settings
+$DeactivationDateField = "info"
+$SearchBase = "OU=Kapel,OU=demo,OU=OS2MO,DC=ad,DC=addev"
+
+# Common settings
+$EarliestDate = ((Get-Date).AddMonths(-1)).ToString('yyyy-MM-dd')
+$SettingLogPath = "C:/temp/"
+$SettingLogsToKeep = 30
+
+function Start-Log {
+    $Timestamp = Get-Date -Format "D.dd.MM.yyyy.T.HH.mm.ss"
+    $LogFilePath = $SettingLogPath + "Log." + $Timestamp + ".txt"
+
+    # Slet gamle log filer
+    Get-ChildItem -Path $SettingLogPath | Sort-Object CreationTime -Descending | Select -Skip $SettingLogsToKeep | Remove-Item -Force
+
+    Start-Transcript -Path $LogFilePath
+}
+
+function Delete-Inactive {
+    Get-ADUser `
+        -SearchBase $SearchBase `
+        -Filter '$DeactivationDateField -le $EarliestDate -and Enabled -eq "false"' `
+        -Properties $DeactivationDateField `
+    | ForEach-Object {
+        Write-Host "Påbegynder sletning af" $_ " (dato for deaktivering er" $_.$DeactivationDateField ")"
+        Remove-ADUser $_ -Confirm:$false -Verbose
+        Write-Host "Fuldførte sletning af" $_
+    }
+}
+
+Start-Log
+Delete-Inactive
+Stop-Transcript
