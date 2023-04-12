8d708168b8a28a8c39340f4121fc7d241527a1c8
fh@magenta-aps.dk <fh@magenta-aps.dk>
2023-04-12 09:25:42 +0200

    feat: [#52311] Corrected file path

--- exporters/common_query_exports/config.py
+++ exporters/common_query_exports/config.py
@@ -6,22 +6,23 @@ from raclients.graph.client import GraphQLClient
 
 class CommonQueryExportSettings(JobSettings):
     # common settings for clients:
-    alle_leder_funktioner_file_path: Path = Path("/opt/docker/queries/Alle_lederfunktioner_os2mo.csv")
+    alle_leder_funktioner_file_path: Path = Path("/opt/docker/os2mo/queries/Alle_lederfunktioner_os2mo.csv")
 
-    alle_bk_stilling_email_file_path: Path = Path("/opt/docker/queries/AlleBK-stilling-email_os2mo.csv")
+    alle_bk_stilling_email_file_path: Path = Path("/opt/docker/os2mo/queries/AlleBK-stilling-email_os2mo.csv")
 
-    ballerup_org_inc_medarbejdere_file_path: Path = Path("/opt/docker/queries/Ballerup_org_incl-medarbejdere_os2mo.csv")
+    ballerup_org_inc_medarbejdere_file_path: Path = Path(
+        "/opt/docker/os2mo/queries/Ballerup_org_incl-medarbejdere_os2mo.csv")
 
     adm_org_incl_start_og_stopdata_og_enhedstyper_file_path: Path = Path(
-        "/opt/docker/queries/Adm-org-incl-start-og-stopdata-og-enhedstyper-os2mo.csv")
+        "/opt/docker/os2mo/queries/Adm-org-incl-start-og-stopdata-og-enhedstyper-os2mo.csv")
 
-    teams_tilknyttede_file_path: Path = Path("/opt/docker/queries/teams-tilknyttede-os2mo.csv")
+    teams_tilknyttede_file_path: Path = Path("/opt/docker/os2mo/queries/teams-tilknyttede-os2mo.csv")
 
-    sd_loen_org_med_pnr_file_path: Path = Path("/opt/docker/queries/SD-løn org med Pnr_os2mo.csv")
+    sd_loen_org_med_pnr_file_path: Path = Path("/opt/docker/os2mo/queries/SD-løn org med Pnr_os2mo.csv")
 
-    amr_udvalgsmedlemmer_i_hieraki_file_path: Path = Path("/opt/docker/queries/AMR-udvalgsmedlemer_i_hieraki.csv")
+    amr_udvalgsmedlemmer_i_hieraki_file_path: Path = Path("/opt/docker/os2mo/queries/AMR-udvalgsmedlemer_i_hieraki.csv")
 
-    med_udvalgsmedlemmer_i_hieraki_file_path: Path = Path("/opt/docker/queries/MED-udvalgsmedlemer_i_hieraki.csv")
+    med_udvalgsmedlemmer_i_hieraki_file_path: Path = Path("/opt/docker/os2mo/queries/MED-udvalgsmedlemer_i_hieraki.csv")
 
 
 @lru_cache()
