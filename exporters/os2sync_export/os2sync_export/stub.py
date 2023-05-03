1ee2fea84d162008f56bb6cf4effbb1094491533
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2023-05-03 14:28:10 +0200

    feat(os2sync_export): log os2sync requests to disk for integration-tests

--- exporters/os2sync_export/os2sync_export/stub.py
+++ exporters/os2sync_export/os2sync_export/stub.py
@@ -10,20 +10,28 @@ class Session:
     headers: dict[str, str] = {}
     text = str(uuid4())
     status_code = 404
+    logname = "os2sync_requests.txt"
+    logger = logging.getLogger(logname)
+
+    log_config = logging.FileHandler(logname, mode="w")
+    formatter = logging.Formatter("%(message)s")
+    log_config.setFormatter(formatter)
+
+    logger.addHandler(log_config)
 
     def raise_for_status(self):
         pass
 
     def get(self, *args, **kwargs):
-        logger.info("GET %r %r", args, kwargs)
+        self.logger.info("GET %r %r", args, kwargs)
         return self
 
     def delete(self, *args, **kwargs):
-        logger.info("DELETE %r %r", args, kwargs)
+        self.logger.info("DELETE %r %r", args, kwargs)
         return self
 
     def post(self, *args, **kwargs):
-        logger.info("POST %r %r", args, json.dumps(kwargs))
+        self.logger.info("POST %r %r", args, json.dumps(kwargs))
         return self
 
     def json(self):
