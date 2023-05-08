4a8fd2782fa8f94130175ab606ffe47f85979596
fh@magenta-aps.dk <fh@magenta-aps.dk>
2023-05-08 10:51:01 +0200

    feat: [#54476] WIP add joins on uuid

--- customers/Frederikshavn/config.py
+++ customers/Frederikshavn/config.py
@@ -5,21 +5,21 @@ from ra_utils.job_settings import JobSettings
 
 class EmployeePhoneBookSettings(JobSettings):
     # common settings for clients:
-    ftp_url: str
-    ftp_port: int
-    ftp_user: str
-    ftp_pass: str
+    ftp_url: str | None
+    ftp_port: int | None
+    ftp_user: str | None
+    ftp_pass: str | None
     ftp_ssh_key_path: str | None
     ftp_ssh_key_pass: str | None
-    ftp_folder: str
-    import_state_file: str
+    ftp_folder: str | None
+    import_state_file: str | None
     import_csv_folder: str | None
     mox_base: str = "http://localhost:5000/lora"
     mora_base: str = "http://localhost:5000"
     report_dir_path: str = "/opt/docker/os2mo/queries"
 
-    sql_cell_phone_number_field: str  # Desired cell phone type - "AD-Mobil".
-    sql_phone_number_field: str  # Desired phone type - "AD-Telefonnummer".
+    sql_cell_phone_number_field: str | None  # Desired cell phone type - "AD-Mobil".
+    sql_phone_number_field: str | None  # Desired phone type - "AD-Telefonnummer".
     sql_visibility_scope_field: str | None  # Exclude visibility scope of - "SECRET".
     sql_excluded_organisation_units_user_key: str | None  # Exclude certain organisation units.
     sql_excluded_organisation_units_uuid: str | None  # Exclude certain organisation units by uuid.
