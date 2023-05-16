340f5d0877fa8048e05d5426f15f27f128950a0d
Andreas Kring <andreas@magenta.dk>
2023-05-16 15:04:50 +0200

    feat: code for getting IT-systems

---
+++ integrations/SD_Lon/sdlon/it_systems.py
@@ -0,0 +1,64 @@
+from functools import cache
+from uuid import UUID
+
+from gql import gql
+from more_itertools import one
+
+from raclients.graph.client import GraphQLClient
+
+
+@cache
+def get_sd_to_ad_it_system_uuid(gql_client: GraphQLClient) -> UUID:
+    query = gql("""
+        query GetItSystems {
+            itsystems(user_keys: "SD til AD") {
+                objects {
+                    uuid
+                }
+            }
+        }            
+    """)
+
+    r = gql_client.execute(query)
+
+    return UUID(one(r["itsystems"]["objects"])["uuid"])
+
+
+def get_employee_itsystems(
+        gql_client: GraphQLClient, employee_uuid: UUID
+) -> list[UUID]:
+    """
+    Get the IT-systems for an employee
+    Args:
+        gql_client: The GraphQL client for calling MO
+        employee_uuid: The employee UUID
+
+    Returns:
+        List of UUIDs of the employees IT-systems
+    """
+
+    query = gql("""
+        query GetEmployeeItSystems($uuid: [UUID!]!) {
+            employees(uuids: $uuid) {
+                objects {
+                    current {
+                        itusers {
+                            itsystem {
+                                uuid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    """)
+
+    r = gql_client.execute(query, variable_values={"uuid": str(employee_uuid)})
+
+    it_users = one(r["employee"]["objects"])["current"]["itusers"]
+    it_system_uuids = [
+        UUID(it_user["itsystem"]["uuid"])
+        for it_user in it_users
+    ]
+
+    return it_system_uuids
