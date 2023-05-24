0cbe98316f77c5291b561ed1fdfb8a4c666230b5
Andreas Kring <andreas@magenta.dk>
2023-05-24 10:53:25 +0200

    feat: implement GraphQL get_employee

--- integrations/SD_Lon/sdlon/models.py
+++ integrations/SD_Lon/sdlon/models.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from uuid import UUID
 
 from pydantic import BaseModel
 
@@ -13,3 +14,11 @@ class SDBasePerson(BaseModel):
     cpr: str
     given_name: Optional[str]
     surname: Optional[str]
+
+
+class MOBasePerson(BaseModel):
+    cpr: str
+    givenname: str
+    surname: str
+    name: str
+    uuid: UUID
