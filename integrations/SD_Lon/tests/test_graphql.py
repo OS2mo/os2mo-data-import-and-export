d17ab4979b5a37be0cad659e82a8adccd8c845a4
Andreas Kring <andreas@magenta.dk>
2023-05-31 11:03:19 +0200

    test: verify that GraphQL client is instantiated correctly

---
+++ integrations/SD_Lon/tests/test_graphql.py
@@ -0,0 +1,22 @@
+from sdlon.graphql import get_mo_client
+
+
+def test_get_mo_client():
+    # Act
+    client = get_mo_client(
+        "http://keycloak-service:8080/auth",
+        "client_id",
+        "client_secret",
+        "http://mo-service:5000",
+        300,
+    )
+
+    # Assert
+    assert client.transport.url == "http://mo-service:5000/graphql/v5"
+    assert (
+        client.transport.client_args["auth_server"]
+        == "http://keycloak-service:8080/auth"
+    )
+    assert client.transport.client_args["client_id"] == "client_id"
+    assert client.transport.client_args["client_secret"] == "client_secret"
+    assert client.transport.client_args["timeout"] == 300
