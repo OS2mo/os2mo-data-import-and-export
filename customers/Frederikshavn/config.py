e6881705b48a0e1f3b314a9dd026a60ff6f8caa3
fh@magenta-aps.dk <fh@magenta-aps.dk>
2023-07-04 15:44:58 +0200

    feat: [#54476] Make pipelines happy

--- customers/Frederikshavn/config.py
+++ customers/Frederikshavn/config.py
@@ -20,13 +20,26 @@ class EmployeePhoneBookSettings(JobSettings):
     # mora_base: str = "http://localhost:5000"
 
     # Settings for Employee Phonebook:
-    sql_cell_phone_number_field: str | None = "AD-Mobil"  # Desired cell phone type - "AD-Mobil".
-    sql_phone_number_field_list: list | None = ["AD-Telefonnummer", "Telefon"]  # Desired phone type -
+    sql_cell_phone_number_field: str | None = (
+        "AD-Mobil"  # Desired cell phone type - "AD-Mobil".
+    )
+    sql_phone_number_field_list: list | None = [
+        "AD-Telefonnummer",
+        "Telefon",
+    ]  # Desired phone type -
     # "AD-Telefonnummer" and "Telefon".
-    sql_visibility_scope_field: str | None = "SECRET"  # Exclude visibility scope of - "SECRET".
-    sql_visibility_title_field: str | None = "Hemmelig"  # Exclude visibility scope of - "Hemmelig".
-    sql_excluded_organisation_units_user_key: str | None = "1018136"  # Exclude certain organisation units.
-    sql_excluded_organisation_units_uuid: str | None = "f11963f6-2df5-9642-f1e3-0983dad332f4"  # Exclude certain
+    sql_visibility_scope_field: str | None = (
+        "SECRET"  # Exclude visibility scope of - "SECRET".
+    )
+    sql_visibility_title_field: str | None = (
+        "Hemmelig"  # Exclude visibility scope of - "Hemmelig".
+    )
+    sql_excluded_organisation_units_user_key: str | None = (
+        "1018136"  # Exclude certain organisation units.
+    )
+    sql_excluded_organisation_units_uuid: str | None = (
+        "f11963f6-2df5-9642-f1e3-0983dad332f4"  # Exclude certain
+    )
     # organisation units by uuid.
 
 
