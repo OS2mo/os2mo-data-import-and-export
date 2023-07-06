317cf110e9fe19422d9427d6761e1424ed8545c0
Kari <kp@magenta-aps.dk>
2023-07-06 17:10:44 +0200

    fix: [#56799] fix backup of missing dbs

--- tools/cron-backup.sh
+++ tools/cron-backup.sh
@@ -45,37 +45,6 @@ if [ "${INSTALLATION_TYPE}" == "docker" ]; then
     fi
     chmod 755 ${HOST_SNAPSHOT_DESTINATION}
 
-    # Create confdb
-    CONFDB_DATABASE_NAME=${CONFDB_DATABASE_NAME:-"mora"}
-    CONFDB_HOST_SNAPSHOT_DESTINATION=${CONFDB_HOST_SNAPSHOT_DESTINATION:-"/opt/docker/os2mo/database_snapshot/confdb.sql"}
-    CONFDB_DOCKER_SNAPSHOT_DESTINATION=${CONFDB_DOCKER_SNAPSHOT_DESTINATION:-"/database_snapshot/confdb.sql"}
-    echo "Snapshotting ${CONFDB_DATABASE_NAME}"
-    docker exec -t ${CONTAINER_NAME} \
-        su --shell /bin/bash \
-           --command "pg_dump --data-only --inserts ${CONFDB_DATABASE_NAME} -f ${CONFDB_DOCKER_SNAPSHOT_DESTINATION}" \
-           postgres
-    EXIT_CODE=$?
-    if [ ${EXIT_CODE} -ne 0 ]; then
-        echo "Unable to snapshot database"
-        exit 1
-    fi
-    chmod 755 ${CONFDB_HOST_SNAPSHOT_DESTINATION}
-
-    # Create sessions
-    SESSIONS_DATABASE_NAME=${SESSIONS_DATABASE_NAME:-"sessions"}
-    SESSIONS_HOST_SNAPSHOT_DESTINATION=${SESSIONS_HOST_SNAPSHOT_DESTINATION:-"/opt/docker/os2mo/database_snapshot/sessions.sql"}
-    SESSIONS_DOCKER_SNAPSHOT_DESTINATION=${SESSIONS_DOCKER_SNAPSHOT_DESTINATION:-"/database_snapshot/sessions.sql"}
-    echo "Snapshotting ${SESSIONS_DATABASE_NAME}"
-    docker exec -t ${CONTAINER_NAME} \
-        su --shell /bin/bash \
-           --command "pg_dump --data-only --inserts ${SESSIONS_DATABASE_NAME} -f ${SESSIONS_DOCKER_SNAPSHOT_DESTINATION}" \
-           postgres
-    EXIT_CODE=$?
-    if [ ${EXIT_CODE} -ne 0 ]; then
-        echo "Unable to snapshot database"
-        exit 1
-    fi
-    chmod 755 ${SESSIONS_HOST_SNAPSHOT_DESTINATION}
 elif [ "${INSTALLATION_TYPE}" == "legacy" ]; then
     echo "WARNING: No snapshotting configured"
     exit 1
