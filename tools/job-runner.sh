fe48db803cb2a3bd54b43d6e879d5cf5428024df
Casper V. Kristensen <vk@magenta.dk>
2023-07-08 14:27:32 +0200

    chore: [#56847] in-line job-runner

--- tools/job-runner.sh
+++ tools/job-runner.sh
@@ -419,11 +419,57 @@ reports_sd_db_overview(){
     return $STATUS
 }
 
-# read the run-job script et al
-for module in tools/job-runner.d/*.sh; do
-    #echo sourcing $module
-    source $module
-done
+run-job(){
+    local JOB=$1
+    prometrics-job-start ${JOB}
+    set -o pipefail
+    # Detect if we are running from cron
+    if [ "$TERM" == "dumb" ]; then
+        # Capture both stdout and stderr using "|&" (requires Bash 4+.)
+        # Send stdout and stderr to systemd journal using the identifier given by the "-t" option.
+        # Job output can be retrieved using e.g. "journalctl -t dipex:job-runner.sh:exports_actual_state_export", etc.
+        $JOB |& systemd-cat -t "dipex:job-runner.sh:$JOB"
+    else
+        $JOB
+    fi
+
+    JOB_STATUS=$?
+    prometrics-job-end ${JOB} ${JOB_STATUS}
+    return $JOB_STATUS
+}
+
+prometrics-job-start(){
+    [ -z "${CRON_LOG_PROM_API}" ] && return 0
+    cat <<EOF | curl -m 2 -sS --data-binary @- "${CRON_LOG_PROM_API}/$1/"
+    # TYPE mo_start_time gauge
+    # HELP mo_start_time Unixtime for job start time
+    mo_start_time $(date +%s)
+EOF
+}
+
+prometrics-job-end(){
+    [ -z "${CRON_LOG_PROM_API}" ] && return 0
+    cat <<EOF | curl -m 2 -sS --data-binary @- "${CRON_LOG_PROM_API}/$1"
+    # TYPE mo_end_time gauge
+    # HELP mo_end_time Unixtime for job end time
+    mo_end_time $(date +%s)
+    # TYPE mo_return_code gauge
+    # HELP mo_return_code Return code of job
+    mo_return_code $2
+EOF
+}
+
+prometrics-git(){
+    git_version=$(git describe --tags)
+
+    [ -z "${CRON_LOG_PROM_API}" ] && return 0
+    cat <<EOF | curl -m 2 -sS --data-binary @- "${CRON_LOG_PROM_API}/git/git_version/${git_version}"
+    # TYPE git_info gauge
+    # HELP git_info A metric with a timestamp to sort by, labeled by git_hash, branch and local_changes
+    git_info $(date +%s)
+EOF
+}
+
 
 prometrics-git
 
