354c305fd913c916fc38f9672f31408fa297946f
Mads Riis-Weitling <mrw@magenta.dk>
2023-09-29 11:47:52 +0200

    fix: [#57093] Mock dependencies when testing `log_payload`
    This makes the test a unittest rather than an integration test.
    
    Integration testing will come later when I figure out how to include a
    separate Postgres instance in the CI `docker-compose` stack, and connect
    to it from the test suite.

--- integrations/SD_Lon/tests/payload_db/test_log_payload.py
+++ integrations/SD_Lon/tests/payload_db/test_log_payload.py
@@ -1,7 +1,23 @@
 # SPDX-FileCopyrightText: Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
-from payload_db import log_payload
+from unittest.mock import Mock
+
+import payload_db
+from pytest import MonkeyPatch
 
 
-def test_log_payload():
-    log_payload("", "", "")
+def test_log_payload(monkeypatch: MonkeyPatch) -> None:
+    # Arrange
+    mock_session: Mock = Mock()
+    mock_session_maker: Mock = Mock(return_value=mock_session)
+    monkeypatch.setattr(payload_db, "get_engine", lambda: None)
+    monkeypatch.setattr(payload_db, "Session", mock_session_maker)
+    # Act
+    payload_db.log_payload("full_url", "params", "response")
+    # Assert
+    mock_session.add.assert_called_once()
+    mock_session.commit.assert_called_once()
+    payload: payload_db.Payload = mock_session.add.call_args.args[0]
+    assert payload.full_url == "full_url"
+    assert payload.params == "params"
+    assert payload.response == "response"
