e507875f70eaff18e7231941366fd32aa1b7609d
Mads Riis-Weitling <mrw@magenta.dk>
2023-09-29 14:47:13 +0200

    feat: [#57093] Use request UUID as PK for `Payload`; add `status_code`

--- integrations/SD_Lon/payload_db/__init__.py
+++ integrations/SD_Lon/payload_db/__init__.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
+from uuid import UUID
+
 from sqlalchemy.orm import sessionmaker
 
 from .engine import get_engine
@@ -9,10 +11,22 @@ from .models import Payload
 Session = sessionmaker()
 
 
-def log_payload(full_url: str, params: str, response: str):
+def log_payload(
+    request_uuid: UUID,
+    full_url: str,
+    params: str,
+    response: str,
+    status_code: int,
+) -> None:
     """Log a given SD payload to the payload database"""
     Session.configure(bind=get_engine())
     session = Session()
-    payload = Payload(full_url=full_url, params=params, response=response)
+    payload = Payload(
+        id=request_uuid,
+        full_url=full_url,
+        params=params,
+        response=response,
+        status_code=status_code,
+    )
     session.add(payload)
     session.commit()
--- integrations/SD_Lon/payload_db/models.py
+++ integrations/SD_Lon/payload_db/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column
 from sqlalchemy import DateTime
 from sqlalchemy import Integer
 from sqlalchemy.dialects.postgresql import TEXT
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql import func
 
@@ -14,8 +15,9 @@ Base = declarative_base()
 class Payload(Base):  # type: ignore
     __tablename__ = "payload"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(UUID(as_uuid=True), primary_key=True)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
     full_url = Column(TEXT)
     params = Column(TEXT)
     response = Column(TEXT)
+    status_code = Column(Integer)
