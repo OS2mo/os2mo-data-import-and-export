18f1d0ac762d349c7ead830d66782f03284ad742
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2023-10-25 11:54:45 +0200

    feat: add alembic migrations

---
+++ exporters/sql_export/sql_export/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Apply Alembic migrations
+alembic upgrade head
+
+# Run app
+uvicorn --factory sql_export.main:create_app --host 0.0.0.0
