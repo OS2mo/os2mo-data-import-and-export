20fb8f1f2c3eb42e91a5803fc529d789913873c1
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2023-11-08 09:06:02 +0100

    refactor: move settings into config file

---
+++ exporters/sql_export/config.py
@@ -0,0 +1,133 @@
+# SPDX-FileCopyrightText: Magenta ApS
+#
+# SPDX-License-Identifier: MPL-2.0
+from functools import lru_cache
+from typing import Any
+
+from pydantic import AnyHttpUrl
+from pydantic import BaseSettings
+from pydantic import Field
+from pydantic import SecretStr
+from ra_utils.job_settings import JobSettings
+
+
+class GqlLoraCacheSettings(JobSettings):  # type: ignore
+    class Config:
+        frozen = True
+
+    use_new_cache: bool = False
+    primary_manager_responsibility: str | None = None
+    prometheus_pushgateway: str | None = "pushgateway"
+    mox_base: str = "http://mo:5000/lora"
+    std_page_size: int = 300
+
+    def to_old_settings(self) -> dict[str, Any]:
+        """Convert our DatabaseSettings to a settings.json format.
+
+        This serves to implement the adapter pattern, adapting from pydantic and its
+        corresponding 12-factor configuration paradigm with environment variables, to
+        the current functionality of the program, based on the settings format from
+        settings.json.
+
+        Eventually the entire settings-processing within the program should be
+        rewritten with a process similar to what has been done for the SD integration,
+        but it was out of scope for the change when this code was introduced.
+        """
+
+        settings = {
+            "mora.base": self.mora_base,
+            "mox.base": self.mox_base,
+            "exporters": {
+                "actual_state": {
+                    "manager_responsibility_class": self.primary_manager_responsibility
+                }
+            },
+            "exporters.actual_state.manager_responsibility_class": self.primary_manager_responsibility,
+            "use_new_cache": self.use_new_cache,
+        }
+
+        return settings
+
+
+@lru_cache()
+def get_gql_cache_settings(*args, **kwargs) -> GqlLoraCacheSettings:
+    return GqlLoraCacheSettings(*args, **kwargs)
+
+
+class DatabaseConfiguration(BaseSettings):  # type: ignore
+    class Config:
+        frozen = True
+
+    type: str | None
+    host: str | None
+    db_name: str
+    user: str | None
+    password: SecretStr | None
+
+
+class DatabaseSettings(JobSettings):
+    class Config:
+        frozen = True
+        env_nested_delimiter = "__"
+
+    mora_base: AnyHttpUrl = Field("http://mo-service:5000")
+    mox_base: AnyHttpUrl = Field("http://mo-service:5000/lora")
+
+    actual_state: DatabaseConfiguration = Field(default_factory=DatabaseConfiguration)
+    historic_state: DatabaseConfiguration | None
+    log_overlapping_aak: bool = False
+    use_new_cache: bool = False
+    primary_manager_responsibility: str | None = None
+
+    def to_old_settings(self) -> dict[str, Any]:
+        """Convert our DatabaseSettings to a settings.json format.
+
+        This serves to implement the adapter pattern, adapting from pydantic and its
+        corresponding 12-factor configuration paradigm with environmental variables, to
+        the current functionality of the program, based on the settings format from
+        settings.json.
+
+        Eventually the entire settings-processing within the program should be
+        rewritten with a process similar to what has been done for the SD integration,
+        but it was out of scope for the change when this code was introduces.
+        """
+
+        def secret_val_or_none(value: SecretStr | None) -> str | None:
+            if value is None:
+                return None
+            return value.get_secret_value()
+
+        settings = {
+            "mora.base": self.mora_base,
+            "mox.base": self.mox_base,
+            "exporters.actual_state.type": self.actual_state.type,
+            "exporters.actual_state.host": self.actual_state.host,
+            "exporters.actual_state.db_name": self.actual_state.db_name,
+            "exporters.actual_state.user": self.actual_state.user,
+            "exporters.actual_state.password": secret_val_or_none(
+                self.actual_state.password
+            ),
+            "primary_manager_responsibility": self.primary_manager_responsibility,
+            "exporters.actual_state.manager_responsibility_class": self.primary_manager_responsibility,
+            "use_new_cache": self.use_new_cache,
+        }
+        if self.historic_state is not None:
+            settings.update(
+                {
+                    "exporters.actual_state_historic.type": self.historic_state.type
+                    or self.actual_state.type,
+                    "exporters.actual_state_historic.host": self.historic_state.host
+                    or self.actual_state.host,
+                    "exporters.actual_state_historic.db_name": self.historic_state.db_name
+                    or self.actual_state.db_name,
+                    "exporters.actual_state_historic.user": self.historic_state.user
+                    or self.actual_state.user,
+                    "exporters.actual_state_historic.password": secret_val_or_none(
+                        self.historic_state.password
+                    )
+                    or secret_val_or_none(self.actual_state.password),
+                    "primary_manager_responsibility": self.primary_manager_responsibility,
+                    "use_new_cache": self.use_new_cache,
+                }
+            )
+        return settings
--- exporters/sql_export/gql_lora_cache_async.py
+++ exporters/sql_export/gql_lora_cache_async.py
@@ -4,65 +4,22 @@ import logging
 import os
 import pickle
 import time
-from functools import lru_cache
 from pathlib import Path
-from typing import Any
 
 from dateutil.parser import parse as parse_date
 from gql import gql
 from more_itertools import first
 from ra_utils.async_to_sync import async_to_sync
-from ra_utils.job_settings import JobSettings
 from raclients.graph.client import GraphQLClient
 from raclients.graph.util import execute_paged
 from tenacity import retry
 from tenacity import stop_after_delay
 from tenacity import wait_exponential
 
+from .config import get_gql_cache_settings
+from .config import GqlLoraCacheSettings
+
 RETRY_MAX_TIME = 60 * 5
-
-
-class GqlLoraCacheSettings(JobSettings):  # type: ignore
-    class Config:
-        frozen = True
-
-    use_new_cache: bool = False
-    primary_manager_responsibility: str | None = None
-    prometheus_pushgateway: str | None = "pushgateway"
-    mox_base: str = "http://mo:5000/lora"
-    std_page_size: int = 300
-
-    def to_old_settings(self) -> dict[str, Any]:
-        """Convert our DatabaseSettings to a settings.json format.
-
-        This serves to implement the adapter pattern, adapting from pydantic and its
-        corresponding 12-factor configuration paradigm with environment variables, to
-        the current functionality of the program, based on the settings format from
-        settings.json.
-
-        Eventually the entire settings-processing within the program should be
-        rewritten with a process similar to what has been done for the SD integration,
-        but it was out of scope for the change when this code was introduced.
-        """
-
-        settings = {
-            "mora.base": self.mora_base,
-            "mox.base": self.mox_base,
-            "exporters": {
-                "actual_state": {
-                    "manager_responsibility_class": self.primary_manager_responsibility
-                }
-            },
-            "exporters.actual_state.manager_responsibility_class": self.primary_manager_responsibility,
-            "use_new_cache": self.use_new_cache,
-        }
-
-        return settings
-
-
-@lru_cache()
-def get_gql_cache_settings(*args, **kwargs) -> GqlLoraCacheSettings:
-    return GqlLoraCacheSettings(*args, **kwargs)
 
 
 logger = logging.getLogger(__name__)
