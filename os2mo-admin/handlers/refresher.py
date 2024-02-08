4bda16d1e13fbb496635d80680fcec1c3367488c
Emil Madsen <emil@magenta.dk>
2024-02-08 13:31:32 +0100

    feat(os2mo-admin): [#59583] restructure project

---
+++ os2mo-admin/handlers/refresher.py
@@ -0,0 +1,131 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+import json
+from typing import Any
+from textwrap import dedent
+
+import click
+from gql import gql
+from raclients.graph.client import GraphQLClient
+
+
+class JSONParamType(click.ParamType):
+    name = "json"
+
+    def convert(self, value: Any, _1, _2) -> dict[Any, Any]:
+        return json.loads(value)
+
+
+JSON = JSONParamType()
+
+
+VALID_COLLECTIONS = [
+    "address",
+    "association",
+    "class",
+    "employee",
+    "engagement",
+    "facet",
+    "itsystem",
+    "ituser",
+    "kle",
+    "leave",
+    "manager",
+    "org_unit",
+    "owner",
+    "related_unit",
+    "role",
+]
+
+
+@click.command()
+@click.pass_context
+@click.option(
+    "--limit",
+    type=click.INT,
+    default=100,
+    help="Number entities to update per refresh.",
+)
+@click.option(
+    "--filter", type=JSON, help="JSON filter to apply to the refresh mutator."
+)
+@click.option(
+    "--collection",
+    type=click.Choice(VALID_COLLECTIONS),
+    required=True,
+    help="Name of the collection to refresh.",
+)
+@click.option(
+    "--print-uuids",
+    type=click.BOOL,
+    is_flag=True,
+    default=False,
+    help="Whether to print UUIDs as they get refreshed.",
+)
+@click.option("--queue", type=click.STRING, help="The specific queue to target.")
+def refresher(
+    ctx: Any,
+    limit: int,
+    filter: dict[Any, Any],
+    collection: str,
+    print_uuids: bool,
+    queue: str,
+) -> None:
+    verbosity = ctx.obj["VERBOSITY"]
+
+    filter_type = f"{collection.capitalize()}Filter"
+    mutator_name = f"{collection}_refresh"
+
+    query = dedent(
+        f"""
+        mutation RefreshMutation(
+          $limit: int!,
+          $cursor: Cursor,
+          $include_uuid: Boolean!
+          $queue: String,
+          $filter: {filter_type}
+        ) {{
+        {mutator_name}(
+            limit: $limit
+            queue: $queue
+            cursor: $cursor
+            filter: $filter
+          ) {{
+            objects @include(if: $include_uuid)
+            page_info {{
+              next_cursor
+            }}
+          }}
+        }}
+    """
+    )
+    if verbosity > 0:
+        click.echo(query)
+    params = {
+        "limit": limit,
+        "cursor": None,
+        "include_uuid": print_uuids,
+        "queue": queue,
+        "filter": filter,
+    }
+    if verbosity > 0:
+        click.echo(params)
+    with GraphQLClient(
+        **ctx.obj["GRAPHQL_PARAMS"],
+        sync=True,
+        httpx_client_kwargs={"timeout": None},
+    ) as session:
+        gql_query = gql(query)
+        cursor = object()
+        counter = 0
+        while cursor is not None:
+            result = session.execute(gql_query, variable_values=params)
+            cursor = result[mutator_name]["page_info"]["next_cursor"]
+            if print_uuids:
+                uuids = result[mutator_name]["objects"]
+                for uuid in uuids:
+                    click.echo(uuid)
+            params["cursor"] = cursor
+            counter += limit
+            if verbosity > 0:
+                click.echo(counter)
