84a9a63ca7669f67ebd368d19a9c2807bdaade30
Andreas Kring <andreas@magenta.dk>
2024-06-19 13:56:08 +0200

    feat: [#61239] adjust SDTool+ curl statement

--- tools/job-runner.sh
+++ tools/job-runner.sh
@@ -171,14 +171,10 @@ imports_sd_changed_at(){
 imports_sdtool_plus(){
     echo running imports_sdtool_plus
 
-    SDTOOL_PLUS_HTTP_STATUS=$(curl -m 1800 --no-progress-meter -o /dev/null -w "%{http_code}" -X POST "http://localhost:8040/trigger")
+    curl -m 1800 --no-progress-meter -f -X POST "http://localhost:8040/trigger"
     CURL_STATUS=$?
     if [[ ${CURL_STATUS} != 0 ]]; then
         return ${CURL_STATUS}
-    fi
-
-    if [[ ${SDTOOL_PLUS_HTTP_STATUS} != 200 ]]; then
-        return 1
     fi
 }
 
