dccb71278c6824d4533d659e279b7b208bdf3aed
Andreas Kring <andreas@magenta.dk>
2024-06-24 15:25:08 +0200

    feat: [#60843] function for getting employees

--- reports/holstebro/manager_report.py
+++ reports/holstebro/manager_report.py
@@ -10,6 +10,40 @@ from ra_utils.job_settings import JobSettings
 from reports.holstebro.graphql import get_mo_client
 
 
+GET_EMPLOYEE_QUERY = gql(
+    """
+    query GetEmployees($cursor: Cursor, $limit: int, $email_addr_type: [UUID!]) {
+      employees(cursor: $cursor, limit: $limit) {
+        page_info {
+          next_cursor
+        }
+        objects {
+          current {
+            user_key
+            given_name
+            name
+            addresses(filter: {address_type: {uuids: $email_addr_type}}) {
+              name
+            }
+            manager_roles {
+              uuid
+            }
+            engagements {
+              org_unit {
+                uuid
+                name
+                user_key
+              }
+              is_primary
+            }
+          }
+        }
+      }
+    }
+    """
+)
+
+
 def get_email_addr_type(gql_client: GraphQLClient) -> UUID:
     get_email_addr_type_query = gql(
         """
@@ -28,6 +62,30 @@ def get_email_addr_type(gql_client: GraphQLClient) -> UUID:
     return UUID(one(r["classes"]["objects"])["current"]["uuid"])
 
 
+def get_employees(
+    gql_client: GraphQLClient,
+    email_addr_type: UUID,
+    limit: int
+) -> list[dict[str, Any]]:
+    employees = []
+    next_cursor = None
+    while True:
+        r = gql_client.execute(
+            GET_EMPLOYEE_QUERY,
+            variable_values={
+                "cursor": next_cursor,
+                "limit": limit,
+                "email_addr_type": str(email_addr_type),
+            }
+        )
+        employees.extend(r["employees"]["objects"])
+        next_cursor = r["employees"]["page_info"]["next_cursor"]
+        if next_cursor is None:
+            break
+
+    return employees
+
+
 def main(
     auth_server: str,
     client_id: str,
@@ -43,7 +101,9 @@ def main(
         gql_version=gql_version,
     )
 
-    addr_type_uuid = get_email_addr_type(gql_client)
+    email_addr_type = get_email_addr_type(gql_client)
+    employees = get_employees(gql_client, email_addr_type, 100)
+
 
 
 if __name__ == "__main__":
--- reports/tests/holstebro/test_manager_report.py
+++ reports/tests/holstebro/test_manager_report.py
@@ -1,7 +1,8 @@
-from unittest.mock import MagicMock
-from uuid import UUID
+from unittest.mock import MagicMock, call
+from uuid import UUID, uuid4
 
-from reports.holstebro.manager_report import get_email_addr_type
+from reports.holstebro.manager_report import get_email_addr_type, \
+    get_employees, GET_EMPLOYEE_QUERY
 
 
 def test_get_email_addr_type():
@@ -24,3 +25,124 @@ def test_get_email_addr_type():
 
     # Assert
     assert addr_type_uuid == UUID("f376deb8-4743-4ca6-a047-3241de8fe9d2")
+    # TODO: assert mock called with
+
+
+def test_get_employees():
+    # Arrange
+    email_addr_type = uuid4()
+
+    employee_obj_batch1 = [
+        {
+            "current": {
+                "user_key": "BirgittaD",
+                "given_name": "Birgitta Munk",
+                "name": "Birgitta Munk Duschek",
+                "addresses": [],
+                "manager_roles": [
+                    {
+                        "uuid": "f82a969e-9953-40cf-925d-61629ba7139f"
+                    }
+                ],
+                "engagements": []
+            }
+        },
+        {
+            "current": {
+                "user_key": "MikkelI",
+                "given_name": "Mikkel",
+                "name": "Mikkel Iversen",
+                "addresses": [],
+                "manager_roles": [],
+                "engagements": []
+            }
+        }
+    ]
+
+    employee_obj_batch2 = [
+        {
+            "current": {
+                "user_key": "AnnaN",
+                "given_name": "Anna Brink",
+                "name": "Anna Brink Nielsen",
+                "addresses": [
+                    {
+                        "name": "annan@kolding.dk"
+                    }
+                ],
+                "manager_roles": [],
+                "engagements": [
+                    {
+                        "org_unit": [
+                            {
+                                "uuid": "5cb38a3c-cacd-5d54-9eb3-88eae2baba1b",
+                                "name": "Vamdrup skole",
+                                "user_key": "Vamdrup skole"
+                            }
+                        ],
+                        "is_primary": True
+                    }
+                ]
+            }
+        }
+    ]
+
+    mock_gql_client = MagicMock()
+    mock_gql_client.execute.side_effect = [
+        {
+            "employees": {
+                "page_info": {
+                    "next_cursor": "cursor1"
+                },
+                "objects": employee_obj_batch1
+            }
+        },
+        {
+            "employees": {
+                "page_info": {
+                    "next_cursor": "cursor2"
+                },
+                "objects": employee_obj_batch2
+            }
+        },
+        {
+            "employees": {
+                "page_info": {
+                    "next_cursor": None
+                },
+                "objects": []
+            }
+        },
+    ]
+
+    # Act
+    employees = get_employees(mock_gql_client, email_addr_type, limit=2)
+
+    # Assert
+    assert employees == employee_obj_batch1 + employee_obj_batch2
+    assert mock_gql_client.execute.call_args_list == [
+        call(
+            GET_EMPLOYEE_QUERY,
+            variable_values={
+                "cursor": None,
+                "limit": 2,
+                "email_addr_type": str(email_addr_type)
+            }
+        ),
+        call(
+            GET_EMPLOYEE_QUERY,
+            variable_values={
+                "cursor": "cursor1",
+                "limit": 2,
+                "email_addr_type": str(email_addr_type)
+            }
+        ),
+        call(
+            GET_EMPLOYEE_QUERY,
+            variable_values={
+                "cursor": "cursor2",
+                "limit": 2,
+                "email_addr_type": str(email_addr_type)
+            }
+        ),
+    ]
