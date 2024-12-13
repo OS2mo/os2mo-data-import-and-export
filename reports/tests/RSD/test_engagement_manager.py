0f1bb06b86b4fd101c89cc72cf02fa022eaaf772
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2024-12-13 11:45:03 +0100

    test: add test for extract ancestors

--- reports/tests/RSD/test_engagement_manager.py
+++ reports/tests/RSD/test_engagement_manager.py
@@ -1,6 +1,7 @@
 import pytest
 from freezegun import freeze_time
 
+from reports.RSD.Engagement_manager import extract_ancestors
 from reports.RSD.Engagement_manager import get_age
 
 
@@ -10,5 +11,39 @@ from reports.RSD.Engagement_manager import get_age
 )
 def test_get_age(cpr_number, expected_age):
     with freeze_time("2023-12-01"):
-
         assert get_age(cpr_number) == expected_age
+
+
+@pytest.mark.parametrize(
+    "ancestors,expected_list",
+    (
+        # Test top unit that has no ancestors
+        ([], ["", "", "", "", "", "", ""]),
+        # Test a unit with one ancestor
+        ([{"name": "Niveau 1"}], ["Niveau 1", "", "", "", "", "", ""]),
+        # Test 8 that with levels of ancestors we return the top 7.
+        (
+            [
+                {"name": "Niveau 8"},
+                {"name": "Niveau 7"},
+                {"name": "Niveau 6"},
+                {"name": "Niveau 5"},
+                {"name": "Niveau 4"},
+                {"name": "Niveau 3"},
+                {"name": "Niveau 2"},
+                {"name": "Niveau 1"},
+            ],
+            [
+                "Niveau 1",
+                "Niveau 2",
+                "Niveau 3",
+                "Niveau 4",
+                "Niveau 5",
+                "Niveau 6",
+                "Niveau 7",
+            ],
+        ),
+    ),
+)
+def test_extract_ancestors(ancestors, expected_list):
+    assert extract_ancestors(ancestors) == expected_list
