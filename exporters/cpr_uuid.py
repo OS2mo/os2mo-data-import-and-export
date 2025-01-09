1fd943097a4a1d7366ae2e4cc2811da07a27da0a
Emil <emil@magenta.dk>
2025-01-09 18:48:01 +0100

    refactor(cpr_uuid): [#63478] eliminate ExportUser pydantic model

--- exporters/cpr_uuid.py
+++ exporters/cpr_uuid.py
@@ -1,19 +1,10 @@
 from collections.abc import Iterator
 from typing import Any
-from uuid import UUID
 
 import click
 from os2mo_helpers.mora_helpers import MoraHelper
-from pydantic import BaseModel
 from ra_utils.load_settings import load_setting
 from ra_utils.tqdm_wrapper import tqdm
-
-
-class ExportUser(BaseModel):
-    cpr: str | None
-    mo_uuid: UUID
-    ad_guid: UUID | None
-    sam_account_name: str | None
 
 
 def cache_ad_reader() -> Any:
@@ -26,8 +17,7 @@ def cache_ad_reader() -> Any:
     return ad_reader
 
 
-def create_mapping(helper: MoraHelper, use_ad: bool) -> Iterator[ExportUser]:
-
+def create_mapping(helper: MoraHelper, use_ad: bool) -> Iterator[dict[str, Any]]:
     print("Fetching all users from MO...")
     employees = helper.read_all_users()
     print("OK")
@@ -40,18 +30,22 @@ def create_mapping(helper: MoraHelper, use_ad: bool) -> Iterator[ExportUser]:
         cpr = employee.get("cpr_no")
         if not cpr:
             print("no 'cpr_no' for MO user %r", employee["uuid"])
-        user = ExportUser(
-            cpr=cpr,
-            mo_uuid=employee["uuid"],
-        )
+
+        ad_guid = None
+        sam_account_name = None
 
         if ad_reader:
-            ad_info = ad_reader.read_user(cpr=user.cpr, cache_only=True)
+            ad_info = ad_reader.read_user(cpr=cpr, cache_only=True)
             if ad_info:
-                user.ad_guid = ad_info["ObjectGuid"]
-                user.sam_account_name = ad_info["SamAccountName"]
+                ad_guid = ad_info["ObjectGuid"]
+                sam_account_name = ad_info["SamAccountName"]
 
-        yield user
+        yield {
+            "cpr": cpr,
+            "mo_uuid": employee["uuid"],
+            "ad_guid": ad_guid,
+            "sam_account_name": sam_account_name,
+        }
 
     print("OK")
 
@@ -59,8 +53,7 @@ def create_mapping(helper: MoraHelper, use_ad: bool) -> Iterator[ExportUser]:
 def main(mora_base: str, use_ad: bool, output_file_path: str) -> None:
     mh = MoraHelper(hostname=mora_base, export_ansi=False)
 
-    employees = list(create_mapping(mh, use_ad))
-    employee_dicts = [x.dict() for x in employees]
+    employee_dicts = list(create_mapping(mh, use_ad))
 
     fields = ["cpr", "mo_uuid", "ad_guid", "sam_account_name"]
     mh._write_csv(fields, employee_dicts, output_file_path)
