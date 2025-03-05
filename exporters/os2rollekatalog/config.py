70c03a5daa12ed44eba8870ee2b815d29516fbe6
Carl Bordum Hansen <carl@bordum.dk>
2025-03-05 17:16:35 +0100

    fix: [#63990] read ldap_url as other configs

--- exporters/os2rollekatalog/config.py
+++ exporters/os2rollekatalog/config.py
@@ -4,5 +4,3 @@ from fastramqpi.ra_utils.job_settings import JobSettings
 class RollekatalogSettings(JobSettings):
     class Config:
         settings_json_prefix = "exporters.os2rollekatalog"
-
-    ldap_url: str | None = None
