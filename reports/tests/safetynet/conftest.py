4a1578ae3432546b994b967959bbcf4b6518bdb9
Andreas Kring <andreas@magenta-aps.dk>
2025-09-08 10:37:45 +0200

    test: test get_sd_manager_eng_user_key

--- reports/tests/safetynet/conftest.py
+++ reports/tests/safetynet/conftest.py
@@ -78,7 +78,7 @@ def opus_ou_manager_the_same_as_eng_employee() -> dict[str, Any]:
 
 
 @pytest.fixture
-def opus_engagements() -> dict[str, Any]:
+def engagements() -> dict[str, Any]:
     return {
         "engagements": {
             "objects": [
@@ -126,6 +126,61 @@ def opus_ou_parent_manager() -> dict[str, Any]:
                             "uuid": "48a3819e-c5e1-4f4d-b4ad-582caff6ea51",
                             "managers": [{"user_key": "54321"}],
                         }
+                    }
+                }
+            ]
+        }
+    }
+
+
+@pytest.fixture
+def sd_ou_manager_not_the_same_as_eng_employee() -> dict[str, Any]:
+    return {
+        "org_units": {
+            "objects": [
+                {
+                    "current": {
+                        "name": "Some name",
+                        "uuid": "9d1af806-f4d6-44e2-a001-a5deb3aa6703",
+                        "engagements": [
+                            {"uuid": "83483193-c623-4a59-a0c1-ac8887fba72e"}
+                        ],
+                        "parent": {"uuid": "06f200ae-a05e-4fb3-91a4-9f16a0fc0b98"},
+                        "children": [],
+                        "managers": [
+                            {
+                                "user_key": "e2b58581-1cbe-4397-8ca6-d8ae97d8cfa4",
+                                "person": [
+                                    {
+                                        "engagements": [
+                                            {
+                                                "user_key": "54321",
+                                                "org_unit_uuid": "9d1af806-f4d6-44e2-a001-a5deb3aa6703",
+                                            },
+                                            {
+                                                "user_key": "23456",
+                                                "org_unit_uuid": "4c448c55-9f2d-4b7e-a386-44ca218c977b",
+                                            },
+                                        ]
+                                    }
+                                ],
+                            }
+                        ],
+                        "addresses": [{"value": "1234567890"}],
+                        "related_units": [
+                            {
+                                "org_units": [
+                                    {"uuid": "626ab928-a76f-46ae-bc8f-2402deb65236"},
+                                    {"uuid": "abcab928-a76f-46ae-bc8f-2402deb65123"},
+                                ]
+                            },
+                            {
+                                "org_units": [
+                                    {"uuid": "18b8b97d-946d-4948-a334-0582935f7c5c"},
+                                    {"uuid": "abcab928-a76f-46ae-bc8f-2402deb65123"},
+                                ]
+                            },
+                        ],
                     }
                 }
             ]
--- reports/tests/safetynet/test_safetynet.py
+++ reports/tests/safetynet/test_safetynet.py
@@ -12,6 +12,7 @@ from pydantic import AnyHttpUrl
 from pydantic import SecretStr
 
 from reports.safetynet.config import SafetyNetSettings
+from reports.safetynet.config import SourceSystem
 from reports.safetynet.safetynet import main
 
 
@@ -21,7 +22,7 @@ def test_adm_engagements_opus_ou_manager_not_the_same_as_eng_employee(
     mock_get_mo_client: MagicMock,
     mock_get_unified_settings: MagicMock,
     opus_ou_manager_not_the_same_as_eng_employee,
-    opus_engagements: dict[str, Any],
+    engagements,
 ) -> None:
     # Arrange
     csv_file = "/tmp/adm-engagements.csv"
@@ -39,7 +40,7 @@ def test_adm_engagements_opus_ou_manager_not_the_same_as_eng_employee(
         # The OU GraphQL call
         opus_ou_manager_not_the_same_as_eng_employee,
         # The engagements GraphQL call
-        opus_engagements,
+        engagements,
     ]
     mock_get_mo_client.return_value = mock_gql_client
 
@@ -65,7 +66,7 @@ def test_adm_engagements_opus_ou_manager_the_same_as_eng_employee(
     mock_get_mo_client: MagicMock,
     mock_get_unified_settings: MagicMock,
     opus_ou_manager_the_same_as_eng_employee,
-    opus_engagements: dict[str, Any],
+    engagements,
     opus_ou_parent_manager: dict[str, Any],
 ) -> None:
     # Arrange
@@ -84,7 +85,7 @@ def test_adm_engagements_opus_ou_manager_the_same_as_eng_employee(
         # The OU GraphQL call
         opus_ou_manager_the_same_as_eng_employee,
         # The engagements GraphQL call
-        opus_engagements,
+        engagements,
         # The parent manager GraphQL call
         opus_ou_parent_manager,
     ]
@@ -104,6 +105,51 @@ def test_adm_engagements_opus_ou_manager_the_same_as_eng_employee(
     assert parent_ou_call[1] == {
         "variable_values": {"org_unit": "9d1af806-f4d6-44e2-a001-a5deb3aa6703"}
     }
+
+    assert csv_lines == [
+        "Medarbejdernummer||CPR||Fornavn||Efternavn||Mail||Afdelingskode||Startdato||Slutdato||LedersMedarbejdernummer||Brugernavn||Titel||Faggruppe\n",
+        "12345||0101011255||Bruce||Lee||bruce@kung.fu||9d1af806-f4d6-44e2-a001-a5deb3aa6703||2021-10-22||2025-09-30||54321||bruce||Kung Fu Master||Kung Fu Master\n",
+    ]
+
+
+@patch("reports.safetynet.safetynet.get_unified_settings")
+@patch("reports.safetynet.safetynet.get_mo_client")
+def test_adm_engagements_sd_ou_manager_the_same_as_eng_employee(
+    mock_get_mo_client: MagicMock,
+    mock_get_unified_settings: MagicMock,
+    sd_ou_manager_not_the_same_as_eng_employee,
+    engagements,
+) -> None:
+    # Arrange
+    csv_file = "/tmp/adm-engagements.csv"
+
+    mock_get_unified_settings.return_value = SafetyNetSettings(
+        auth_server=cast(AnyHttpUrl, "http://mocked.keycloak"),
+        client_id="client-id",
+        client_secret=SecretStr("secret"),
+        mora_base="http://mora.base",
+        safetynet_adm_unit_uuid=UUID("9d1af806-f4d6-44e2-a001-a5deb3aa6703"),
+        source_system=SourceSystem.SD,
+    )
+
+    mock_gql_client = MagicMock(spec=GraphQLClient)
+    mock_gql_client.execute.side_effect = [
+        # The OU GraphQL call
+        sd_ou_manager_not_the_same_as_eng_employee,
+        # The engagements GraphQL call
+        engagements,
+    ]
+    mock_get_mo_client.return_value = mock_gql_client
+
+    runner = CliRunner()
+
+    # Act
+    runner.invoke(main, ["--skip-upload", "--only-adm-org"])
+
+    # Assert
+    with open(csv_file) as fp:
+        csv_lines = fp.readlines()
+    os.remove(csv_file)
 
     assert csv_lines == [
         "Medarbejdernummer||CPR||Fornavn||Efternavn||Mail||Afdelingskode||Startdato||Slutdato||LedersMedarbejdernummer||Brugernavn||Titel||Faggruppe\n",
