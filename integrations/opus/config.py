e0e18b055eea200f4ca61da541c130d1a873e536
Lorenzo Panieri <lp@magenta-aps.dk>
2026-04-20 10:17:16 +0200

    refactor: [#69701] refactor `integrations.opus.clear_and_import_opus` to use `JobSettings`

--- integrations/opus/config.py
+++ integrations/opus/config.py
@@ -8,3 +8,10 @@ class ClearAndImportOpusSettings(JobSettings):
     mox_base: str = "http://localhost:5000/lora"
     integrations_opus_units_filter_ids: list[str] = []
     integrations_opus_import_run_db: str | None = None
+
+
+class OpusCheckDarAddressesSettings(JobSettings):
+    class Config(JobSettings.Config):
+        frozen = True
+
+    integrations_opus_units_filter_ids: list[str] = []
--- integrations/opus/opus_check_dar_adresses.py
+++ integrations/opus/opus_check_dar_adresses.py
@@ -4,8 +4,8 @@ import click
 from fastramqpi.ra_utils.tqdm_wrapper import tqdm
 
 from integrations import dawa_helper
+from integrations.opus.config import OpusCheckDarAddressesSettings
 from integrations.opus.opus_helpers import get_latest_dump
-from integrations.opus.opus_helpers import load_settings
 from integrations.opus.opus_helpers import read_and_transform_data
 
 logger = logging.getLogger("OpusInvalidDar")
@@ -13,12 +13,12 @@ logger = logging.getLogger("OpusInvalidDar")
 
 @click.command()
 def cli():
-    settings = load_settings()
+    settings = OpusCheckDarAddressesSettings()
     _, opus_dump = get_latest_dump()
     org_units, _, _, _, _ = read_and_transform_data(
         None,
         opus_dump,
-        filter_ids=settings["integrations.opus.units.filter_ids"],
+        filter_ids=settings.integrations_opus_units_filter_ids,
     )
     invalid_addresses = []
     for unit in tqdm(org_units):
