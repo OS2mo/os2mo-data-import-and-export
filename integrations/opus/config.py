8d3ec2bf76c4b3456952fa36241c2597014d031e
Lorenzo Panieri <lp@magenta-aps.dk>
2026-04-21 12:53:54 +0200

    refactor: [#69681] removed unused field `mox_base` from `ClearAndImportOpusSettings`

--- integrations/opus/config.py
+++ integrations/opus/config.py
@@ -7,7 +7,6 @@ class ClearAndImportOpusSettings(JobSettings):
     class Config(JobSettings.Config):
         fronzen = True
 
-    mox_base: str = "http://localhost:5000/lora"
     integrations_opus_units_filter_ids: list[str] = []
     integrations_opus_import_run_db: str | None = None
 
