09788591aba7634d577b86be294ae8ba2ddebc4e
blj <blj@magenta.dk>
2026-06-30 11:13:00 +0000

    chore(sql_export): track init-databases.sql and unblock it from .gitignore
    The root .gitignore had a blanket *.sql rule that prevented
    init-databases.sql from being committed. Add a negation exception so it
    can be tracked alongside the docker-compose.yaml that mounts it.

---
+++ exporters/sql_export/init-databases.sql
@@ -0,0 +1,6 @@
+-- SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+-- SPDX-License-Identifier: MPL-2.0
+
+-- Create the actualstate and historic databases if they don't exist
+CREATE DATABASE actualstate OWNER fastramqpi;
+CREATE DATABASE historic OWNER fastramqpi;
